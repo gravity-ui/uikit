d73c8eee31226cd6efcfe483e4fb66ec4b8824e8
Andrey Morozov <amje@yandex-team.ru>
2022-01-27 16:19:27 +0300

    feat: border-radius tokens (#65)
    * refactor: implement pin mixin
    
    * refactor: set button pin default
    
    * feat: new border-radius tokens

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -74,7 +74,7 @@ const PureButton = React.forwardRef<HTMLButtonElement | HTMLAnchorElement, Butto
         {
             view = 'normal',
             size = 'm',
-            pin,
+            pin = 'round-round',
             selected = false,
             disabled = false,
             loading = false,

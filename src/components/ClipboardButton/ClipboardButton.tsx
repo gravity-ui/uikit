d1b2cf213642ee6f59d91b6681807996712528e9
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2022-02-10 11:24:33 +0300

    feat(ClipboardButton): add `onCopy` prop

--- src/components/ClipboardButton/ClipboardButton.tsx
+++ src/components/ClipboardButton/ClipboardButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {CopyToClipboard} from '../CopyToClipboard';
+import {CopyToClipboard, CopyToClipboardProps} from '../CopyToClipboard';
 import {QAProps} from '../types';
 import {FakeButton} from './FakeButton';
 
@@ -10,11 +10,13 @@ export interface ClipboardButtonProps extends QAProps {
     size?: number;
     /** Element CSS class */
     className?: string;
+    /** Handler that would be invoked after success copy to clipboard */
+    onCopy?: CopyToClipboardProps['onCopy'];
 }
 
-export function ClipboardButton({text, size, className, qa}: ClipboardButtonProps) {
+export function ClipboardButton({text, size, className, qa, onCopy}: ClipboardButtonProps) {
     return (
-        <CopyToClipboard text={text} timeout={1000}>
+        <CopyToClipboard text={text} timeout={1000} onCopy={onCopy}>
             {(status) => (
                 <FakeButton className={className} data-qa={qa} status={status} size={size} />
             )}

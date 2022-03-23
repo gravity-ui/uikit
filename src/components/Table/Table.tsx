ca870e2d01886a3a3d845129e1f25103cc08d6c9
Andrey Morozov <amje@yandex-team.ru>
2022-03-24 00:04:17 +0300

    feat: i18n support

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -4,6 +4,7 @@ import _has from 'lodash/has';
 import _isNumber from 'lodash/isNumber';
 import ResizeObserver from 'resize-observer-polyfill';
 import {block} from '../utils/cn';
+import i18n from './i18n';
 import './Table.scss';
 
 const DASH = '\u2014';
@@ -405,7 +406,7 @@ export class Table<I extends TableDataItem = Record<string, string>> extends Rea
         return (
             <tr className={b('row', {empty: true})}>
                 <td className={b('cell')} colSpan={columns.length}>
-                    {emptyMessage ? emptyMessage : 'No data'}
+                    {emptyMessage ? emptyMessage : i18n('label_empty')}
                 </td>
             </tr>
         );

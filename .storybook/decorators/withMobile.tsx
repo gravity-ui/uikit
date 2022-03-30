f7ab1ccbaa0965c80c6c6774cab97e4b0b84a483
Valeriy <ValeraS@users.noreply.github.com>
2022-03-30 17:42:16 +0300

    feat(mobile): get rid of react-router-dom (#110)
    Closes #105

--- .storybook/decorators/withMobile.tsx
+++ .storybook/decorators/withMobile.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
 import {Story as StoryType, StoryContext} from '@storybook/react';
 import {useMobile} from '../../src/components/mobile';
 
 export function withMobile(Story: StoryType, context: StoryContext) {
     const mobileValue = context.globals.platform === 'mobile';
 
-    const [mobile, setMobile] = useMobile(); // eslint-disable-line react-hooks/rules-of-hooks
+    const [, setMobile] = useMobile(); // eslint-disable-line react-hooks/rules-of-hooks
 
-    if (mobile !== mobileValue) {
+    React.useEffect(() => {
         setMobile(mobileValue);
-    }
+    }, [mobileValue]);
 
-    return (
-        <Router>
-            <Story {...context} />
-        </Router>
-    );
+    return <Story {...context} />;
 }

c91b4c2ec371409c3a6643ad41e97eb8be013827
Roman Barlos <darkgenius@nextmail.ru>
2022-04-25 16:33:05 +0300

    Fix stories styles (#142)

--- src/components/Stories/Stories.tsx
+++ src/components/Stories/Stories.tsx
@@ -119,17 +119,21 @@ export function Stories({
                                     </div>
                                     <div className={b('controls-block')}>
                                         {hasPreviousStory && (
-                                            <Button onClick={handleGotoPrevious} view="outlined">
+                                            <Button
+                                                onClick={handleGotoPrevious}
+                                                view="outlined"
+                                                size="l"
+                                            >
                                                 {i18n(lang, 'label_back')}
                                             </Button>
                                         )}
                                         {(isFirstStory || isLastStory) && (
-                                            <Button onClick={handleButtonClose}>
+                                            <Button onClick={handleButtonClose} size="l">
                                                 {i18n(lang, 'label_close')}
                                             </Button>
                                         )}
                                         {hasNextStory && (
-                                            <Button onClick={handleGotoNext} view="action">
+                                            <Button onClick={handleGotoNext} view="action" size="l">
                                                 {i18n(lang, 'label_next')}
                                             </Button>
                                         )}

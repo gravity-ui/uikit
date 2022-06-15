951f74a90c32090a77688421e27b1ad4c38cfc6f
Andrey Morozov <amje@yandex-team.ru>
2022-06-15 21:59:12 +0300

    feat: improve radio-group components a11y

--- src/components/RadioButton/__stories__/RadioButton.stories.tsx
+++ src/components/RadioButton/__stories__/RadioButton.stories.tsx
@@ -14,10 +14,12 @@ const options: RadioButtonOption[] = [
     {value: 'Value 3', content: 'Value 3'},
 ];
 
-const DefaultTemplate: Story<RadioButtonProps> = (args) => <RadioButton {...args} />;
+const DefaultTemplate: Story<RadioButtonProps> = (args) => {
+    const [value, setValue] = React.useState<string>(options[0].value);
+    return <RadioButton {...args} value={value} onUpdate={setValue} />;
+};
 export const Default = DefaultTemplate.bind({});
 Default.args = {
-    value: options[0].value,
     options,
 };
 

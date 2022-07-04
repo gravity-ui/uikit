e0e92c2353ecffd6688fbcbeb660094d49394b6b
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2022-07-04 12:36:20 +0300

    fix: do not filter when toast is missing

--- src/components/Toaster/utilities/removeToast.ts
+++ src/components/Toaster/utilities/removeToast.ts
@@ -1,5 +1,10 @@
 import {ToastProps} from '../types';
+import {hasToast} from './hasToast';
 
 export function removeToast(toasts: ToastProps[], name: ToastProps['name']) {
+    if (!hasToast(toasts, name)) {
+        return toasts;
+    }
+
     return toasts.filter((toast) => toast.name !== name);
 }

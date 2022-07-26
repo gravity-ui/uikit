8c5816d16b2ead43d5ff687eab1c1f95bb5cb255
Alexey <sudilovskiy@yandex-team.ru>
2022-07-27 00:00:54 +0300

    test: improve toast override test

--- src/components/Toaster/__tests__/ToasterProvider.test.tsx
+++ src/components/Toaster/__tests__/ToasterProvider.test.tsx
@@ -1,32 +1,32 @@
-import {act, render, screen, waitForElementToBeRemoved} from '@testing-library/react';
+import {act, render, screen} from '@testing-library/react';
 import React, {useEffect} from 'react';
 import {useToaster} from '../hooks/useToaster';
 import {ToasterComponent} from '../ToasterComponent/ToasterComponent';
 import {ToasterProvider} from '../Provider/ToasterProvider';
-import {ToasterPublicMethods, ToastProps} from '../types';
+import {ToasterPublicMethods} from '../types';
+import {getToast} from './getToast';
 
-function Toast(props: ToastProps & {onMount: (api: ToasterPublicMethods) => void}) {
+function ToastAPI({onMount}: {onMount: (api: ToasterPublicMethods) => void}) {
     const toaster = useToaster();
 
     useEffect(() => {
-        toaster.add(props);
-    }, [props, toaster]);
-
-    useEffect(() => {
-        props.onMount(toaster);
+        onMount(toaster);
     }, []);
 
     return null;
 }
 
+beforeEach(() => jest.useFakeTimers());
+afterEach(() => jest.useRealTimers());
+
+// We test that after adding toast the next add will remove
+// previous toast from DOM and add it again
 it('should override already added toast', async function () {
-    let providerAPI: undefined | null | ToasterPublicMethods;
+    let providerAPI: undefined | ToasterPublicMethods;
 
     render(
         <ToasterProvider>
-            <Toast
-                name="test"
-                title="Test Toast"
+            <ToastAPI
                 onMount={(api) => {
                     providerAPI = api;
                 }}
@@ -35,21 +35,27 @@ it('should override already added toast', async function () {
         </ToasterProvider>,
     );
 
-    let toast = await screen.findByText('Test Toast');
-
-    expect(toast).toBeInTheDocument();
-
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    await act(async () => {
+    act(() => {
         providerAPI?.add({
             name: 'test',
             title: 'Test Toast',
         });
-
-        await waitForElementToBeRemoved(toast);
-
-        expect(toast).not.toBeInTheDocument();
     });
+
+    let toast = getToast();
+
+    expect(toast).toBeInTheDocument();
+
+    jest.advanceTimersByTime(1);
+
+    act(() => {
+        providerAPI?.add({
+            name: 'test',
+            title: 'Test Toast',
+        });
+    });
+
+    expect(toast).not.toBeInTheDocument();
 
     toast = await screen.findByText('Test Toast');
 

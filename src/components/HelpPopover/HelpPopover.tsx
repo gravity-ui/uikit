a289e6abf4bf968833ade28376f8d054a8bcad10
Daniil Chervyakov <da-chervyakov@yandex-team.ru>
2022-08-17 16:22:44 +0300

    fix: return className to component

--- src/components/HelpPopover/HelpPopover.tsx
+++ src/components/HelpPopover/HelpPopover.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import {block} from '../utils/cn';
 import {Popover, PopoverProps} from '../Popover';
 import {Icon} from '../Icon';
 import {QuestionMarkIcon} from '../icons/QuestionMarkIcon';
 import {QAProps} from '../types';
+
+const b = block('help-popover');
 
 /**
  * @see {@link https://github.com/microsoft/TypeScript/issues/28339}
@@ -14,7 +17,7 @@ export type HelpPopoverProps = DistributiveOmit<PopoverProps, 'children'> & QAPr
 
 export function HelpPopover(props: HelpPopoverProps) {
     return (
-        <Popover {...props}>
+        <Popover {...props} className={b(null, props.className)}>
             <Icon data={QuestionMarkIcon} size={16} />
         </Popover>
     );

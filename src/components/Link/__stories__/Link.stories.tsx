5484c61633f8b19c6cff51261687dd095a8abcf8
LakeVostok <nskulikov@yandex-team.ru>
2022-09-07 12:44:31 +0500

    docs(Link): impoving Link docs

--- src/components/Link/__stories__/Link.stories.tsx
+++ src/components/Link/__stories__/Link.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Meta, Story} from '@storybook/react';
 import {Link, LinkProps} from '../Link';
+import {LinkShowcase} from '../__stories__/LinkShowcase';
 
 export default {
     title: 'Components/Link',
@@ -9,3 +10,6 @@ export default {
 
 const DefaultTemplate: Story<LinkProps> = (args) => <Link {...args}>Link</Link>;
 export const Default = DefaultTemplate.bind({});
+
+const ShowcaseTemplate: Story = () => <LinkShowcase />;
+export const Showcase = ShowcaseTemplate.bind({});
---
+++ src/demo/ShowcaseGrid/ShowcaseGrid.tsx
@@ -0,0 +1,123 @@
+import React, {useMemo} from 'react';
+import {Text} from '../../components/Text';
+import {getPropsCombinations, PropCombination, PropSequences} from './getPropsCombinations';
+
+type Combinations<ComponentType extends React.ElementType> = PropCombination<
+    Partial<React.ComponentProps<ComponentType>>
+>;
+
+export function ShowcaseGrid<ComponentType extends React.ElementType>({
+    component: Component,
+    propsCombinations,
+    staticProps,
+    rowKey,
+}: {
+    component: ComponentType;
+    propsCombinations: Combinations<ComponentType>;
+    staticProps?: Partial<React.ComponentProps<ComponentType>>;
+    rowKey?: keyof React.ComponentProps<ComponentType>;
+}) {
+    const combinations = useMemo(() => {
+        return getPropsCombinations<typeof Component>({propsCombinations, staticProps});
+    }, [propsCombinations, staticProps]);
+
+    if (rowKey) {
+        const names = propsCombinations[rowKey].map(({name}) => name);
+
+        const rows = combinations.reduce((result, combination) => {
+            names.forEach((name) => {
+                if (combination.names[rowKey] === name) {
+                    result[name] = result[name] || [];
+                    result[name].push(combination);
+                }
+            });
+
+            return result;
+        }, {} as Record<string, PropSequences<React.ComponentProps<ComponentType>>>);
+
+        return (
+            <table
+                style={{
+                    borderSpacing: '10px',
+                }}
+            >
+                <tbody>
+                    <tr>
+                        <td></td>
+                        {rows[names[0]].map((combination) => {
+                            const title = Object.keys(combination.names)
+                                .filter((name) => name !== rowKey)
+                                .map((name) => combination.names[name])
+                                .join(', ');
+
+                            return (
+                                <td key={title}>
+                                    <Text variant="subheader-1">{title}</Text>
+                                </td>
+                            );
+                        })}
+                    </tr>
+
+                    {names.map((name) => {
+                        return (
+                            <tr key={name}>
+                                <td>
+                                    <Text variant="subheader-1">{name}</Text>
+                                </td>
+                                {rows[name].map((combination) => {
+                                    const title = Object.keys(combination.names)
+                                        .map((name) => combination.names[name])
+                                        .join(', ');
+
+                                    return (
+                                        <td
+                                            key={title}
+                                            style={{
+                                                textAlign: 'center',
+                                            }}
+                                        >
+                                            <Component {...combination.props} />
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        );
+    }
+
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    {combinations.map((combination) => {
+                        const title = Object.keys(combination.names)
+                            .map((name) => combination.names[name])
+                            .join(', ');
+
+                        return (
+                            <td key={title}>
+                                <Text variant="subheader-1">{title}</Text>
+                            </td>
+                        );
+                    })}
+                </tr>
+                <tr>
+                    {combinations.map((combination) => {
+                        const title = Object.keys(combination.names)
+                            .map((name) => combination.names[name])
+                            .join(', ');
+
+                        return (
+                            <td key={title}>
+                                <Component {...combination.props} />
+                            </td>
+                        );
+                    })}
+                </tr>
+            </tbody>
+        </table>
+    );
+}

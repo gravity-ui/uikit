ea9e31f39f4ed71bebd708e948f80e034d08e821
bocembocem <58661343+bocembocem@users.noreply.github.com>
2022-09-09 17:01:28 +0300

    fix(RadioButton): fix option icon styles (#339)
    Co-authored-by: Nikita Zolotykh <nvzolotykh@yandex-team.ru>

--- src/components/utils/common.ts
+++ src/components/utils/common.ts
@@ -1,7 +1,11 @@
 import {NAMESPACE} from './cn';
+import {isOfType} from './isOfType';
+import {Icon} from '../Icon';
 
 let nextUniqueId = 1;
 
 export function getUniqId() {
     return `${NAMESPACE}uniq-${nextUniqueId++}`;
 }
+
+export const isIcon = isOfType(Icon);

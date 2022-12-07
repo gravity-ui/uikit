07ac37efb861e32fb2941210a95f54f0bd3bfbbd
d3m1d0v <d3m1d0v@yandex-team.ru>
2022-12-07 16:40:59 +0300

    fix(HotKey): dont use os to identify current platform

--- src/components/HotKey/utils.ts
+++ src/components/HotKey/utils.ts
@@ -1,11 +1,5 @@
 export const isMac = (): boolean =>
-    typeof navigator !== 'undefined'
-        ? /Mac|iP(hone|[oa]d)/.test(navigator.platform)
-        : // @ts-expect-error Cannot find name 'os'.ts(2304)
-        typeof os !== 'undefined' && os.platform
-        ? // @ts-expect-error Cannot find name 'os'.ts(2304)
-          os.platform() == 'darwin'
-        : false;
+    typeof navigator !== 'undefined' ? /Mac|iP(hone|[oa]d)/.test(navigator.platform) : false;
 
 export function split(val: string, separator: string | RegExp): string[] {
     return val.trim().split(separator).filter(Boolean);

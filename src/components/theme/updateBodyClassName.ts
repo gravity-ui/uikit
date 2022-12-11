f6c9411197c68a3a5cd97a625447b84b4d6fb949
Andrey Volynkin <avol-v@yandex-team.ru>
2022-12-11 16:05:51 +0300

    feat: add nativeScrollbar property to ThemeProvider

--- src/components/theme/updateBodyClassName.ts
+++ src/components/theme/updateBodyClassName.ts
@@ -4,11 +4,22 @@ import {RealTheme} from './types';
 const b = block('root');
 const rootClassName = b();
 
+export type BodyClassNameModifiers = {
+    'native-scrollbar': boolean;
+};
+
+const defaultModifiers: BodyClassNameModifiers = {
+    'native-scrollbar': false,
+};
+
 function modifier(className: string) {
     return className.split(/\s+/)[1];
 }
 
-export function updateBodyClassName(newTheme: RealTheme) {
+export function updateBodyClassName(
+    newTheme: RealTheme,
+    modifiers?: Partial<BodyClassNameModifiers>,
+) {
     const bodyEl = document.body;
 
     if (!bodyEl.classList.contains(rootClassName)) {
@@ -21,4 +32,8 @@ export function updateBodyClassName(newTheme: RealTheme) {
         }
     });
     bodyEl.classList.add(modifier(b({theme: newTheme})));
+
+    for (const [key, value] of Object.entries({...defaultModifiers, ...modifiers})) {
+        bodyEl.classList.toggle(modifier(b({[key]: true})), value);
+    }
 }

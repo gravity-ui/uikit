1a1535ed8183017740149729c3cc6f521e674581
Gregorii Kovorotnyi <gkovorotny@yandex-team.ru>
2022-12-26 09:42:47 +0300

    refactor: throttle useElementSize

--- src/components/utils/useElementSize.ts
+++ src/components/utils/useElementSize.ts
@@ -1,5 +1,8 @@
-import {useLayoutEffect, useState, MutableRefObject} from 'react';
+import {useLayoutEffect, useState, MutableRefObject, useCallback} from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
+import _throttle from 'lodash/throttle';
+
+const RESIZE_THROTTLE = 16;
 
 interface ElementSize {
     width: number;
@@ -14,25 +17,27 @@ export function useElementSize<T extends HTMLElement = HTMLDivElement>(
         height: 0,
     });
 
+    const handleResize = useCallback<ResizeObserverCallback>((entries) => {
+        if (!Array.isArray(entries)) {
+            return;
+        }
+
+        const entry = entries[0];
+        setSize({width: entry.contentRect.width, height: entry.contentRect.height});
+    }, []);
+
     useLayoutEffect(() => {
         if (!ref?.current) {
             return undefined;
         }
 
-        const observer = new ResizeObserver((entries) => {
-            if (!Array.isArray(entries)) {
-                return;
-            }
-
-            const entry = entries[0];
-            setSize({width: entry.contentRect.width, height: entry.contentRect.height});
-        });
+        const observer = new ResizeObserver(_throttle(handleResize, RESIZE_THROTTLE));
         observer.observe(ref.current);
 
         return () => {
             observer.disconnect();
         };
-    }, [ref]);
+    }, [ref, handleResize]);
 
     return size;
 }

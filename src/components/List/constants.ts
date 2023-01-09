78a8a6017e4a5b0c1f2922f76b76ad3c5f01969b
Ксения <31247233+kseniya57@users.noreply.github.com>
2023-01-09 14:50:44 +0300

    fix(List): remove focus from list on Tab key (#444)
    Co-authored-by: kseniyakuzina <kseniyakuzina@yandex-team.ru>

--- src/components/List/constants.ts
+++ src/components/List/constants.ts
@@ -1,3 +1,10 @@
+import {KeyCode} from '../constants';
+
 export const ListQa = {
     ACTIVE_ITEM: 'list-active-item',
 };
+
+export const listNavigationIgnoredKeys = [
+    // Tab key should focus the next element
+    KeyCode.TAB,
+];

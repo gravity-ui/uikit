f19666754a1d1a9a7cd9094533f1f0db112ce205
Andrey Morozov <amje@yandex-team.ru>
2023-01-31 23:21:08 +0300

    ci: fix storybook

--- .storybook/theme-addon/register.tsx
+++ .storybook/theme-addon/register.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {addons, types} from '@storybook/addons';
-import {useGlobals} from '@storybook/api';
+import {useGlobals, type API} from '@storybook/api';
 import {FORCE_RE_RENDER} from '@storybook/core-events';
 import {themes} from '../theme';
 import {getThemeType} from '../../src/components/theme/getThemeType';
@@ -18,7 +18,7 @@ addons.register(ADDON_ID, (api) => {
     });
 });
 
-function Tool({api}) {
+function Tool({api}: {api: API}) {
     const [{theme}] = useGlobals();
     React.useEffect(() => {
         api.setOptions({theme: themes[getThemeType(theme)]});

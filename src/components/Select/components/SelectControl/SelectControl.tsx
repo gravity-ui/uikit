061a8df8b87d82c5f0185e7271bcedfe7fa33fb5
Mr.Dr.Professor Patrick <alaev89@yandex-team.ru>
2023-02-24 20:48:50 +0100

    feat(Select): add error property (#548)

--- src/components/Select/components/SelectControl/SelectControl.tsx
+++ src/components/Select/components/SelectControl/SelectControl.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {useForkRef} from '../../../utils/useForkRef';
-import type {CnMods} from '../../../utils/cn';
+import {Button} from '../../../Button';
 import {Icon} from '../../../Icon';
 import {Chevron} from '../../../icons/Chevron';
 import type {SelectProps} from '../../types';
 import {selectControlBlock} from '../../constants';
+import {mapToButtonView} from '../../utils';
 
 import './SelectControl.scss';
 
@@ -21,6 +22,7 @@ type ControlProps = {
     qa?: string;
     label?: string;
     placeholder?: SelectProps['placeholder'];
+    error?: SelectProps['error'];
     open?: boolean;
     disabled?: boolean;
 };
@@ -39,6 +41,7 @@ export const SelectControl = React.forwardRef<HTMLElement, ControlProps>((props,
         name,
         label,
         placeholder,
+        error,
         open,
         disabled,
     } = props;
@@ -46,13 +49,6 @@ export const SelectControl = React.forwardRef<HTMLElement, ControlProps>((props,
     const handleControlRef = useForkRef<HTMLElement>(ref, controlRef);
     const showOptionsText = Boolean(selectedOptionsContent);
     const showPlaceholder = Boolean(placeholder && !showOptionsText);
-    const mods: CnMods = {
-        view,
-        size,
-        pin,
-        disabled,
-        open,
-    };
 
     const handleClick = React.useCallback(() => setOpen(!open), [setOpen, open]);
 
@@ -66,27 +62,38 @@ export const SelectControl = React.forwardRef<HTMLElement, ControlProps>((props,
     }
 
     return (
-        <button
-            ref={handleControlRef as React.Ref<HTMLButtonElement>}
-            name={name}
-            className={selectControlBlock(mods, className)}
-            aria-haspopup="listbox"
-            aria-expanded={open ? 'true' : 'false'}
-            disabled={disabled}
-            onClick={handleClick}
-            onKeyDown={onKeyDown}
-            type="button"
-            data-qa={qa}
-        >
-            {label && <span className={selectControlBlock('label')}>{label}</span>}
-            {showPlaceholder && (
-                <span className={selectControlBlock('placeholder')}>{placeholder}</span>
+        <React.Fragment>
+            <Button
+                ref={handleControlRef as React.Ref<HTMLButtonElement>}
+                className={selectControlBlock({open, error: Boolean(error)}, className)}
+                qa={qa}
+                view={mapToButtonView(view)}
+                size={size}
+                pin={pin}
+                extraProps={{
+                    name,
+                    'aria-haspopup': 'listbox',
+                    'aria-expanded': open ? 'true' : 'false',
+                    onKeyDown,
+                }}
+                disabled={disabled}
+                onClick={handleClick}
+            >
+                {label && <span className={selectControlBlock('label')}>{label}</span>}
+                {showPlaceholder && (
+                    <span className={selectControlBlock('placeholder')}>{placeholder}</span>
+                )}
+                {showOptionsText && (
+                    <span className={selectControlBlock('option-text')}>
+                        {selectedOptionsContent}
+                    </span>
+                )}
+                <Icon className={selectControlBlock('chevron-icon', {disabled})} data={Chevron} />
+            </Button>
+            {typeof error === 'string' && (
+                <div className={selectControlBlock('error')}>{error}</div>
             )}
-            {showOptionsText && (
-                <span className={selectControlBlock('option-text')}>{selectedOptionsContent}</span>
-            )}
-            <Icon className={selectControlBlock('chevron-icon')} data={Chevron} />
-        </button>
+        </React.Fragment>
     );
 });
 

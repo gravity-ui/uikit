9e3c20a4169ef6219ba9d636356b87a84ce46012
Yevhenii Chernovol <echernovol@yandex-team.ru>
2023-03-03 17:37:39 +0100

    feat(utils.useOpenState): introduce

--- src/components/utils/useSelect/types.ts
+++ src/components/utils/useSelect/types.ts
@@ -1,5 +1,12 @@
 export type UseSelectOption<T = unknown> = T & {value: string};
 
+export type UseOpenProps = {
+    defaultOpen?: boolean;
+    open?: boolean;
+    onClose?: () => void;
+    onOpenChange?: (open: boolean) => void;
+};
+
 export type UseSelectProps = {
     value?: string[];
     defaultValue?: string[];
---
+++ src/components/utils/useSelect/useOpenState.ts
@@ -0,0 +1,38 @@
+import React from 'react';
+import type {UseOpenProps} from './types';
+
+export const useOpenState = (props: UseOpenProps) => {
+    const [open, setOpenState] = React.useState(props.defaultOpen || false);
+    const isOpenControlled = React.useMemo(() => typeof props.open === 'boolean', [props.open]);
+    const {onOpenChange, onClose} = props;
+
+    React.useEffect(() => {
+        if (!isOpenControlled) return;
+
+        setOpenState(props.open as boolean);
+    }, [props.open, isOpenControlled]);
+
+    const setOpen = React.useCallback(() => {
+        if (isOpenControlled || open) return;
+        setOpenState(true);
+        onOpenChange?.(true);
+    }, [setOpenState, isOpenControlled, onOpenChange, open]);
+
+    const setClose = React.useCallback(() => {
+        if (!open) return;
+        onClose?.();
+        if (isOpenControlled) return;
+        setOpenState(false);
+        onOpenChange?.(false);
+    }, [setOpenState, onClose, onOpenChange, isOpenControlled, open]);
+
+    const toggleOpen = React.useCallback(() => {
+        if (open) {
+            setClose();
+        } else {
+            setOpen();
+        }
+    }, [setClose, open, setOpen]);
+
+    return {open, setOpen, setClose, toggleOpen};
+};

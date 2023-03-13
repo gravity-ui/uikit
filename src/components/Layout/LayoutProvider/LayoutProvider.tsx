44977c310883725e2604cf54f2e180f6d8818da3
Alexandr Isaev <aisaev188@yandex-team.ru>
2023-03-13 10:21:23 +0300

    feat: add documentation

--- src/components/Layout/LayoutProvider/LayoutProvider.tsx
+++ src/components/Layout/LayoutProvider/LayoutProvider.tsx
@@ -6,27 +6,29 @@ import {makeDefaultTheme} from '../utils/makeDefaultTheme';
 import {useCurrentActiveMediaQuery} from '../hooks/useCurrentActiveMediaQuery';
 
 interface LayoutProviderProps {
-    value?: RecursivePartial<LayoutTheme>;
+    theme?: RecursivePartial<LayoutTheme>;
     children: React.ReactNode;
 }
 
 /**
  * Provide context for layout components and current media queries.
  */
-export const LayoutProvider: React.FC<LayoutProviderProps> = React.memo(({children, value}) => {
-    const theme = makeDefaultTheme({override: value});
-    const activeMediaQuery = useCurrentActiveMediaQuery(theme.breakpoints);
+export const LayoutProvider: React.FC<LayoutProviderProps> = React.memo(
+    ({children, theme: override}) => {
+        const theme = makeDefaultTheme({override});
+        const activeMediaQuery = useCurrentActiveMediaQuery(theme.breakpoints);
 
-    return (
-        <LayoutContext.Provider
-            value={{
-                activeMediaQuery,
-                theme,
-            }}
-        >
-            {children}
-        </LayoutContext.Provider>
-    );
-});
+        return (
+            <LayoutContext.Provider
+                value={{
+                    activeMediaQuery,
+                    theme,
+                }}
+            >
+                {children}
+            </LayoutContext.Provider>
+        );
+    },
+);
 
 LayoutProvider.displayName = 'LayoutProvider';

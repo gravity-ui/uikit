6c6c62f3c4e13025f299c9019d700ce22f0bc1ec
Isaev Alexandr <IsaevAlexandr188@gmail.com>
2023-03-29 14:13:02 +0300

    fix(layout/Flex): remove wrappers around null children elements (#588)

--- src/components/layout/demo/Box/Box.tsx
+++ src/components/layout/demo/Box/Box.tsx
@@ -22,20 +22,19 @@ export const Box: React.FC<BoxProps> = ({
     ...props
 }) => {
     return (
-        <Flex {...props}>
-            <div
-                style={{
-                    padding: 5,
-                    boxSizing: 'border-box',
-                    width: w,
-                    height: h,
-                    minHeight,
-                    border: `2px dashed ${bc}`,
-                    backgroundColor: bgc,
-                }}
-            >
-                {children}
-            </div>
+        <Flex
+            {...props}
+            style={{
+                padding: 5,
+                boxSizing: 'border-box',
+                width: w,
+                height: h,
+                minHeight,
+                border: `2px dashed ${bc}`,
+                backgroundColor: bgc,
+            }}
+        >
+            {children}
         </Flex>
     );
 };

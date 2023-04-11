d5db9531ccf912ba9bb23e833af57d1ee1d16937
Flunt1k <flunt1k@yandex-team.com>
2023-04-11 15:55:38 +0200

    feat(Select): add unit test

--- src/components/Select/__tests__/Select.popup.test.tsx
+++ src/components/Select/__tests__/Select.popup.test.tsx
@@ -1,6 +1,10 @@
+import React from 'react';
+
 import {DEFAULT_OPTIONS, setup, TEST_QA} from './utils';
 import userEvent from '@testing-library/user-event';
 import {SelectQa} from '../constants';
+import {SelectRenderOption} from '../types';
+import {TextInputSize} from '../../TextInput';
 
 const onUpdate = jest.fn();
 describe('Select popup', () => {
@@ -32,4 +36,31 @@ describe('Select popup', () => {
 
         expect(popup).not.toBeNull();
     });
+
+    test.each([
+        ['s', {mobile: false, size: 's', height: 28}],
+        ['m', {mobile: false, size: 'm', height: 28}],
+        ['l', {mobile: false, size: 'l', height: 32}],
+        ['xl', {mobile: false, size: 'xl', height: 36}],
+        ['mobile', {mobile: true, size: undefined, height: 32}],
+    ])(
+        'should return correct height for option depends on size (%s)',
+        async (_type, {size, height, mobile}) => {
+            const renderOption: SelectRenderOption<any> = (_option, {itemHeight}) => {
+                console.log(itemHeight);
+                expect(itemHeight).toEqual(height);
+                return <div />;
+            };
+
+            const {getByTestId} = setup(
+                {size: size as TextInputSize, renderOption, onUpdate, options: DEFAULT_OPTIONS},
+                mobile,
+            );
+
+            const user = userEvent.setup();
+            const selectControl = getByTestId(TEST_QA);
+            // open select popup
+            await user.click(selectControl);
+        },
+    );
 });

eb47759bb41be8c3c104b93339225b2fa99e302f
Mr.Dr.Professor Patrick <alaev89@yandex-team.ru>
2023-04-14 18:33:55 +0200

    feat: add useFileInput hook (#624)

---
+++ src/components/utils/useFileInput/__stories__/UseFileInput.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Meta, Story} from '@storybook/react';
+import {Button} from '../../../Button';
+import {useFileInput} from '../useFileInput';
+
+export default {title: 'Hooks/useFileInput'} as Meta;
+
+const DefaultTemplate: Story = () => {
+    const onUpdate = React.useCallback((files: File[]) => console.log(files), []);
+    const {controlProps, triggerProps} = useFileInput({onUpdate});
+
+    return (
+        <React.Fragment>
+            <input {...controlProps} />
+            <Button {...triggerProps}>Upload</Button>
+        </React.Fragment>
+    );
+};
+
+export const Default = DefaultTemplate.bind({});

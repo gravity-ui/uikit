0b8613ec36a873449ade9974547553f4d8f2c762
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2023-04-18 19:34:56 +0200

    test: handlers prioritize

--- src/components/Persona/__tests__/Persona.test.tsx
+++ src/components/Persona/__tests__/Persona.test.tsx
@@ -21,3 +21,25 @@ test('should allow to override button', async () => {
 
     expect(onClick).toHaveBeenCalledWith('Test Data');
 });
+
+test('should ignore `onClose` if `renderButton` exist', async () => {
+    const onClick = jest.fn();
+    const onCustomClick = jest.fn();
+    render(
+        <Persona
+            text={'Test'}
+            onClick={onClick}
+            renderButton={() => (
+                <Persona.Button
+                    onClick={() => onCustomClick('Test Data')}
+                    extraProps={{'aria-label': 'Test Button'}}
+                />
+            )}
+        />,
+    );
+
+    await userEvent.click(screen.getByRole('button', {name: 'Test Button'}));
+
+    expect(onCustomClick).toHaveBeenCalledWith('Test Data');
+    expect(onClick).not.toHaveBeenCalled();
+});

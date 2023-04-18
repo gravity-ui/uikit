d568f8c2ef3059e87880f21a5d0d2712fbe6f6c6
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2023-04-18 19:30:11 +0200

    test: custom button behaviour

---
+++ src/components/Persona/__tests__/Persona.test.tsx
@@ -0,0 +1,23 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import {Persona} from '../Persona';
+
+test('should allow to override button', async () => {
+    const onClick = jest.fn();
+    render(
+        <Persona
+            text={'Test'}
+            renderButton={() => (
+                <Persona.Button
+                    onClick={() => onClick('Test Data')}
+                    extraProps={{'aria-label': 'Test Button'}}
+                />
+            )}
+        />,
+    );
+
+    await userEvent.click(screen.getByRole('button', {name: 'Test Button'}));
+
+    expect(onClick).toHaveBeenCalledWith('Test Data');
+});

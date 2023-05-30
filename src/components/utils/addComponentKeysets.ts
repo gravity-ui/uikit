da2c12fcb60d429c0e3496dfba03b23e6c7ffa66
Valerii Sidorenko <balepas@yandex-team.ru>
2023-05-30 18:45:09 +0200

    feat(18n): export addComponentKeysets (#676)

---
+++ src/components/utils/addComponentKeysets.ts
@@ -0,0 +1,11 @@
+import {i18n} from '../../i18n';
+import {Lang} from './configure';
+
+import type {KeysData} from '@gravity-ui/i18n';
+
+export function addComponentKeysets<T extends KeysData>(data: Record<Lang, T>, keysetName: string) {
+    Object.entries(data).forEach(([lang, keys]) => i18n.registerKeyset(lang, keysetName, keys));
+
+    type TKey = Extract<keyof T, string>;
+    return i18n.keyset<TKey>(keysetName);
+}

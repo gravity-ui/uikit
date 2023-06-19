931f59208edfa214065daf71536a60af0ab08be6
Andrey Morozov <amje@yandex-team.ru>
2023-06-19 17:05:21 +0300

    feat!: removed migrated components

--- src/components/StoriesGroup/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './StoriesGroup';
-export * from './types';
--- src/components/StoriesGroup/types.ts
+++
@@ -1,7 +0,0 @@
-import type {StoriesItem, StoriesItemMedia} from '../Stories/types';
-
-export interface StoriesGroupItem {
-    items: StoriesItem[];
-    /** Custom image for preview, otherwise first StoriesItem.media from StoriesItem will be used */
-    thumbnailMedia?: StoriesItemMedia;
-}

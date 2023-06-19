35bfdc100ca4e18fdceb5395f3165a9a75dcaf63
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2023-06-19 17:52:12 +0200

    test: `ResizeObserver` mock

--- test-utils/setup-tests.ts
+++ test-utils/setup-tests.ts
@@ -6,3 +6,11 @@ libConfigure({
     lang: Lang.En,
 });
 configure({testIdAttribute: 'data-qa'});
+
+global.ResizeObserver = class implements ResizeObserver {
+    // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+    constructor(_callback: ResizeObserverCallback) {}
+    disconnect() {}
+    observe(_target: Element, _options?: ResizeObserverOptions) {}
+    unobserve(_target: Element) {}
+};

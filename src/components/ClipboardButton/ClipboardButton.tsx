0508268f7b00b76ed037b17d21d3dd50d1266bda
catakot <catakot@gmail.com>
2023-06-21 13:58:36 +0300

    feat(ClipboardButton): added tooltip to clipboard button (#667)

--- src/components/ClipboardButton/ClipboardButton.tsx
+++ src/components/ClipboardButton/ClipboardButton.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 
 import {Button} from '../Button';
+import type {ButtonProps} from '../Button';
 import {ClipboardIcon} from '../ClipboardIcon';
 import {CopyToClipboard} from '../CopyToClipboard';
+import {CopyToClipboardStatus} from '../CopyToClipboard/types';
 import type {CopyToClipboardBaseProps} from '../CopyToClipboard/types';
+import {Tooltip} from '../Tooltip';
 import type {QAProps} from '../types';
 import {block} from '../utils/cn';
 
+import i18n from './i18n';
+
 import './ClipboardButton.scss';
 
-export interface ClipboardButtonProps extends CopyToClipboardBaseProps, QAProps {
+export interface ClipboardButtonProps
+    extends CopyToClipboardBaseProps,
+        Omit<ClipboardButtonComponentProps, 'status' | 'onClick'>,
+        QAProps {
+    /** Time to restore initial state, ms */
+    timeout?: number;
+}
+
+interface ClipboardButtonComponentProps extends QAProps {
     /** Icon size in pixels */
     size?: number;
     /** Element CSS class */
     className?: string;
-    /** Time to restore initial state, ms */
-    timeout?: number;
+    status: CopyToClipboardStatus;
+    onClick?: ButtonProps['onClick'];
+    /** Disable tooltip. Tooltip won't be shown */
+    hasTooltip?: boolean;
+    /** Text shown before copy */
+    tooltipInitialText?: string;
+    /** Text shown after copy */
+    tooltipSuccessText?: string;
 }
 
 const b = block('clipboard-button');
@@ -23,15 +42,17 @@ const b = block('clipboard-button');
 const DEFAULT_ICON_SIZE = 24;
 const DEFAULT_TIMEOUT = 1000;
 
-export function ClipboardButton({
-    text,
-    size = DEFAULT_ICON_SIZE,
-    className,
-    qa,
-    onCopy,
-    timeout = DEFAULT_TIMEOUT,
-    options,
-}: ClipboardButtonProps) {
+const ClipboardButtonComponent = (props: ClipboardButtonComponentProps) => {
+    const {
+        size = DEFAULT_ICON_SIZE,
+        className,
+        qa,
+        hasTooltip = true,
+        tooltipInitialText = i18n('startCopy'),
+        tooltipSuccessText = i18n('endCopy'),
+        status,
+        onClick,
+    } = props;
     const buttonRef = React.useRef<HTMLButtonElement | null>(null);
 
     React.useEffect(() => {
@@ -39,14 +60,32 @@ export function ClipboardButton({
     }, [size]);
 
     return (
+        <Tooltip
+            disabled={!hasTooltip}
+            content={
+                status === CopyToClipboardStatus.Success ? tooltipSuccessText : tooltipInitialText
+            }
+        >
+            <Button
+                ref={buttonRef}
+                view="flat"
+                className={b(null, className)}
+                qa={qa}
+                onClick={onClick}
+            >
+                <Button.Icon>
+                    <ClipboardIcon status={status} size={size} className={b('icon')} />
+                </Button.Icon>
+            </Button>
+        </Tooltip>
+    );
+};
+
+export function ClipboardButton(props: ClipboardButtonProps) {
+    const {text, timeout = DEFAULT_TIMEOUT, onCopy, options, ...buttonProps} = props;
+    return (
         <CopyToClipboard text={text} timeout={timeout} onCopy={onCopy} options={options}>
-            {(status) => (
-                <Button ref={buttonRef} view="flat" className={b(null, className)} qa={qa}>
-                    <Button.Icon>
-                        <ClipboardIcon status={status} size={size} className={b('icon')} />
-                    </Button.Icon>
-                </Button>
-            )}
+            {(status) => <ClipboardButtonComponent {...buttonProps} status={status} />}
         </CopyToClipboard>
     );
 }

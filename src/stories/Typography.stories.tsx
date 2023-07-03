c491afb48be2a662fd01b31af2dd142d46afa917
Andrey Morozov <amje@yandex-team.ru>
2023-07-03 19:55:29 +0300

    refactor: typography demo

--- src/stories/Typography.stories.tsx
+++ src/stories/Typography.stories.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import type {Meta, StoryFn} from '@storybook/react';
 
-import {Typography} from '../demo/Typography/Typography';
+import {RenderFonts} from '../demo/typography/Fonts';
+import {RenderVariants} from '../demo/typography/Variants';
 
 export default {
     title: 'Typography',
 } as Meta;
 
-export const Default: StoryFn = () => <Typography />;
-
-Default.storyName = 'Typography';
+export const Fonts: StoryFn = () => <RenderFonts />;
+export const Variants: StoryFn = () => <RenderVariants />;

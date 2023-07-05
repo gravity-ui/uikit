e23073927a3483fd3ea40a0a208208abbc37b437
Andrey Morozov <amje@yandex-team.ru>
2023-07-05 16:45:59 +0300

    refactor!: remove moved components (#795)

--- src/components/SharePopover/i18n/index.ts
+++
@@ -1,8 +0,0 @@
-import {addComponentKeysets} from '../../utils/addComponentKeysets';
-
-import en from './en.json';
-import ru from './ru.json';
-
-const COMPONENT = 'yc-share-tooltip';
-
-export default addComponentKeysets({en, ru}, COMPONENT);

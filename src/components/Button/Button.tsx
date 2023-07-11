d43b7350185c14a9db86336bdd230ba0a8ee44f8
Andrey Morozov <amje@yandex-team.ru>
2023-07-11 13:34:17 +0300

    feat(Button): added new views

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -15,12 +15,18 @@ export type ButtonView =
     | 'action' // Branded background, no border
     | 'outlined' // No background, grey border
     | 'outlined-info' // No background, with info-type border color
+    | 'outlined-success' // No background, with success-type border color
+    | 'outlined-warning' // No background, with warning-type border color
     | 'outlined-danger' // No background, with danger-type border color
+    | 'outlined-action' // No background, with branded border color
     | 'raised' // With white background and shadow
     | 'flat' // No background, no border
-    | 'flat-info' // No background, no border, info-type text color
-    | 'flat-danger' // No background, no border, danger-type text color
     | 'flat-secondary' // No background, no border, secondary-type text color
+    | 'flat-info' // No background, no border, info-type text color
+    | 'flat-success' // No background, no border, success-type text color
+    | 'flat-warning' // No background, no border, warning-type text color
+    | 'flat-danger' // No background, no border, danger-type text color
+    | 'flat-action' // No background, no border, branded text color
     | 'normal-contrast' // normal button appearance with contrast background
     | 'outlined-contrast' // outlined button appearance with contrast background
     | 'flat-contrast'; // flat button appearance with contrast background

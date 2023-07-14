513c25f71b04613c0c98e23ab62315507416103c
sunduckcow <83990793+sunduckcow@users.noreply.github.com>
2023-07-14 13:49:44 +0300

    docs(Button): update docs (#800)
    Co-authored-by: sunduckcow <sunduckcow@yandex-team.ru>

--- src/components/Button/__stories__/examples/ButtonExampleView/ButtonExampleView.tsx
+++ src/components/Button/__stories__/examples/ButtonExampleView/ButtonExampleView.tsx
@@ -1,200 +1,76 @@
 import React from 'react';
 
+import {Gear} from '@gravity-ui/icons';
+
 import {Button} from '../../..';
 import {DocsExample} from '../../../../../demo/DocsExample/DocsExample';
 import {Icon} from '../../../../Icon';
-import {GearIcon} from '../../../../icons';
 
-export function ButtonExampleViewAction() {
+export function ButtonExampleViewRegular() {
     return (
-        <DocsExample>
-            <Button view="action" size="l">
-                Create
+        <DocsExample gap="l" space="l">
+            <Button view="normal" size="l">
+                Normal
             </Button>
             <Button view="action" size="l">
-                Connect
-                {/* TODO ConnectIcon */}
-                <Icon data={GearIcon} size={18} />
+                Action
             </Button>
-            <Button view="action" size="l">
-                {/* TODO EditIcon */}
-                <Icon data={GearIcon} size={18} />
+            <Button view="raised" size="l">
+                Raised
             </Button>
         </DocsExample>
     );
 }
-ButtonExampleViewAction.code = `
-<Button view="action" size="l">Create</Button>
-<Button view="action" size="l">
-    Connect
-    <Icon data={GearIcon} size={18} />
-</Button>
-<Button view="action" size="l">
-    <Icon data={GearIcon} size={18} />
-</Button>
-`.trim();
-
-export function ButtonExampleViewNormal() {
-    return (
-        <DocsExample>
-            <Button view="normal" size="l">
-                Add
-            </Button>
-            <Button view="normal" size="l">
-                Start
-                {/* TODO RunIcon */}
-                <Icon data={GearIcon} size={18} />
-            </Button>
-            <Button view="normal" size="l">
-                {/* TODO RefreshIcon */}
-                <Icon data={GearIcon} size={18} />
-            </Button>
-        </DocsExample>
-    );
-}
-ButtonExampleViewNormal.code = `
-<Button view="normal" size="l">Add</Button>
-<Button view="normal" size="l">
-    Start
-    <Icon data={GearIcon} size={18} />
-</Button>
-<Button view="normal" size="l">
-    <Icon data={GearIcon} size={18} />
-</Button>
+ButtonExampleViewRegular.code = `
+<Button view="normal" size="l">Normal</Button>
+<Button view="action" size="l">Action</Button>
+<Button view="raised" size="l">Raised</Button>
 `.trim();
 
 export function ButtonExampleViewOutlined() {
     return (
         <DocsExample>
             <Button view="outlined" size="l">
-                Cancel
+                Outlined
             </Button>
-            <Button view="outlined" size="l">
-                Attach
-                {/* TODO AttachIcon */}
-                <Icon data={GearIcon} size={18} />
+            <Button view="outlined-info" size="l">
+                Outlined info
             </Button>
-            <Button view="outlined" size="l">
-                {/* TODO CopyIcon */}
-                <Icon data={GearIcon} size={18} />
+            <Button view="outlined-danger" size="l">
+                Outlined danger
             </Button>
         </DocsExample>
     );
 }
 ButtonExampleViewOutlined.code = `
-<Button view="outlined" size="l">Cancel</Button>
-<Button view="outlined" size="l">
-    Attach
-    <Icon data={GearIcon} size={18} />
-</Button>
-<Button view="outlined" size="l">
-    <Icon data={GearIcon} size={18} />
-</Button>
+<Button view="outlined" size="l">Outlined</Button>
+<Button view="outlined-info" size="l">Outlined info</Button>
+<Button view="outlined-danger" size="l">Outlined danger</Button>
 `.trim();
 
 export function ButtonExampleViewFlat() {
     return (
         <DocsExample>
             <Button view="flat" size="l">
-                Expand
+                Flat
             </Button>
-            <Button view="flat" size="l">
-                {/* TODO CloseIcon */}
-                <Icon data={GearIcon} size={18} />
+            <Button view="flat-info" size="l">
+                Flat info
+            </Button>
+            <Button view="flat-danger" size="l">
+                Flat danger
+            </Button>
+            <Button view="flat-secondary" size="l">
+                Flat secondary
             </Button>
         </DocsExample>
     );
 }
 ButtonExampleViewFlat.code = `
-<Button view="flat" size="l">Expand</Button>
-<Button view="flat" size="l">
-    <Icon data={GearIcon} size={18} />
-</Button>
-`.trim();
-
-export function ButtonExampleViewRaised() {
-    return (
-        <DocsExample>
-            <Button view="raised" size="l">
-                Create
-            </Button>
-            <Button view="raised" size="l">
-                {/* TODO PlusIcon */}
-                <Icon data={GearIcon} size={18} />
-            </Button>
-        </DocsExample>
-    );
-}
-ButtonExampleViewRaised.code = `
-<Button view="raised" size="l">Create</Button>
-<Button view="raised" size="l">
-    <Icon data={GearIcon} size={18} />
-</Button>
-`.trim();
-
-export function ButtonExampleViewOutlinedInfo() {
-    return (
-        <DocsExample>
-            <Button view="outlined-info" size="l">
-                Go
-            </Button>
-        </DocsExample>
-    );
-}
-ButtonExampleViewOutlinedInfo.code = `
-<Button view="outlined-info" size="l">Go</Button>
-`.trim();
-
-export function ButtonExampleViewFlatInfo() {
-    return (
-        <DocsExample>
-            <Button view="flat-info" size="l">
-                Go
-            </Button>
-        </DocsExample>
-    );
-}
-ButtonExampleViewFlatInfo.code = `
-<Button view="flat-info" size="l">Go</Button>
-`.trim();
-
-export function ButtonExampleViewOutlinedDanger() {
-    return (
-        <DocsExample>
-            <Button view="outlined-danger" size="l">
-                Stop
-            </Button>
-        </DocsExample>
-    );
-}
-ButtonExampleViewOutlinedDanger.code = `
-<Button view="outlined-danger" size="l">Stop</Button>
-`.trim();
-
-export function ButtonExampleViewFlatDanger() {
-    return (
-        <DocsExample>
-            <Button view="flat-danger" size="l">
-                Delete
-            </Button>
-        </DocsExample>
-    );
-}
-ButtonExampleViewFlatDanger.code = `
-<Button view="flat-danger" size="l">Delete</Button>
-`.trim();
-
-export function ButtonExampleViewFlatSecondary() {
-    return (
-        <DocsExample>
-            <Button view="flat-secondary" size="l">
-                Cancel
-            </Button>
-        </DocsExample>
-    );
-}
-ButtonExampleViewFlatSecondary.code = `
-<Button view="flat-secondary" size="l">Cancel</Button>
+<Button view="flat" size="l">Flat</Button>
+<Button view="flat-info" size="l">Flat info</Button>
+<Button view="flat-danger" size="l">Flat danger</Button>
+<Button view="flat-secondary" size="l">Flat secondary</Button>
 `.trim();
 
 export function ButtonExampleViewSpecial() {
@@ -218,6 +94,50 @@ ButtonExampleViewSpecial.code = `
 <Button view="flat-contrast" size="l">Flat</Button>
 `.trim();
 
+export function ButtonExampleIcons() {
+    return (
+        <DocsExample gap="l" space="l">
+            <Button view="outlined" size="l">
+                <Icon data={Gear} size={18} />
+                Left
+            </Button>
+            <Button view="outlined" size="l">
+                Right
+                <Icon data={Gear} size={18} />
+            </Button>
+            <Button view="outlined" size="l">
+                <Icon data={Gear} size={18} />
+                Both
+                <Icon data={Gear} size={18} />
+            </Button>
+            <div style={{display: 'flex', gap: '16px', alignItems: 'center'}}>
+                No text:
+                <Button view="outlined" size="l">
+                    <Icon data={Gear} size={18} />
+                </Button>
+            </div>
+        </DocsExample>
+    );
+}
+ButtonExampleIcons.code = `
+<Button view="outlined" size="l">
+    <Icon data={Gear} size={18} />
+    Left
+</Button>
+<Button view="outlined" size="l">
+    Right
+    <Icon data={Gear} size={18} />
+</Button>
+<Button view="outlined" size="l">
+    <Icon data={Gear} size={18} />
+    Both
+    <Icon data={Gear} size={18} />
+</Button>
+<Button view="outlined" size="l">
+    <Icon data={Gear} size={18} />
+</Button>
+`.trim();
+
 export function ButtonExampleState() {
     return (
         <DocsExample gap="l" space="l">
@@ -234,6 +154,12 @@ export function ButtonExampleState() {
         </DocsExample>
     );
 }
+ButtonExampleState.code = `
+<Button size="l">Default</Button>
+<Button size="l" disabled>Disabled</Button>
+<Button size="l" loading>Loading</Button>
+<Button size="l" selected>Selected</Button>
+`.trim();
 
 export function ButtonExampleSize() {
     return (
@@ -246,3 +172,135 @@ export function ButtonExampleSize() {
         </DocsExample>
     );
 }
+ButtonExampleSize.code = `
+<Button size="xs">XS-size</Button>
+<Button size="s">S-size</Button>
+<Button size="m">M-size</Button>
+<Button size="l">L-size</Button>
+<Button size="xl">XL-size</Button>
+`.trim();
+
+export function ButtonExampleWidth() {
+    return (
+        <DocsExample gap="l" space="l">
+            <div style={{width: 100, border: '2px dashed gray'}}>
+                <p>
+                    <Button>none</Button>
+                </p>
+                <p>
+                    <Button>none none none</Button>
+                </p>
+                <p>
+                    <Button width="auto">auto</Button>
+                </p>
+                <p>
+                    <Button width="auto">auto auto auto</Button>
+                </p>
+                <p>
+                    <Button width="max">max</Button>
+                </p>
+                <p>
+                    <Button width="max">max max max</Button>
+                </p>
+            </div>
+        </DocsExample>
+    );
+}
+ButtonExampleWidth.code = `
+<Button>none</Button>
+<Button>none none none</Button>
+<Button width="auto">auto</Button>
+<Button width="auto">auto auto auto</Button>
+<Button width="max">max</Button>
+<Button width="max">max max max</Button>
+`.trim();
+
+export function ButtonExampleEachPin() {
+    return (
+        <DocsExample gap="l" space="l">
+            <Button view="action" pin="brick-brick">
+                brick-brick
+            </Button>
+            <Button view="action" pin="brick-round">
+                brick-round
+            </Button>
+            <Button view="action" pin="brick-circle">
+                brick-circle
+            </Button>
+            <Button view="outlined-info" pin="brick-clear">
+                brick-clear
+            </Button>
+        </DocsExample>
+    );
+}
+ButtonExampleEachPin.code = `
+<Button view="action" pin="brick-brick">
+    brick-brick  
+</Button>
+<Button view="action" pin="brick-round">
+brick-round
+</Button>
+<Button view="action" pin="brick-circle">
+brick-circle
+</Button>
+<Button view="outlined-info" pin="brick-clear">
+brick-clear
+</Button>
+`.trim();
+
+export function ButtonExampleCombinedPins() {
+    return (
+        <DocsExample gap="l" space="l">
+            <div>
+                <Button view="outlined-danger" pin="circle-clear">
+                    right
+                </Button>
+                <Button view="outlined-danger" pin="clear-circle">
+                    left
+                </Button>
+            </div>
+            <div>
+                <Button view="outlined-info" pin="round-clear">
+                    right
+                </Button>
+                <Button view="action" pin="brick-brick">
+                    center
+                </Button>
+                <Button view="outlined-info" pin="clear-round">
+                    left
+                </Button>
+            </div>
+            <div>
+                <Button view="outlined" pin="brick-clear">
+                    1
+                </Button>
+                <Button view="outlined" pin="clear-clear">
+                    2
+                </Button>
+                <Button view="outlined" pin="clear-clear">
+                    3
+                </Button>
+                <Button view="outlined" pin="clear-brick">
+                    4+
+                </Button>
+            </div>
+        </DocsExample>
+    );
+}
+ButtonExampleCombinedPins.code = `
+<div>
+    <Button view="outlined-danger" pin="circle-clear">right</Button>
+    <Button view="outlined-danger" pin="clear-circle">left</Button>
+</div>
+<div>
+    <Button view="outlined-info" pin="round-clear">right</Button>
+    <Button view="action" pin="brick-brick">center</Button>
+    <Button view="outlined-info" pin="clear-round">left</Button>
+</div>
+<div>
+    <Button view="outlined" pin="brick-clear">1</Button>
+    <Button view="outlined" pin="clear-clear">2</Button>
+    <Button view="outlined" pin="clear-clear">3</Button>
+    <Button view="outlined" pin="clear-brick">4+</Button>
+</div>
+`.trim();

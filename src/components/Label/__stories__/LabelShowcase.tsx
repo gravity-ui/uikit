695c7eecfebd2c99ba6cbe39f1179a94a14211f0
Georgy Malkov <malkovgv@yandex-team.ru>
2023-07-14 10:08:21 +0300

    docs(Label): update component description (#806)

---
+++ src/components/Label/__stories__/LabelShowcase.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import block from 'bem-cn-lite';
+
+import {Icon as IconComponent} from '../../Icon';
+import {GearIcon} from '../../icons/GearIcon';
+import {Tick as TickIcon} from '../../icons/Tick';
+import {Label} from '../Label';
+import type {LabelProps} from '../Label';
+
+import './LabelShowcase.scss';
+
+const b = block('label-showcase');
+type WithKey<T> = T & {key: React.Key};
+
+const icons = {
+    '-': undefined,
+    TickIcon: <IconComponent size={12} data={TickIcon} />,
+    GearIcon: <IconComponent size={12} data={GearIcon} />,
+};
+
+export function LabelShowcase(args: LabelProps) {
+    const themes = ['normal', 'info', 'danger', 'warning', 'success', 'unknown', 'clear'] as const;
+    const sizes = ['xs', 's', 'm'] as const;
+
+    const getLabel = ({...props}: WithKey<LabelProps>) => <Label {...props} />;
+
+    const section = (props: LabelProps) => {
+        const cases: LabelProps[] = [
+            {children: 'Label', icon: icons['TickIcon'], type: 'default'},
+            {children: 'Label', type: 'default'},
+            {children: 'Label', icon: icons['TickIcon'], type: 'copy'},
+            {children: 'Label', type: 'copy'},
+            {children: 'Label', icon: icons['TickIcon'], type: 'close'},
+            {children: 'Label', type: 'close'},
+            {children: 'Key', value: 'Value', icon: icons['TickIcon'], type: 'default'},
+            {children: 'Key', value: 'Value', type: 'default'},
+            {children: 'Key', value: 'Value', icon: icons['TickIcon'], type: 'copy'},
+            {children: 'Key', value: 'Value', type: 'copy'},
+            {children: 'Key', value: 'Value', icon: icons['TickIcon'], type: 'close'},
+            {children: 'Key', value: 'Value', type: 'close'},
+            {icon: icons['TickIcon']},
+        ];
+        return cases.map((label, i) => getLabel({key: i, ...props, ...label}));
+    };
+
+    return (
+        <div className={b()}>
+            <div
+                className="grid"
+                style={{gridTemplateColumns: `repeat(${1 + themes.length}, 1fr)`}}
+            >
+                <div></div>
+                {themes.map((theme) => (
+                    <h1 key={`${theme}-header`}>{theme}</h1>
+                ))}
+                {sizes.map((size) => (
+                    <React.Fragment key={size}>
+                        <h1>{size}</h1>
+                        {themes.map((theme) => (
+                            <div key={theme} className="section">
+                                {section({theme, size, ...args})}
+                            </div>
+                        ))}
+                    </React.Fragment>
+                ))}
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Label/__stories__/examples/LabelExampleView/LabelExampleView.tsx
@@ -0,0 +1,158 @@
+import React from 'react';
+
+import {Unstyled} from '@storybook/blocks';
+
+import {Label} from '../../..';
+import {DocsExample} from '../../../../../demo/DocsExample/DocsExample';
+import {Icon} from '../../../../Icon';
+import {GearIcon} from '../../../../icons';
+
+export function LabelExampleViewTheme() {
+    return (
+        <Unstyled>
+            <DocsExample>
+                <Label theme="normal" size="s">
+                    Normal
+                </Label>
+                <Label theme="info" size="s">
+                    Info
+                </Label>
+                <Label theme="warning" size="s">
+                    Warning
+                </Label>
+                <Label theme="danger" size="s">
+                    Danger
+                </Label>
+                <Label theme="success" size="s">
+                    Success
+                </Label>
+                <Label theme="unknown" size="s">
+                    Unknown
+                </Label>
+                <Label theme="clear" size="s">
+                    Clear
+                </Label>
+            </DocsExample>
+        </Unstyled>
+    );
+}
+LabelExampleViewTheme.code = `
+<Label theme="normal" size="s">Normal</Label>
+<Label theme="info" size="s">Info</Label>
+<Label theme="warning" size="s">Warning</Label>
+<Label theme="danger" size="s">Danger</Label>
+<Label theme="success" size="s">Success</Label>
+<Label theme="unknown" size="s">Unknown</Label>
+<Label theme="clear" size="s">Clear</Label>
+`.trim();
+
+export function LabelExampleViewType() {
+    return (
+        <Unstyled>
+            <DocsExample>
+                <Label type="default" onClick={() => alert('On click label')} size="s">
+                    Click on label
+                </Label>
+                <Label type="close" onClose={() => alert('On click close')} size="s">
+                    Click on close
+                </Label>
+                <Label type="copy" copyText="Copy" onCopy={() => alert('On click copy')} size="s">
+                    Click on copy
+                </Label>
+            </DocsExample>
+        </Unstyled>
+    );
+}
+LabelExampleViewType.code = `
+<Label type="default" onClick={() => alert('On click label')} size="s">Click on label</Label>
+<Label type="close" onClose={() => alert('On click close')} size="s">Click on close</Label>
+<Label type="copy" copyText="Copy" onCopy={() => alert('On click copy')}  size="s">Click on copy</Label>
+`.trim();
+
+export function LabelExampleViewIcon() {
+    return (
+        <Unstyled>
+            <DocsExample>
+                <Label size="s" icon={<Icon size={16} data={GearIcon} />}>
+                    Default with Icon
+                </Label>
+                <Label theme="info" size="s" icon={<Icon size={16} data={GearIcon} />}>
+                    Info with icon
+                </Label>
+                <Label type="close" size="s" icon={<Icon size={16} data={GearIcon} />}>
+                    Close action and icon
+                </Label>
+                <Label size="m" icon={<Icon size={18} data={GearIcon} />}>
+                    M-size icon
+                </Label>
+            </DocsExample>
+        </Unstyled>
+    );
+}
+LabelExampleViewIcon.code = `
+<Label size="s" icon={<Icon size={16} data={GearIcon} />}>Default with icon</Label>
+<Label theme="info" size="s" icon={<Icon size={16} data={GearIcon} />}>Info with icon</Label>
+<Label type="close" size="s" icon={<Icon size={16} data={GearIcon} />}>Close action and icon</Label>
+<Label size="m" icon={<Icon data={GearIcon} />}>M-size icon</Label>
+`.trim();
+
+export function LabelExampleValue() {
+    return (
+        <Unstyled>
+            <DocsExample>
+                <Label size="xs" value="Value">
+                    Key
+                </Label>
+                <Label size="s" value="Value">
+                    Key
+                </Label>
+                <Label size="m" value="Value">
+                    Key
+                </Label>
+            </DocsExample>
+        </Unstyled>
+    );
+}
+LabelExampleValue.code = `
+<Label size="xs" value="Value">Key</Label>
+<Label size="s" value="Value">Key</Label>
+<Label size="m" value="Value">Key</Label>
+`.trim();
+
+export function LabelExampleState() {
+    return (
+        <Unstyled>
+            <DocsExample>
+                <Label size="s">Default</Label>
+                <Label size="s" disabled>
+                    Disabled
+                </Label>
+                <Label size="s" interactive>
+                    Interactive
+                </Label>
+            </DocsExample>
+        </Unstyled>
+    );
+}
+LabelExampleState.code = `
+<Label size="s">Default</Label>
+<Label size="s" disabled>Disabled</Label>
+<Label size="s" interactive>Interactive</Label>
+`.trim();
+
+export function LabelExampleSize() {
+    return (
+        <Unstyled>
+            <DocsExample>
+                <Label size="xs">XS-size</Label>
+                <Label size="s">S-size</Label>
+                <Label size="m">M-size</Label>
+            </DocsExample>
+        </Unstyled>
+    );
+}
+LabelExampleSize.code = `
+<Label size="xs">XS-size</Label>
+<Label size="s">S-size</Label>
+<Label size="m">M-size</Label>
+`.trim();

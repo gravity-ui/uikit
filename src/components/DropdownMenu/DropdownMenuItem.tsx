4519ea5b3baa3a0e665a8b477e73ef8ea5cb44a3
zzman <43282255+GermanVor@users.noreply.github.com>
2023-07-24 15:39:27 +0200

    fix: add menu-item-content-children class name (#839)

--- src/components/DropdownMenu/DropdownMenuItem.tsx
+++ src/components/DropdownMenu/DropdownMenuItem.tsx
@@ -101,7 +101,9 @@ export const DropdownMenuItem = <T,>({
                 onClick={handleMenuItemClick}
             >
                 <div className={cnDropdownMenu('menu-item-content')}>
-                    <div>{text || children}</div>
+                    <div className={cnDropdownMenu('menu-item-content-children')}>
+                        {text || children}
+                    </div>
                     {hasSubmenu && <Icon data={ChevronRight} size={10} />}
                 </div>
             </Menu.Item>

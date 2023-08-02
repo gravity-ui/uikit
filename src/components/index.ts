1ee8f3ca925f18e10f3eb0f7cd9c751791a240f8
zamkovskaya <128601366+zamkovskaya@users.noreply.github.com>
2023-08-02 11:49:28 +0200

    feat: export hook useActionHandlers (#836)

--- src/components/index.ts
+++ src/components/index.ts
@@ -65,4 +65,5 @@ export {useOnFocusOutside} from './utils/useOnFocusOutside';
 export * from './utils/interactions';
 export * from './utils/xpath';
 export * from './utils/useFileInput/useFileInput';
+export {useActionHandlers} from './utils/useActionHandlers';
 export {getLayersCount} from './utils/LayerManager';

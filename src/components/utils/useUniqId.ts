7c760696c70a464975ff92add0fbc08e0f86d782
Valeriy Sidorenko <balepas@yandex-team.ru>
2023-08-10 12:15:25 +0200

    feat: use useId from React 18 to generate uniq ids

--- src/components/utils/useUniqId.ts
+++ src/components/utils/useUniqId.ts
@@ -2,7 +2,13 @@ import React from 'react';
 
 import {getUniqId} from './common';
 
-export function useUniqId() {
-    const idRef = React.useRef(getUniqId());
+function useUniqIdFallback() {
+    const idRef = React.useRef<string>();
+    if (idRef.current === undefined) {
+        idRef.current = getUniqId();
+    }
     return idRef.current;
 }
+
+export const useUniqId: () => string =
+    typeof React.useId === 'function' ? React.useId : useUniqIdFallback;

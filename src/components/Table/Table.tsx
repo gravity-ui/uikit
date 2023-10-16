07e0f1ca98f544e5a2021f0005d1f133e42eb686
Bluepuper <razhev2017@gmail.com>
2023-10-16 16:28:42 +0300

    feat: remove 'th-content' styles

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -152,8 +152,7 @@ export class Table<I extends TableDataItem = Record<string, string>> extends Rea
             content = id;
         }
 
-        if (id === '_selection') return content;
-        return <span className={b('th-content')}>{content}</span>;
+        return content;
     }
 
     static getBodyCellContent<I extends TableDataItem>(

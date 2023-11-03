0b16b12cf190d5393db3e42113ec3f8be8273742
Andrey Morozov <amje@yandex-team.ru>
2023-11-03 19:46:35 +0300

    feat(Button)!: add CSS API (#1066)

--- src/components/controls/TextInput/TextInput.tsx
+++ src/components/controls/TextInput/TextInput.tsx
@@ -235,7 +235,7 @@ export const TextInput = React.forwardRef<HTMLSpanElement, TextInputProps>(funct
                     <ClearButton
                         size={mapTextInputSizeToButtonSize(size)}
                         onClick={handleClear}
-                        className={b('clear')}
+                        className={b('clear', {size})}
                     />
                 )}
                 {isRightContentVisible && (

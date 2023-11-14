52de79ce4d944192b7c878786058d69267cb61ca
kseniyakuzina <kseniyakuzina@yandex-team.ru>
2023-11-14 17:12:00 +0300

    feat(useAsyncActionHandler): add useAsyncActionHandler hook

---
+++ src/hooks/useAsyncActionHandler/__stories__/ProgressButton.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {Button, ButtonProps} from '../../../components';
+import {useAsyncActionHandler} from '../useAsyncActionHandler';
+
+export type ProgressButtonProps = {
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => PromiseLike<unknown>;
+} & Exclude<ButtonProps, 'onClick'>;
+
+export const ProgressButton = (props: ProgressButtonProps) => {
+    const {onClick, loading: providedIsLoading} = props;
+
+    const {isLoading, handler: handleClick} = useAsyncActionHandler({handler: onClick});
+
+    return <Button {...props} onClick={handleClick} loading={isLoading || providedIsLoading} />;
+};
---
+++ src/hooks/useAsyncActionHandler/__stories__/UseAsyncActionHandlerDemo.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {ProgressButton} from './ProgressButton';
+import {cnUseAsyncActionHandlerDemo} from './UseAsyncActionHandlerDemo.classname';
+
+import './UseAsyncActionHandlerDemo.scss';
+
+export const UseAsyncActionHandlerDemo = () => {
+    const [items, setItems] = React.useState<string[]>([]);
+
+    const handleLoadItems = React.useCallback(async () => {
+        const loadItems = () =>
+            new Promise<string[]>((resolve) => {
+                setTimeout(() => {
+                    resolve(
+                        Array.from(
+                            {length: 10},
+                            (_item, index) =>
+                                `Item ${Math.round(Math.random() * 100 * (index + 1))}`,
+                        ),
+                    );
+                }, 1000);
+            });
+
+        try {
+            setItems(await loadItems());
+        } catch (error) {
+            console.error(error);
+        }
+    }, []);
+
+    return (
+        <div className={cnUseAsyncActionHandlerDemo()}>
+            <ProgressButton onClick={handleLoadItems}>Load items</ProgressButton>
+            <ul>
+                {items.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};

66a25b3b1c6241c10fa0db24c19dd9911cb51214
Ксения <31247233+kseniya57@users.noreply.github.com>
2023-11-14 20:30:40 +0300

    feat(useAsyncActionHandler): add useAsyncActionHandler hook (#1095)
    Co-authored-by: kseniyakuzina <kseniyakuzina@yandex-team.ru>

---
+++ src/hooks/useAsyncActionHandler/useAsyncActionHandler.ts
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type AnyAsyncAction = (...args: any[]) => PromiseLike<any>;
+
+export interface UseAsyncActionHandlerProps<Action extends AnyAsyncAction> {
+    handler: Action;
+}
+
+export interface UseAsyncActionHandlerResult<Action extends AnyAsyncAction> {
+    isLoading: boolean;
+    handler: (...args: Parameters<Action>) => Promise<Awaited<ReturnType<Action>>>;
+}
+
+export function useAsyncActionHandler<Action extends AnyAsyncAction>({
+    handler,
+}: UseAsyncActionHandlerProps<Action>): UseAsyncActionHandlerResult<Action> {
+    const [isLoading, setLoading] = React.useState(false);
+
+    const handleAction = React.useCallback<UseAsyncActionHandlerResult<Action>['handler']>(
+        async (...args) => {
+            setLoading(true);
+
+            try {
+                return await handler(...args);
+            } finally {
+                setLoading(false);
+            }
+        },
+        [handler],
+    );
+
+    return {
+        isLoading,
+        handler: handleAction,
+    };
+}

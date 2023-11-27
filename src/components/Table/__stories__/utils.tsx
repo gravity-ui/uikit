c6931bd0fbd2f08ce2b91ff43fdb7d1b040a53b4
Hellen <el-makarova@yandex-team.ru>
2023-11-27 18:49:12 +0300

    feat(Table): support rtl (#1152)

--- src/components/Table/__stories__/utils.tsx
+++ src/components/Table/__stories__/utils.tsx
@@ -81,7 +81,7 @@ export const columns: TableColumnConfig<DataItem>[] = [
     {
         id: 'count',
         name: 'Count',
-        align: 'right',
+        align: 'end',
     },
     {
         id: 'date',

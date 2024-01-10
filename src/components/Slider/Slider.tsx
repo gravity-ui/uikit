c8e298e4006812e0ee3690b2063d80258f6189bc
Kirill Revenkov <revenkov-k@yandex-team.ru>
2024-01-10 23:06:20 +0300

    feat: add Slider component based on BaseSlider

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+import debounce from 'lodash/debounce';
+
+import {blockNew} from '../utils/cn';
+
+import {BaseSlider} from './BaseSlider/BaseSlider';
+import type {BaseSliderRefType, RcSliderValueType, SliderProps, SliderValue} from './sliderTypes';
+import {getInnerState} from './utils';
+
+import './Slider.scss';
+
+const b = blockNew('slider');
+
+export const Slider = React.forwardRef(function Slider(
+    {
+        value,
+        defaultValue = 0,
+        size = 'm',
+        min = 0,
+        max = 100,
+        step = 1,
+        infoPointCount = 0,
+        availableValues,
+        error = false,
+        disabled = false,
+        keyboard = true,
+        debounceDelay = 0,
+        onBlur,
+        onChange,
+        onChangeComplete,
+        onFocus,
+        autoFocus = false,
+        tabIndex,
+        className,
+        qa,
+    }: SliderProps,
+    ref: React.ForwardedRef<BaseSliderRefType>,
+) {
+    const baseSliderRef = React.useRef<BaseSliderRefType>(null);
+
+    React.useImperativeHandle(ref, () => ({
+        focus: () => {
+            baseSliderRef.current?.focus();
+        },
+        blur: () => {
+            baseSliderRef.current?.blur();
+        },
+    }));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const handleChange = React.useCallback(
+        debounce(
+            (changedValue: RcSliderValueType) => onChange?.(changedValue as SliderValue),
+            debounceDelay,
+        ),
+        [onChange, debounceDelay],
+    );
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const handleChangeComplete = React.useCallback(
+        debounce(
+            (changedValue: RcSliderValueType) => onChangeComplete?.(changedValue as SliderValue),
+            debounceDelay,
+        ),
+        [onChangeComplete, debounceDelay],
+    );
+
+    React.useEffect(() => {
+        return () => {
+            handleChange.cancel();
+            handleChangeComplete.cancel();
+        };
+    }, [handleChange, handleChangeComplete]);
+
+    const innerState = getInnerState({
+        availableValues,
+        defaultValue,
+        infoPointCount,
+        max,
+        min,
+        step,
+        value,
+    });
+    const styleModifiers = {size, error: error && !disabled, disabled};
+
+    return (
+        <div className={b(null, className)}>
+            <BaseSlider
+                ref={baseSliderRef}
+                value={innerState.value}
+                defaultValue={innerState.defaultValue}
+                min={innerState.min}
+                max={innerState.max}
+                step={innerState.step}
+                range={innerState.range}
+                disabled={disabled}
+                keyboard={keyboard}
+                marks={innerState.marks}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                onChange={handleChange}
+                onChangeComplete={handleChangeComplete}
+                styleModifiers={styleModifiers}
+                // eslint-disable-next-line jsx-a11y/no-autofocus
+                autoFocus={autoFocus}
+                tabIndex={tabIndex}
+                data-qa={qa}
+                pushable={false}
+            ></BaseSlider>
+        </div>
+    );
+});

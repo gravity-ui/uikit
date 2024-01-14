8fb5e9ca0d91e0790b1785ddc994527eb41f20c6
Kirill Revenkov <revenkov-k@yandex-team.ru>
2024-01-14 21:50:48 +0300

    feat: storibook - default template

--- src/components/Slider/__stories__/Slider.stories.tsx
+++ src/components/Slider/__stories__/Slider.stories.tsx
@@ -6,22 +6,9 @@ import {Slider} from '../Slider';
 import type {SliderProps} from '../sliderTypes';
 
 export default {
-    title: 'Components/Slider',
+    title: 'Components/Inputs/Slider',
     component: Slider,
 } as Meta;
 
-const DefaultTemplate: StoryFn<SliderProps> = (args) => <Slider {...args} onChange={console.log} />;
+const DefaultTemplate: StoryFn<SliderProps> = (args) => <Slider {...args} />;
 export const Default = DefaultTemplate.bind({});
-
-const SizeTemplate: StoryFn<SliderProps> = (args) => (
-    <div>
-        s: <Slider {...args} size="s" />
-        <span style={{margin: '16px'}} />
-        m: <Slider {...args} size="m" />
-        <span style={{margin: '16px'}} />
-        l: <Slider {...args} size="l" />
-        <span style={{margin: '16px'}} />
-        xl: <Slider {...args} size="xl" />
-    </div>
-);
-export const Size = SizeTemplate.bind({});

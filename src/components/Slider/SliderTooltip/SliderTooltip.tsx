bbb4d7f63a0276b5f8b28e2384b978ee9c12d5eb
Kirill Revenkov <revenkov-k@yandex-team.ru>
2024-01-14 21:42:43 +0300

    feat: make tooltip non-interactive

--- src/components/Slider/SliderTooltip/SliderTooltip.tsx
+++ src/components/Slider/SliderTooltip/SliderTooltip.tsx
@@ -16,11 +16,25 @@ type SliderTooltipProps = {
     styleModifiers: Omit<StyleModifiers, 'withTooltip'>;
 };
 
-export const SliderTooltip = ({value, className, style, styleModifiers}: SliderTooltipProps) => (
-    <div className={b(styleModifiers, className)} style={style}>
-        <div className={b('card', styleModifiers)}>
-            {value}
-            <SliderTooltipPin className={b('pin')} />
+export const SliderTooltip = ({value, className, style, styleModifiers}: SliderTooltipProps) => {
+    const preventAction = React.useCallback((e: React.SyntheticEvent) => {
+        //make tooltip non-interactive
+        e.preventDefault();
+        e.stopPropagation();
+    }, []);
+    return (
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+        <div
+            className={b(styleModifiers, className)}
+            style={style}
+            onClick={preventAction}
+            onMouseDown={preventAction}
+            onTouchStart={preventAction}
+        >
+            <div className={b('card', styleModifiers)}>
+                {value}
+                <SliderTooltipPin className={b('pin')} />
+            </div>
         </div>
-    </div>
-);
+    );
+};

d17816dbffd61d9eba067efbcdfcbd620c718e6b
Viktor Rozaev <vrozaev@nebius.com>
2024-01-19 10:42:26 +0100

    feat(Disclosure): added qa attribute for details and summary nodes

---
+++ src/components/Disclosure/constants.ts
@@ -0,0 +1,8 @@
+import {blockNew} from '../utils/cn';
+
+export const b = blockNew('disclosure');
+
+export const DisclosureQa = {
+    SUMMARY: 'disclosure-summary',
+    DETAILS: 'disclosure-details',
+};

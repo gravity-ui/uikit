37d5836794479ccdbc9f3a93d5f2469845b8637f
Aleksey Troynikov <atroynikov@yandex-team.ru>
2024-01-23 18:16:32 +0300

    refactor(CopyToClipboard): replaced class component by functional component (#1216)

--- src/components/CopyToClipboard/types.ts
+++ src/components/CopyToClipboard/types.ts
@@ -1,3 +1,5 @@
+import type React from 'react';
+
 import type ReactCopyToClipboard from 'react-copy-to-clipboard';
 
 export type CopyToClipboardStatus = 'pending' | 'success' | 'error';
@@ -6,13 +8,10 @@ export type OnCopyHandler = (text: string, result: boolean) => void;
 
 export type CopyToClipboardContent = (status: CopyToClipboardStatus) => React.ReactElement;
 
-export interface CopyToClipboardBaseProps {
-    /** Text to copy */
+export interface CopyToClipboardProps {
     text: string;
-
-    /** Handler that would be invoked after copy to clipboard */
+    timeout?: number;
+    children: CopyToClipboardContent;
     onCopy?: OnCopyHandler;
-
-    /** copy-to-clipboard options */
     options?: ReactCopyToClipboard.Options;
 }

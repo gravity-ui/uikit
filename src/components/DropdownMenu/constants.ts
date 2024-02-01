304054d5e81a45d865da3fe41a697da9cc6c279f
Andrey Morozov <amje@yandex-team.ru>
2024-02-01 22:54:26 +0300

    fix(DropdownMenu): fix rtl behaviour

--- src/components/DropdownMenu/constants.ts
+++ src/components/DropdownMenu/constants.ts
@@ -1,7 +1,3 @@
-import type {PopperPlacement} from '../../hooks/private';
-
 import type {DropdownMenuListItem} from './types';
-
-export const subMenuPlacement: PopperPlacement = ['right-start', 'left-start'];
 
 export const dropdownMenuSeparator: DropdownMenuListItem = {text: '', action: () => {}, path: []};

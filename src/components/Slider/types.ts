255fa69d78101e728cbd1c6e1e3a8819844c9175
Kirill Revenkov <revenkov-k@yandex-team.ru>
2024-02-04 23:47:16 +0300

    feat: add types description

--- src/components/Slider/types.ts
+++ src/components/Slider/types.ts
@@ -9,29 +9,48 @@ export type SliderValue = number | [number, number];
 export type RcSliderValueType = number | number[];
 
 export type SliderProps<ValueType = number | [number, number]> = {
+    /** The value of the control */
     value?: ValueType;
+    /** The control's default value, used when the component is not controlled */
     defaultValue?: ValueType;
+    /** The size of the control */
     size?: SliderSize;
+    /** Min value of the component */
     min?: number;
+    /** Max value of the component */
     max?: number;
-    step?: number;
-    marksCount?: number;
+    /** Specifies the array of available values for the slider */
     availableValues?: number[];
+    /** Value to be added or subtracted on each step the slider makes. This prop will be ignored if available values is set.  */
+    step?: number;
+    /** Amount of text marks under the slider. Split whole range on equal parts. Could be set >=2. This prop will be ignored if available values is set. */
+    marksCount?: number;
+    /** Show tooltip with current value of component or not */
     hasTooltip?: boolean;
 
+    /** Indicates that the user cannot interact with the control */
     disabled?: boolean;
+    /** Text of an error to show */
     errorText?: string;
     /** Describes the validation state */
     validationState?: 'invalid';
+
+    /** Specifies the delay (in milliseconds) before the processing function is called */
     debounceDelay?: number;
+    /** Fires when the control gets focus. Provides focus event as a callback's argument */
     onFocus?: (e: React.FocusEvent<HTMLDivElement>) => void;
+    /** Fires when the control lost focus. Provides focus event as a callback's argument */
     onBlur?: (e: React.FocusEvent<HTMLDivElement>) => void;
+    /** Fires when the slider’s value is changed by the user. Provides change event as an callback's argument */
     onChange?: (value: ValueType) => void;
+    /** Fires when ontouchend or onmouseup is triggered. Provides change event as an callback's argument */
     onChangeComplete?: (value: ValueType) => void;
 
+    /** The control's autoFocus attribute */
     autoFocus?: boolean;
+    /** The control's tabIndex attribute */
     tabIndex?: ValueType;
-
+    /** Ref to Slider's component props of focus and blur */
     apiRef?: React.RefObject<BaseSliderRefType>;
 } & Pick<DOMProps, 'className'> &
     QAProps;

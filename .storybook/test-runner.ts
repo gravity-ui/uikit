0d89d8f793f9b017a06148c28f139632bd3515a8
Kezhik Kyzyl-ool <k.kyzylool@nebius.com>
2024-02-13 10:14:20 +0100

    fix: configuring axe in storybook test runner

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,5 +1,6 @@
 import type {TestRunnerConfig} from '@storybook/test-runner';
-import {checkA11y, injectAxe} from 'axe-playwright';
+import {getStoryContext} from '@storybook/test-runner';
+import {checkA11y, configureAxe, injectAxe} from 'axe-playwright';
 
 /*
  * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
@@ -9,7 +10,15 @@ const config: TestRunnerConfig = {
     async preVisit(page) {
         await injectAxe(page);
     },
-    async postVisit(page) {
+    async postVisit(page, context) {
+        // Get the entire context of a story, including parameters, args, argTypes, etc.
+        const storyContext = await getStoryContext(page, context);
+
+        // Apply story-level a11y rules
+        await configureAxe(page, {
+            rules: storyContext.parameters?.a11y?.config?.rules,
+        });
+
         await checkA11y(page, '#storybook-root', {
             detailedReport: true,
             detailedReportOptions: {

815db6cd3c182eb120c6010fafb7183b17b93801
Kirill Revenkov <revenkov-k@yandex-team.ru>
2024-02-26 11:34:44 +0300

    feat: add types for new component

--- src/components/Select/types.ts
+++ src/components/Select/types.ts
@@ -16,6 +16,7 @@ export type SelectRenderControlProps = {
     onClick: () => void;
     onKeyDown?: (e: React.KeyboardEvent<HTMLElement>) => void;
     renderClear?: (args: SelectRenderClearArgs) => React.ReactNode;
+    renderCounter?: () => React.ReactNode;
     ref: React.Ref<HTMLElement>;
     open: boolean;
     popupId: string;
@@ -51,6 +52,12 @@ export type SelectRenderPopup = (popupItems: {
 
 export type SelectSize = InputControlSize;
 
+export type SelectRenderCounter = (
+    size: SelectSize,
+    count: number,
+    originalComponent: React.ReactNode,
+) => React.ReactNode;
+
 export type SelectProps<T = any> = QAProps &
     Pick<ControlGroupProps, 'name' | 'disabled'> &
     UseOpenProps & {
@@ -69,6 +76,7 @@ export type SelectProps<T = any> = QAProps &
         renderSelectedOption?: (option: SelectOption<T>, index: number) => React.ReactElement;
         renderEmptyOptions?: ({filter}: {filter: string}) => React.ReactElement;
         renderPopup?: SelectRenderPopup;
+        renderCounter?: SelectRenderCounter;
         getOptionHeight?: (option: SelectOption<T>, index: number) => number;
         getOptionGroupHeight?: (option: SelectOptionGroup<T>, index: number) => number;
         filterOption?: (option: SelectOption<T>, filter: string) => boolean;
@@ -112,6 +120,8 @@ export type SelectProps<T = any> = QAProps &
             | React.ReactElement<SelectOptionGroup<T>, typeof OptionGroup>
             | React.ReactElement<SelectOptionGroup<T>, typeof OptionGroup>[];
         id?: string;
+        /**Shows selected options count if multiple selection is avalable */
+        hasCounter?: boolean;
     };
 
 export type SelectOption<T = any> = QAProps &
@@ -145,6 +155,11 @@ export type SelectClearProps = SelectClearIconProps & {
      */
     onMouseEnter: (e: React.MouseEvent) => void;
     onMouseLeave: (e: React.MouseEvent) => void;
+};
+
+export type SelectCounterProps = {
+    count: number;
+    size: SelectSize;
 };
 
 export type SelectOptions<T = any> = NonNullable<SelectProps<T>['options']>;

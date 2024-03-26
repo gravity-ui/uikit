b7da9e1d639697c20a2217a22041c45ae022d7e5
Andrey Morozov <amje@yandex-team.ru>
2024-03-26 14:16:17 +0300

    test(Button): add visual test for Button inside text (#1451)

--- src/components/Button/__tests__/helpersPlaywright.ts
+++ src/components/Button/__tests__/helpersPlaywright.ts
@@ -2,5 +2,16 @@ import {composeStories} from '@storybook/react';
 
 import * as ButtonStories from '../__stories__/Button.stories';
 
-export const {Default, Selected, Size, View, Icon, Disabled, Loading, Width, Pin, Link} =
-    composeStories(ButtonStories);
+export const {
+    Default,
+    Selected,
+    Size,
+    View,
+    Icon,
+    Disabled,
+    Loading,
+    Width,
+    Pin,
+    Link,
+    InsideText,
+} = composeStories(ButtonStories);

01521827b3f8d26c46f207243733d1eabba21a55
Valerii Sidorenko <balepas@yandex-team.ru>
2024-04-03 15:39:36 +0200

    chore: update storybook v8 (#1480)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type {StorybookConfig} from '@storybook/react-webpack5';
 const config: StorybookConfig = {
     framework: {
         name: '@storybook/react-webpack5',
-        options: {fastRefresh: true},
+        options: {},
     },
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
     docs: {
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
         {name: '@storybook/addon-essentials', options: {backgrounds: false}},
         './theme-addon/register.tsx',
         '@storybook/addon-a11y',
+        '@storybook/addon-webpack5-compiler-babel',
     ],
 };
 

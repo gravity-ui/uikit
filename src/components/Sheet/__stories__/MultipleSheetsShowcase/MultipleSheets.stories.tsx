ad9680a27c5c123379cdce33793a2964c77da842
leutinatasya <leutinatasya@yandex-team.ru>
2024-05-03 18:05:22 +0300

    fix(Sheet): resize sheet if image in content is loaded

---
+++ src/components/Sheet/__stories__/MultipleSheetsShowcase/MultipleSheets.stories.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import type {Meta, StoryFn} from '@storybook/react';
+
+import {Button} from '../../..';
+import {cn} from '../../../utils/cn';
+import {Sheet} from '../../Sheet';
+import type {SheetProps} from '../../Sheet';
+
+import './MultipleSheets.scss';
+
+const b = cn('sheet-stories-with-image-showcase');
+
+export default {
+    title: 'Components/Overlays/Sheet',
+    component: Sheet,
+} as Meta;
+
+export const MultipleSheets: StoryFn<SheetProps> = (args: SheetProps) => {
+    const [visible, setVisible] = React.useState(false);
+    const [moreContentVisible, setMoreContentVisible] = React.useState(false);
+
+    return (
+        <div className={b()}>
+            <Button className={b('show-btn')} onClick={() => setVisible(true)}>
+                Show modal
+            </Button>
+            <Sheet {...args} visible={visible} id="main" onClose={() => setVisible(false)}>
+                <img
+                    src="https://avatars.githubusercontent.com/u/107542106"
+                    width="100%"
+                    alt="example"
+                />
+                <Button
+                    size="xl"
+                    width="max"
+                    className={b('show-btn')}
+                    onClick={() => setMoreContentVisible(true)}
+                >
+                    Show one more modal
+                </Button>
+            </Sheet>
+            <Sheet
+                {...args}
+                id="more-content"
+                visible={moreContentVisible}
+                onClose={() => setMoreContentVisible(false)}
+            >
+                <div className={b('text')}>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam consequatur
+                    quasi quo temporibus. Optio tenetur, aliquam ratione natus asperiores
+                    necessitatibus? Cumque nulla nesciunt esse minus cum nam rerum illum dicta.
+                </div>
+                <div>
+                    <Button
+                        size="xl"
+                        width="max"
+                        className={b('show-btn')}
+                        onClick={() => setMoreContentVisible(false)}
+                    >
+                        Close
+                    </Button>
+                </div>
+            </Sheet>
+        </div>
+    );
+};

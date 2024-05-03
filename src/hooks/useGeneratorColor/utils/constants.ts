e38ee024e12bda0b32f6385a0fe41185e7d8e7b0
German Shteinardt <shteinardt@yandex-team.ru>
2024-05-03 11:19:08 +0200

    feat: added a hook for generating a unique color

---
+++ src/hooks/useGeneratorColor/utils/constants.ts
@@ -0,0 +1,34 @@
+import type {ColorOptions, ThemeColorSettings} from '../types';
+
+const bright: ColorOptions = {
+    lightness: [45, 55],
+    situration: [45, 55],
+};
+
+export const colorOptions: Record<string, ThemeColorSettings> = {
+    dark: {
+        saturated: {
+            lightness: [40, 80],
+            situration: [15, 55],
+        },
+        unsaturated: {
+            lightness: [25, 35],
+            situration: [45, 55],
+        },
+        bright,
+    },
+    light: {
+        saturated: {
+            lightness: [40, 80],
+            situration: [15, 55],
+        },
+        unsaturated: {
+            lightness: [80, 90],
+            situration: [45, 55],
+        },
+        bright,
+    },
+};
+
+export const WHITE_COLOR = '#ffffff';
+export const BLACK_COLOR = '#000000';

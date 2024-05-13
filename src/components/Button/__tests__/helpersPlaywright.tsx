90cc393d2f55d9ed1a89f802b3ddca5dce15262a
telegine <telegine@yandex-team.ru>
2024-05-13 13:07:45 +0300

    test(Button): add test for only icon in shrinked container

--- src/components/Button/__tests__/helpersPlaywright.tsx
+++ src/components/Button/__tests__/helpersPlaywright.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-import {ChevronDown, Globe} from '@gravity-ui/icons';
+import {ChevronDown, Globe, Xmark} from '@gravity-ui/icons';
 import {composeStories} from '@storybook/react';
 
 import {Button} from '..';
 import {Showcase} from '../../../demo/Showcase';
 import {Icon} from '../../Icon';
+import {cn} from '../../utils/cn';
 import * as DefaultButtonStories from '../__stories__/Button.stories';
+
+import './helpersPlaywright.scss';
+
+const b = cn('button-view-showcase-container');
 
 export const ButtonStories = composeStories(DefaultButtonStories);
 
@@ -37,5 +42,20 @@ export const CustomIconSizeButton = () => (
             Both bigger icons
             <Icon size={24} data={ChevronDown} />
         </Button>
+    </Showcase>
+);
+
+export const SingleIconButtonInShrinkedContainer = () => (
+    <Showcase className={b()}>
+        <div className={b('shrinked-container')}>
+            <Button size="xl">
+                <Icon size={24} data={Xmark} />
+            </Button>
+        </div>
+        <div className={b('shrinked-container', {max: true})}>
+            <Button size="xl" width="max">
+                <Icon size={24} data={Xmark} />
+            </Button>
+        </div>
     </Showcase>
 );

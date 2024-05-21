30ffc82b19a894a49af0d281154ff702179a51ce
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2024-05-21 08:04:12 +0200

    refactor(Avatar): extract default value

--- src/components/Avatar/index.ts
+++ src/components/Avatar/index.ts
@@ -1,5 +1,5 @@
 export type {AvatarSize} from './types/common';
 export type {AvatarProps, AvatarTheme, AvatarView} from './types/main';
-export {AVATAR_SIZES} from './constants';
+export {AVATAR_SIZES, DEFAULT_AVATAR_SIZE} from './constants';
 export {Avatar} from './Avatar';
 export {getAvatarDisplayText} from './AvatarText';

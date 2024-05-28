9f68aa3b7614785da709fc99a4a52595ed3ebfa8
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2024-05-28 14:13:05 +0200

    chore: expand description

--- src/components/AvatarStack/types.ts
+++ src/components/AvatarStack/types.ts
@@ -7,7 +7,14 @@ export type AvatarStackOverlapSize = 's' | 'm' | 'l';
 export interface AvatarStackProps {
     /** Amount of avatars to be shown before more button. Default 3. */
     max?: number;
-    /** How much each avatar should overlap next one */
+    /**
+     * How much each avatar should overlap next one
+     * | Avatar sizes | Recommended overlap |
+     * | :----------: | :-----------------: |
+     * | `xs`-`m`     | `s`                 |
+     * | `l`          | `m`                 |
+     * | `xl`         | `l`                 |
+     */
     overlapSize?: AvatarStackOverlapSize;
     size?: AvatarSize;
     className?: string;

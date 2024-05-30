12e003c05fc88416bcaab3226e575bf7321d952b
itwillwork <nurullin_edgar@mail.ru>
2024-05-30 23:38:49 +0300

    docs(ClipboardButton): add visual tests

---
+++ src/components/ClipboardButton/__tests__/helpersPlaywright.tsx
@@ -0,0 +1,5 @@
+import {composeStories} from '@storybook/react';
+
+import * as DefaultClipboardButtonStories from '../__stories__/ClipboardButton.stories';
+
+export const ClipboardButtonStories = composeStories(DefaultClipboardButtonStories);

29078f429abc837e7d7082a0a18af6fdd7b8e18b
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2024-05-31 13:48:33 +0200

    fix: add aria-label

--- src/components/AvatarStack/AvatarStackMoreButton.tsx
+++ src/components/AvatarStack/AvatarStackMoreButton.tsx
@@ -4,6 +4,8 @@ import type {AvatarSize} from '../Avatar';
 import {Avatar, DEFAULT_AVATAR_SIZE} from '../Avatar';
 import {block} from '../utils/cn';
 
+import i18n from './i18n';
+
 const b = block('avatar-stack');
 
 type Props = Pick<React.HTMLProps<HTMLButtonElement>, 'className' | 'onClick' | 'aria-label'> & {
@@ -15,7 +17,11 @@ export const AvatarStackMoreButton = React.forwardRef<HTMLButtonElement, Props>(
     ({className, size = DEFAULT_AVATAR_SIZE, onClick, count, 'aria-label': ariaLabel}, ref) => {
         return (
             <button ref={ref} className={b('more-button', {size}, className)} onClick={onClick}>
-                <Avatar text={`+${count}`} size={size} aria-label={ariaLabel} />
+                <Avatar
+                    text={`+${count}`}
+                    size={size}
+                    aria-label={ariaLabel || i18n('more', {count})}
+                />
             </button>
         );
     },

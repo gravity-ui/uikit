d1a6da8c3f649f3731e625960c34a375a6321b2f
German Shteinardt <shteinardt@yandex-team.ru>
2024-06-04 15:42:03 +0200

    refactor: refactor the class GenerateColor into pure functions

--- src/hooks/useColorGenerator/color.ts
+++ src/hooks/useColorGenerator/color.ts
@@ -1,130 +1,87 @@
 /* eslint-disable no-bitwise */
 import {BLACK_COLOR, WHITE_COLOR, colorOptions} from './constants';
-import type {ColorProps, ThemeColorSettings} from './types';
-import {getHue, hashFnv32a, normalizeHash, randomIndex} from './utils';
+import type {ColorProps, HslColorProps} from './types';
+import {getHue, hashFnv32a, normalizeHash} from './utils';
 
-export class ColorGenerator {
-    private _colorKeys?: string[];
-    private _saturation?: number;
-    private _lightness?: number;
-    private _token: string;
-    private _hash: number;
-    private _mode: ColorProps['mode'];
-    private _hue: number | null;
-    private _saturationRange: [number, number];
-    private _lightnessRange: [number, number];
-    private _themeOptions: ThemeColorSettings;
+// https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB
+const hslToRgb = (h: number, s: number, l: number) => {
+    s /= 100;
+    l /= 100;
 
-    constructor({token, colorKeys, theme, mode}: ColorProps) {
-        this._token = token;
-        this._mode = mode ?? 'unsaturated';
-        this._hash = this.getHash(token);
-        this._colorKeys = colorKeys;
-        this._themeOptions = colorOptions[theme];
-        this._lightnessRange = this._themeOptions[this._mode].lightness;
-        this._saturationRange = this._themeOptions[this._mode].situration;
-        this._hue = null;
-    }
+    const k = (n: number) => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
 
-    get color() {
-        if (this._colorKeys && this._colorKeys.length > 0) {
-            const index = this.getColorKeysIndex();
-
-            return this._colorKeys[index];
-        }
-
-        return this.hslColor();
-    }
-
-    get textColor() {
-        if (!this._hue || !this._saturation || !this._lightness) {
-            return WHITE_COLOR;
-        }
-
-        return this.getTextColor(this._hue, this._saturation, this._lightness);
-    }
-
-    private getColorKeysIndex() {
-        if (!this._colorKeys || this._colorKeys.length === 0) {
-            return -1;
-        }
-
-        return randomIndex(this._token, this._colorKeys.length);
-    }
-
-    // https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB
-    private hslToRgb = (h: number, s: number, l: number) => {
-        s /= 100;
-        l /= 100;
-
-        const k = (n: number) => (n + h / 30) % 12;
-        const a = s * Math.min(l, 1 - l);
-
-        const f = (n: number) => {
-            return l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
-        };
-
-        const r = ~~(255 * f(0));
-        const g = ~~(255 * f(8));
-        const b = ~~(255 * f(4));
-
-        return [r, g, b] as [number, number, number];
+    const f = (n: number) => {
+        return l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
     };
 
-    // https://www.w3.org/TR/WCAG20/#relativeluminancedef
-    private getTextColor(
-        h: number,
-        s: number,
-        l: number,
-        lightColor = WHITE_COLOR,
-        darkColor = BLACK_COLOR,
-    ) {
-        const rgb = this.hslToRgb(h, s, l);
-        const N = rgb.length;
-        const normalizedValues = Array(N);
+    const r = ~~(255 * f(0));
+    const g = ~~(255 * f(8));
+    const b = ~~(255 * f(4));
 
-        for (let i = 0; i < N; i++) {
-            let c = rgb[i];
-            c /= 255.0;
+    return [r, g, b] as [number, number, number];
+};
 
-            if (c <= 0.04045) {
-                c /= 12.92;
-            } else {
-                c = Math.pow((c + 0.055) / 1.055, 2.4);
-            }
+// https://www.w3.org/TR/WCAG20/#relativeluminancedef
+const getRelativeLuminance = (h: number, s: number, l: number) => {
+    const rgb = hslToRgb(h, s, l);
+    const N = rgb.length;
+    const normalizedValues = Array(N);
 
-            normalizedValues[i] = c;
+    for (let i = 0; i < N; i++) {
+        let c = rgb[i];
+        c /= 255.0;
+
+        if (c <= 0.04045) {
+            c /= 12.92;
+        } else {
+            c = Math.pow((c + 0.055) / 1.055, 2.4);
         }
 
-        const [r, g, b] = normalizedValues;
-        const L = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-
-        return L > 0.179 ? darkColor : lightColor;
+        normalizedValues[i] = c;
     }
 
-    private hslColor() {
-        this._hue = getHue(this._hash);
+    const [r, g, b] = normalizedValues;
+    const L = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-        this._saturation = normalizeHash(
-            this._hash,
-            this._saturationRange[0],
-            this._saturationRange[1],
-        );
+    return L;
+};
 
-        this._lightness = normalizeHash(
-            this._hash,
-            this._lightnessRange[0],
-            this._lightnessRange[1],
-        );
+const getHash = (seed: string) => {
+    const hash = hashFnv32a(seed, 0x73_6f_6d_65) ^ hashFnv32a(seed, 0x64_6f_72_61);
 
-        const color = `hsl(${this._hue}deg ${this._saturation}% ${this._lightness}%)`;
+    return hash;
+};
 
-        return color;
-    }
+const generateHSLColor = ({hash, intensity, theme}: HslColorProps) => {
+    const hue = getHue(hash);
+    const themeOptions = colorOptions[theme];
+    const lightnessRange = themeOptions[intensity].lightness;
+    const saturationRange = themeOptions[intensity].situration;
 
-    private getHash(token: string) {
-        const hash = hashFnv32a(token, 0x73_6f_6d_65) ^ hashFnv32a(token, 0x64_6f_72_61);
+    const saturation = normalizeHash(hash, saturationRange[0], saturationRange[1]);
+    const lightness = normalizeHash(hash, lightnessRange[0], lightnessRange[1]);
 
-        return hash;
-    }
-}
+    const color = `hsl(${hue}deg ${saturation}% ${lightness}%)`;
+
+    return {color, hue, saturation, lightness};
+};
+
+export const getPersistentColor = ({seed, intensity = 'light', theme}: ColorProps) => {
+    const hash = getHash(seed);
+    const values = generateHSLColor({hash, intensity, theme});
+
+    return values;
+};
+
+export const getTextColor = (
+    h: number,
+    s: number,
+    l: number,
+    lightColor = WHITE_COLOR,
+    darkColor = BLACK_COLOR,
+) => {
+    const L = getRelativeLuminance(h, s, l);
+
+    return L > 0.179 ? darkColor : lightColor;
+};

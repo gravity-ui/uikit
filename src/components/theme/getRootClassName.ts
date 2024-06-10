f96c2d08246563eb7054f855c86d4c754107261b
Andrey Morozov <amje@yandex-team.ru>
2024-06-10 15:47:22 +0300

    fix: review fixes

--- src/components/theme/getRootClassName.ts
+++ src/components/theme/getRootClassName.ts
@@ -1,10 +1,13 @@
 import {block} from '../utils/cn';
-import type {CnMods} from '../utils/cn';
 
 import {ROOT_CLASSNAME} from './constants';
 
 const b = block(ROOT_CLASSNAME);
 
-export function getRootClassName(mods: CnMods, className: string) {
-    return b(mods, className);
+interface RootMods {
+    theme?: string;
+}
+
+export function getRootClassName({theme}: RootMods = {}, className?: string) {
+    return b({theme}, className);
 }

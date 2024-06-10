a4641c51de4be027fcccaff6ae3a8552465630a6
Andrey Morozov <amje@yandex-team.ru>
2024-06-10 16:46:07 +0300

    fix: review fixes 2

--- src/components/theme/getRootClassName.ts
+++ src/components/theme/getRootClassName.ts
@@ -1,11 +1,12 @@
 import {block} from '../utils/cn';
 
 import {ROOT_CLASSNAME} from './constants';
+import type {RealTheme} from './types';
 
 const b = block(ROOT_CLASSNAME);
 
 interface RootMods {
-    theme?: string;
+    theme?: RealTheme;
 }
 
 export function getRootClassName({theme}: RootMods = {}, className?: string) {

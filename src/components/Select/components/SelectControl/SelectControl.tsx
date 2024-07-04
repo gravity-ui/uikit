a0d10461a7697292fd441a5d916b23f8d4edbaba
Kirill Revenkov <revenkov-k@yandex-team.ru>
2024-07-04 11:45:31 +0300

    refactor: css only decision

--- src/components/Select/components/SelectControl/SelectControl.tsx
+++ src/components/Select/components/SelectControl/SelectControl.tsx
@@ -190,17 +190,15 @@ export const SelectControl = React.forwardRef<HTMLButtonElement, ControlProps>((
                     data-qa={qa}
                     title={title}
                 >
-                    <div className={selectControlBlock('content-container')}>
-                        {label && <span className={selectControlBlock('label')}>{label}</span>}
-                        {showPlaceholder && (
-                            <span className={selectControlBlock('placeholder')}>{placeholder}</span>
-                        )}
-                        {showOptionsText && (
-                            <span className={selectControlBlock('option-text')}>
-                                {selectedOptionsContent}
-                            </span>
-                        )}
-                    </div>
+                    {label && <span className={selectControlBlock('label')}>{label}</span>}
+                    {showPlaceholder && (
+                        <span className={selectControlBlock('placeholder')}>{placeholder}</span>
+                    )}
+                    {showOptionsText && (
+                        <span className={selectControlBlock('option-text')}>
+                            {selectedOptionsContent}
+                        </span>
+                    )}
                 </button>
                 {renderCounterComponent()}
                 {renderClearIcon({})}

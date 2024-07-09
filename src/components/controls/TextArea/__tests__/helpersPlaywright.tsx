554fd4b4778d759ec7a3fa427a5ab43ff52b09bd
GermanVor <vorotnikov.german@yandex.ru>
2024-07-10 01:54:26 +0200

    chore: remove visual tests

--- src/components/controls/TextArea/__tests__/helpersPlaywright.tsx
+++
@@ -1,5 +0,0 @@
-import {composeStories} from '@storybook/react';
-
-import * as DefaultTextAreaStories from '../__stories__/TextArea.stories';
-
-export const TextAreaStories = composeStories(DefaultTextAreaStories);

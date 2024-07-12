0b2f0549674a9c0806bbb5510d9f90f72425be9b
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2024-07-12 11:39:17 +0200

    chore: export `AvatarStackMoreButtonProps`

--- src/components/AvatarStack/AvatarStackMoreButton.tsx
+++ src/components/AvatarStack/AvatarStackMoreButton.tsx
@@ -8,23 +8,27 @@ import i18n from './i18n';
 
 const b = block('avatar-stack');
 
-type Props = Pick<React.HTMLProps<HTMLButtonElement>, 'className' | 'onClick' | 'aria-label'> & {
+export type AvatarStackMoreButtonProps = Pick<
+    React.HTMLProps<HTMLButtonElement>,
+    'className' | 'onClick' | 'aria-label'
+> & {
     size?: AvatarSize;
     count: number;
 };
 
-export const AvatarStackMoreButton = React.forwardRef<HTMLButtonElement, Props>(
-    ({className, size = DEFAULT_AVATAR_SIZE, onClick, count, 'aria-label': ariaLabel}, ref) => {
-        return (
-            <button ref={ref} className={b('more-button', {size}, className)} onClick={onClick}>
-                <Avatar
-                    text={`+${count}`}
-                    size={size}
-                    aria-label={ariaLabel || i18n('more', {count})}
-                />
-            </button>
-        );
-    },
-);
+export const AvatarStackMoreButton = React.forwardRef<
+    HTMLButtonElement,
+    AvatarStackMoreButtonProps
+>(({className, size = DEFAULT_AVATAR_SIZE, onClick, count, 'aria-label': ariaLabel}, ref) => {
+    return (
+        <button ref={ref} className={b('more-button', {size}, className)} onClick={onClick}>
+            <Avatar
+                text={`+${count}`}
+                size={size}
+                aria-label={ariaLabel || i18n('more', {count})}
+            />
+        </button>
+    );
+});
 
 AvatarStackMoreButton.displayName = 'AvatarStack.MoreButton';

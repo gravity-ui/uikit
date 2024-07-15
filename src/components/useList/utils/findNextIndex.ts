02123f086eb33d6de1095f4fe307ffa6cdd251bf
Alexandr Isaev <aisaev188@yandex-team.ru>
2024-07-15 19:03:25 +0300

    fix(useList): fix disebled elements activate logic

--- src/components/useList/utils/findNextIndex.ts
+++ src/components/useList/utils/findNextIndex.ts
@@ -1,16 +1,20 @@
+import type {ListItemId} from '../types';
+
 interface FindNextItemsProps {
     list: string[];
     index: number;
     step: number;
-    disabledItems?: Record<string, boolean>;
+    disabledItemsById?: Record<ListItemId, boolean>;
 }
 
-export const findNextIndex = ({list, index, step, disabledItems = {}}: FindNextItemsProps) => {
+export const findNextIndex = ({list, index, step, disabledItemsById = {}}: FindNextItemsProps) => {
     const dataLength = list.length;
     let currentIndex = (index + dataLength) % dataLength;
 
     for (let i = 0; i < dataLength; i += 1) {
-        if (list[currentIndex] && !disabledItems[currentIndex]) {
+        const id = list[currentIndex];
+
+        if (id && !disabledItemsById[id]) {
             return currentIndex;
         }
         currentIndex = (currentIndex + dataLength + step) % dataLength;

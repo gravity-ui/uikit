a5d3f5ae7946ac41cc51aa1f4735781128d07b81
itwillwork <nurullin_edgar@mail.ru>
2024-08-07 13:16:45 +0300

    test(ActionTooltip): add visual tests

---
+++ src/components/ActionTooltip/__tests__/ActionTooltip.visual.test.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+import {expect} from '@playwright/experimental-ct-react';
+
+import {test} from '~playwright/core';
+
+import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+import {ActionTooltip} from '../ActionTooltip';
+
+import {defaultProps, descriptionCases, hotkeyCases, placementCases} from './cases';
+
+test.describe('ActionTooltip', {tag: '@ActionTooltip'}, () => {
+    const smokeScenarios = createSmokeScenarios(defaultProps, {
+        placement: placementCases,
+        hotkey: hotkeyCases,
+        description: descriptionCases,
+    });
+
+    smokeScenarios.forEach(([title, details, props]) => {
+        test(title, details, async ({mount, page, expectScreenshot}) => {
+            const root = await mount(
+                <div
+                    style={{
+                        width: '400px',
+                        height: '400px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div>
+                        <ActionTooltip {...props}>
+                            <div
+                                data-qa="trigger"
+                                style={{padding: '10px', border: '1px tomato dotted'}}
+                            >
+                                trigger block
+                            </div>
+                        </ActionTooltip>
+                    </div>
+                </div>,
+            );
+
+            await expect(root.locator("[data-qa='trigger']")).toBeVisible();
+            await root.locator("[data-qa='trigger']").hover();
+            await expect(page.locator("[role='tooltip']")).toBeVisible({timeout: 1000});
+
+            await expectScreenshot({});
+        });
+    });
+
+    const disabledStateSmokeScenarios = createSmokeScenarios(
+        {
+            ...defaultProps,
+            disabled: true,
+        },
+        {},
+    );
+
+    disabledStateSmokeScenarios.forEach(([title, details, props]) => {
+        test(`disabled-${title}`, details, async ({mount, page, expectScreenshot}) => {
+            const root = await mount(
+                <div
+                    style={{
+                        width: '400px',
+                        height: '400px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div>
+                        <ActionTooltip {...props}>
+                            <div
+                                data-qa="trigger"
+                                style={{padding: '10px', border: '1px tomato dotted'}}
+                            >
+                                trigger block
+                            </div>
+                        </ActionTooltip>
+                    </div>
+                </div>,
+            );
+
+            await expect(root.locator("[data-qa='trigger']")).toBeVisible();
+            await root.locator("[data-qa='trigger']").hover();
+
+            // wait for check the tooltip will definitely not show up
+            await page.waitForTimeout(3000);
+
+            await expect(page.locator("[role='tooltip']")).not.toBeVisible({timeout: 1000});
+
+            await expectScreenshot({});
+        });
+    });
+});
---
+++ src/components/ActionTooltip/__tests__/cases.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import type {Cases} from '../../../stories/tests-factory/models';
+import type {ActionTooltipProps} from '../ActionTooltip';
+
+export const defaultProps: ActionTooltipProps = {
+    title: 'Title',
+    children: <div>trigger</div>,
+};
+
+export const placementCases: Cases<ActionTooltipProps['placement']> = [
+    'auto',
+    'auto-start',
+    'auto-end',
+    'top',
+    'bottom',
+    'right',
+    'left',
+    'top-start',
+    'top-end',
+    'bottom-start',
+    'bottom-end',
+    'right-start',
+    'right-end',
+    'left-start',
+    'left-end',
+];
+export const hotkeyCases: Cases<ActionTooltipProps['hotkey']> = ['mod+a mod+c mod+v'];
+export const descriptionCases: Cases<ActionTooltipProps['description']> = ['Description'];

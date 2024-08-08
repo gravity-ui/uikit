8dbae0f7e5d2ecf3d4192846650228b2e16369b1
itwillwork <nurullin_edgar@mail.ru>
2024-08-08 23:54:52 +0300

    test(Checkbox): add visual tests

---
+++ src/components/Checkbox/__tests__/cases.tsx
@@ -0,0 +1,16 @@
+import type {Cases} from '../../../stories/tests-factory/models';
+import type {CheckboxProps} from '../Checkbox';
+
+export const defaultProps: CheckboxProps = {
+    name: '',
+    value: '',
+    content: 'Checkbox label',
+};
+
+export const sizeCases: Array<CheckboxProps['size']> = ['m', 'l'];
+
+export const disabledCases: Cases<CheckboxProps['disabled']> = [true];
+
+export const checkedCases: Cases<CheckboxProps['checked']> = [true];
+
+export const indeterminateCases: Cases<CheckboxProps['indeterminate']> = [true];

a283fa57ac6e6689080a5dd76f2ca8feed489e32
itwillwork <nurullin_edgar@mail.ru>
2024-08-08 23:24:57 +0300

    test(Card): add visual tests

--- src/components/Card/__tests__/Card.visual.test.tsx
+++ src/components/Card/__tests__/Card.visual.test.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 import {test} from '~playwright/core';
 
+import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+import {Card} from '../Card';
+
+import {
+    containerViewCases,
+    defaultProps,
+    disabledCases,
+    selectedCases,
+    selectionViewCases,
+    sizeCases,
+    themeCases,
+} from './cases';
 import {CardStories} from './helpersPlaywright';
 
-test.describe('Card', () => {
+test.describe('Card', {tag: '@Card'}, () => {
     test('render story: <Default>', async ({mount, expectScreenshot}) => {
         await mount(<CardStories.Default />);
 
@@ -45,5 +57,85 @@ test.describe('Card', () => {
         await mount(<CardStories.Custom />);
 
         await expectScreenshot();
+    });
+
+    createSmokeScenarios(
+        {
+            ...defaultProps,
+            type: 'selection',
+        } as const,
+        {
+            size: sizeCases,
+            disabled: disabledCases,
+            selected: selectedCases,
+            view: selectionViewCases,
+        },
+    ).forEach(([title, details, props]) => {
+        test(`selection ${title}`, details, async ({mount, expectScreenshot}) => {
+            const root = await mount(
+                <Card {...props}>
+                    <div data-qa="content" style={{padding: '10px'}}>
+                        Some text
+                    </div>
+                </Card>,
+            );
+
+            await expectScreenshot({screenshotPostfix: 'before hover'});
+
+            await root.locator("[data-qa='content']").hover();
+
+            await expectScreenshot({screenshotPostfix: 'after hover'});
+        });
+    });
+
+    createSmokeScenarios(
+        {
+            ...defaultProps,
+            type: 'action',
+            onClick: () => {},
+        } as const,
+        {
+            size: sizeCases,
+            disabled: disabledCases,
+        },
+    ).forEach(([title, details, props]) => {
+        test(`action ${title}`, details, async ({mount, expectScreenshot}) => {
+            const root = await mount(
+                <Card {...props}>
+                    <div data-qa="content" style={{padding: '10px'}}>
+                        Some text
+                    </div>
+                </Card>,
+            );
+
+            await expectScreenshot({screenshotPostfix: 'before hover'});
+
+            await root.locator("[data-qa='content']").hover();
+
+            await expectScreenshot({screenshotPostfix: 'after hover'});
+        });
+    });
+
+    createSmokeScenarios(
+        {
+            ...defaultProps,
+            type: 'container',
+        } as const,
+        {
+            size: sizeCases,
+            disabled: disabledCases,
+            view: containerViewCases,
+            theme: themeCases,
+        },
+    ).forEach(([title, details, props]) => {
+        test(`container ${title}`, details, async ({mount, expectScreenshot}) => {
+            await mount(
+                <Card {...props}>
+                    <div style={{padding: '10px'}}>Some text</div>
+                </Card>,
+            );
+
+            await expectScreenshot();
+        });
     });
 });

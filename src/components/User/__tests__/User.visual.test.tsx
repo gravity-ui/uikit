8196a2727a1bec881a30d408b19a51528d05a911
itwillwork <nurullin_edgar@mail.ru>
2024-08-14 11:15:27 +0300

    test(User): add visual tests

--- src/components/User/__tests__/User.visual.test.tsx
+++ src/components/User/__tests__/User.visual.test.tsx
@@ -4,12 +4,32 @@ import {expect} from '@playwright/experimental-ct-react';
 
 import {test} from '~playwright/core';
 
+import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+
+import {avatarCases, descriptionCases, nameCases, sizeCases} from './cases';
+import {TestUser} from './helpers';
 import {Default} from './stories';
 
-test.describe('User', () => {
+test.describe('User', {tag: '@User'}, () => {
     test('render story: <Default>', async ({mount}) => {
         const component = await mount(<Default />);
 
         await expect(component).toHaveScreenshot();
+    });
+
+    createSmokeScenarios(
+        {},
+        {
+            avatar: avatarCases,
+            description: descriptionCases,
+            name: nameCases,
+            size: sizeCases,
+        },
+    ).forEach(([title, details, props]) => {
+        test(title, details, async ({mount, expectScreenshot}) => {
+            await mount(<TestUser {...props} />);
+
+            await expectScreenshot();
+        });
     });
 });

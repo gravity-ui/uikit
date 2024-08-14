6bd94701ff6a74c4271c8950f18aa41309a99403
itwillwork <nurullin_edgar@mail.ru>
2024-08-15 02:19:14 +0300

    test(RadioButton): add visual tests

--- src/components/RadioButton/index.ts
+++ src/components/RadioButton/index.ts
@@ -1,3 +1,5 @@
 export * from './RadioButton';
 // FIXME rename types RadioButtonOption to RadioButtonOptionProps
 export {RadioButtonOption as RadioButtonItem} from './RadioButtonOption';
+
+// RadioButton

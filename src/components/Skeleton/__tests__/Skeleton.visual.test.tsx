011b35fc248ac00f3b1668d5154eaf4b6e45296f
itwillwork <nurullin_edgar@mail.ru>
2024-08-15 00:02:11 +0300

    test(Skeleton): add visual tests

---
+++ src/components/Skeleton/__tests__/Skeleton.visual.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {test} from '~playwright/core';
+
+import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+import type {SkeletonProps} from '../Skeleton';
+import {Skeleton} from '../Skeleton';
+
+test.describe('Skeleton', {tag: '@Skeleton'}, () => {
+    const defaultProps: SkeletonProps = {
+        style: {
+            width: '30px',
+            height: '30px',
+        },
+    };
+
+    createSmokeScenarios(defaultProps, {}).forEach(([title, details, props]) => {
+        test(title, details, async ({mount, expectScreenshot}) => {
+            await mount(<Skeleton {...props} />);
+
+            await expectScreenshot();
+        });
+    });
+});

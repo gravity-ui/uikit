4849c0c1610f420f01e47509b6dee25cf2ebee5f
itwillwork <nurullin_edgar@mail.ru>
2024-08-15 17:36:27 +0300

    test(Slider): add visual tests

---
+++ src/components/Slider/__tests__/cases.tsx
@@ -0,0 +1,10 @@
+import type {Cases} from '../../../stories/tests-factory/models';
+import type {SliderProps} from '../Slider';
+
+export const sizeCases: Cases<SliderProps['size']> = ['s', 'm', 'l', 'xl'];
+export const disabledCases: Cases<SliderProps['disabled']> = [true];
+export const validationStateCases: Cases<SliderProps['validationState']> = ['invalid'];
+export const hasTooltipCases: Cases<SliderProps['hasTooltip']> = [true];
+export const marksCountCases: Cases<SliderProps['marksCount']> = [4];
+export const stepCases: Cases<SliderProps['step']> = [5];
+export const availableValuesCases: Cases<SliderProps['availableValues']> = [10, 20, 50, 55, 65, 80];

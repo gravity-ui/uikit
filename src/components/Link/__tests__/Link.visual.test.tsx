410ac9a0013ef8d67eb97f22aad29f9b85f7e8fb
itwillwork <nurullin_edgar@mail.ru>
2024-08-15 10:47:37 +0300

    test(Link): add visual tests

---
+++ src/components/Link/__tests__/Link.visual.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {test} from '~playwright/core';
+
+import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+import type {LinkProps} from '../Link';
+import {Link} from '../Link';
+
+import {underlineCases, viewCases, visitableCases} from './cases';
+
+test.describe('Link', {tag: '@Link'}, () => {
+    const defaultProps: LinkProps = {
+        href: '#',
+        target: '_blank',
+        children: 'Link',
+    };
+
+    createSmokeScenarios(defaultProps, {
+        underline: underlineCases,
+        visitable: visitableCases,
+        view: viewCases,
+    }).forEach(([title, details, props]) => {
+        test(title, details, async ({mount, expectScreenshot}) => {
+            await mount(<Link {...props} />);
+
+            await expectScreenshot();
+        });
+    });
+});

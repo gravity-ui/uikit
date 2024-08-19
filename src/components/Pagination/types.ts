d15e5f845347392890ab82d16cdd2a1140457705
spotikhanov <spotikhanov@yandex.ru>
2024-08-19 18:37:22 +0300

    feat(Pagination): support 's' and 'xl' variants in PaginationSize

--- src/components/Pagination/types.ts
+++ src/components/Pagination/types.ts
@@ -2,7 +2,7 @@ import type {QAProps} from '../types';
 
 export type ActionName = 'previous' | 'next' | 'first';
 
-export type PaginationSize = 'm' | 'l';
+export type PaginationSize = 's' | 'm' | 'l' | 'xl';
 
 export type PaginationProps = {
     /**

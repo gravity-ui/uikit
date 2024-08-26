a4cd8fdeba0ab467e5043140c56594ee5f093057
itwillwork <nurullin_edgar@mail.ru>
2024-08-26 12:23:33 +0300

    test(ClipboardButton): improve visual tests

--- src/components/ClipboardButton/__tests__/ClipboardButton.visual.test.tsx
+++ src/components/ClipboardButton/__tests__/ClipboardButton.visual.test.tsx
@@ -3,20 +3,21 @@ import React from 'react';
 import {test} from '~playwright/core';
 
 import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+import type {ClipboardButtonProps} from '../ClipboardButton';
 import {ClipboardButton} from '../ClipboardButton';
 
-import {defaultProps, sizeCases, viewCases} from './cases';
+import {sizeCases, viewCases} from './cases';
 
 test.describe('ClipboardButton', {tag: '@ClipboardButton'}, () => {
-    createSmokeScenarios(
-        {
-            ...defaultProps,
-        } as const,
-        {
-            size: sizeCases,
-            view: viewCases,
-        },
-    ).forEach(([title, details, props]) => {
+    const defaultProps: ClipboardButtonProps = {
+        text: 'Text',
+        onCopy: () => {},
+    };
+
+    createSmokeScenarios(defaultProps, {
+        size: sizeCases,
+        view: viewCases,
+    }).forEach(([title, details, props]) => {
         test(title, details, async ({mount, page, expectScreenshot}) => {
             const root = await mount(
                 <div style={{padding: '40px'}}>
@@ -26,16 +27,16 @@ test.describe('ClipboardButton', {tag: '@ClipboardButton'}, () => {
 
             await root.locator("button[type='button']").hover();
 
-            await expectScreenshot({screenshotPostfix: 'init'});
+            await expectScreenshot({});
 
             // wait for render tooltip
             await page.waitForTimeout(2000);
 
-            await expectScreenshot({screenshotPostfix: 'before copy'});
+            await expectScreenshot({nameSuffix: 'after hover'});
 
             await root.locator("button[type='button']").click();
 
-            await expectScreenshot({screenshotPostfix: 'after copy'});
+            await expectScreenshot({nameSuffix: 'after copy'});
         });
     });
 
@@ -50,8 +51,11 @@ test.describe('ClipboardButton', {tag: '@ClipboardButton'}, () => {
             size: sizeCases,
             view: viewCases,
         },
+        {
+            scenarioName: 'custom tooltip',
+        },
     ).forEach(([title, details, props]) => {
-        test(`custom tooltip ${title}`, details, async ({mount, page, expectScreenshot}) => {
+        test(title, details, async ({mount, page, expectScreenshot}) => {
             const root = await mount(
                 <div style={{padding: '100px'}}>
                     <ClipboardButton {...props} />
@@ -60,18 +64,16 @@ test.describe('ClipboardButton', {tag: '@ClipboardButton'}, () => {
 
             await root.locator("button[type='button']").hover();
 
-            await expectScreenshot({screenshotPostfix: 'init'});
+            await expectScreenshot({});
 
             // wait for render tooltip
             await page.waitForTimeout(2000);
 
-            await expectScreenshot({screenshotPostfix: 'before copy'});
+            await expectScreenshot({nameSuffix: 'after hover'});
 
             await root.locator("button[type='button']").click();
 
-            await expectScreenshot({screenshotPostfix: 'after copy'});
+            await expectScreenshot({nameSuffix: 'after copy'});
         });
     });
-
-    //
 });
--- src/components/ClipboardButton/__tests__/cases.tsx
+++ src/components/ClipboardButton/__tests__/cases.tsx
@@ -1,13 +1,9 @@
+import type {CasesWithName} from '../../../stories/tests-factory/models';
 import {
     sizeCases as buttonSizeCases,
     viewsCases as buttonViewsCases,
 } from '../../Button/__tests__/cases';
 import type {ClipboardButtonProps} from '../ClipboardButton';
 
-export const defaultProps: ClipboardButtonProps = {
-    text: 'Text',
-    onCopy: () => {},
-};
-
-export const sizeCases: Array<ClipboardButtonProps['size']> = buttonSizeCases;
-export const viewCases: Array<ClipboardButtonProps['view']> = buttonViewsCases;
+export const sizeCases: CasesWithName<ClipboardButtonProps['size']> = buttonSizeCases;
+export const viewCases: CasesWithName<ClipboardButtonProps['view']> = buttonViewsCases;

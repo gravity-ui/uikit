88d3fad3303b90b46f90d538bc6bef8ab19cd1f2
itwillwork <nurullin_edgar@mail.ru>
2024-08-27 00:20:03 +0300

    test(Icon): improve visual tests

--- src/components/Icon/__tests__/Icon.visual.test.tsx
+++ src/components/Icon/__tests__/Icon.visual.test.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import {test} from '~playwright/core';
 
 import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+import type {IconProps} from '../Icon';
 
 import {sizeCases} from './cases';
 import {TestIcon} from './helpersPlaywright';
 
 test.describe('Icon', {tag: '@Icon'}, () => {
-    createSmokeScenarios(
+    createSmokeScenarios<Omit<IconProps, 'data'>>(
         {},
         {
             size: sizeCases,

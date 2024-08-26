82c998daf276dd3e3d0d98b8db387c1a19d19a58
itwillwork <nurullin_edgar@mail.ru>
2024-08-26 11:29:45 +0300

    test(ArrowToggle): improve visual tests

--- src/components/ArrowToggle/__tests__/ArrowToggle.visual.test.tsx
+++ src/components/ArrowToggle/__tests__/ArrowToggle.visual.test.tsx
@@ -4,10 +4,13 @@ import {test} from '~playwright/core';
 
 import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
 import {ArrowToggle} from '../ArrowToggle';
+import type {ArrowToggleProps} from '../ArrowToggle';
 
-import {defaultProps, directionCases, sizeCases} from './cases';
+import {directionCases, sizeCases} from './cases';
 
 test.describe('ArrowToggle', {tag: '@ArrowToggle'}, () => {
+    const defaultProps: ArrowToggleProps = {};
+
     const smokeScenarios = createSmokeScenarios(defaultProps, {
         size: sizeCases,
         direction: directionCases,
--- src/components/ArrowToggle/__tests__/cases.tsx
+++ src/components/ArrowToggle/__tests__/cases.tsx
@@ -1,7 +1,6 @@
 import type {Cases} from '../../../stories/tests-factory/models';
 import type {ArrowToggleProps} from '../ArrowToggle';
 
-export const defaultProps: ArrowToggleProps = {};
 export const sizeCases: Cases<ArrowToggleProps['size']> = [10, 20, 30, 40, 50, 100];
 export const directionCases: Cases<ArrowToggleProps['direction']> = [
     'top',

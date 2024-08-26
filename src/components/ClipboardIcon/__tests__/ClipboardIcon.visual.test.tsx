ad4e5ebf99afccdbdb3b6126663e2e0a4f1032d6
itwillwork <nurullin_edgar@mail.ru>
2024-08-26 12:13:43 +0300

    test(ClipboardIcon): improve visual tests

--- src/components/ClipboardIcon/__tests__/ClipboardIcon.visual.test.tsx
+++ src/components/ClipboardIcon/__tests__/ClipboardIcon.visual.test.tsx
@@ -3,20 +3,20 @@ import React from 'react';
 import {test} from '~playwright/core';
 
 import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+import type {ClipboardIconProps} from '../ClipboardIcon';
 import {ClipboardIcon} from '../ClipboardIcon';
 
-import {defaultProps, sizeCases, statusCases} from './cases';
+import {sizeCases, statusCases} from './cases';
 
 test.describe('ClipboardIcon', {tag: '@ClipboardIcon'}, () => {
-    createSmokeScenarios(
-        {
-            ...defaultProps,
-        } as const,
-        {
-            size: sizeCases,
-            status: statusCases,
-        },
-    ).forEach(([title, details, props]) => {
+    const defaultProps: ClipboardIconProps = {
+        status: 'success',
+    };
+
+    createSmokeScenarios(defaultProps, {
+        size: sizeCases,
+        status: statusCases,
+    }).forEach(([title, details, props]) => {
         test(title, details, async ({mount, expectScreenshot}) => {
             await mount(<ClipboardIcon {...props} />);
 

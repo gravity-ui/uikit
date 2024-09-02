61df912f3a93d5a82bb3884898acc64bc08e4e0d
Vladimir Valerianov <formula1@yandex-team.ru>
2024-09-02 14:32:39 +0300

    fix(MobileProvider): get rid of irrelevant hook

--- src/components/mobile/useTouch.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import {MobileContext} from './MobileContext';
-import type {MobileContextProps} from './MobileContext';
-
-export function useTouch(): MobileContextProps['touch'] {
-    return React.useContext(MobileContext).touch;
-}

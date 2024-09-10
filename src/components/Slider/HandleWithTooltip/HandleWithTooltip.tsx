32a96d0ac58f72bd6481dc6b107e43d4b78d1363
Kirill Revenkov <revenkov-k@yandex-team.ru>
2024-09-10 12:46:20 +0300

    feat: add visibility logic

--- src/components/Slider/HandleWithTooltip/HandleWithTooltip.tsx
+++ src/components/Slider/HandleWithTooltip/HandleWithTooltip.tsx
@@ -10,24 +10,43 @@ export const HandleWithTooltip = ({
     className,
     tooltipFormat,
 }: HandleWithTooltipProps) => {
-    // const [tooltipVisible, setTooltipVisible] = useState(false);
+    const visibleOnHover = stateModifiers.tooltipDisplay === 'auto';
+    const [tooltipVisible, setTooltipVisible] = React.useState(false);
+
+    if (stateModifiers.tooltipDisplay === 'on' && !tooltipVisible) {
+        setTooltipVisible(true);
+    }
+
     const styleProp = stateModifiers.rtl ? 'right' : 'left';
     const tooltipContent = tooltipFormat
         ? tooltipFormat(originHandleProps.value)
         : originHandleProps.value;
 
+    const handleMouseEnter = React.useCallback(() => {
+        if (visibleOnHover) {
+            setTooltipVisible(true);
+        }
+    }, [visibleOnHover]);
+    const handleMouseLeave = React.useCallback(() => {
+        if (visibleOnHover) {
+            setTooltipVisible(true);
+        }
+    }, [visibleOnHover]);
+
     return (
-        <React.Fragment>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             {originHandle}
-            <SliderTooltip
-                className={className}
-                style={{
-                    insetInlineStart: originHandle.props.style?.[styleProp],
-                }}
-                stateModifiers={stateModifiers}
-            >
-                {tooltipContent}
-            </SliderTooltip>
-        </React.Fragment>
+            {tooltipVisible && (
+                <SliderTooltip
+                    className={className}
+                    style={{
+                        insetInlineStart: originHandle.props.style?.[styleProp],
+                    }}
+                    stateModifiers={stateModifiers}
+                >
+                    {tooltipContent}
+                </SliderTooltip>
+            )}
+        </div>
     );
 };

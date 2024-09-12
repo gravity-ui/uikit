31d2f87bc30a73d72f4bbad02590e6133c230a1c
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-09-12 10:47:02 +0200

    fix: command

--- scripts/playwright-docker.sh
+++ scripts/playwright-docker.sh
@@ -7,24 +7,26 @@ IMAGE_TAG="v1.42.1-jammy" # This version have to be synchronized with playwright
 
 NODE_MODULES_CACHE_DIR="$HOME/.cache/uikit-playwright-docker-node-modules"
 
-# Determine if docker or podman is installed
-if command -v podman &> /dev/null; then
-    CONTAINER_RUNTIME="podman"
-elif command -v docker &> /dev/null; then
-    CONTAINER_RUNTIME="docker"
-else
-    echo "Neither podman nor docker is installed."
-    exit 1
-fi
-
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
 
 run_command() {
-    $CONTAINER_RUNTIME  run --rm --network host -it -w /work \
+    $CONTAINER_RUNTIME run --rm --network host -it -w /work \
         -v $(pwd):/work \
         -v "$NODE_MODULES_CACHE_DIR:/work/node_modules" \
         "$IMAGE_NAME:$IMAGE_TAG" \
         /bin/bash -c "$1"
 }
+
+if command_exists docker; then
+  CONTAINER_TOOL="docker"
+elif command_exists podman; then
+  CONTAINER_TOOL="podman"
+else
+  echo "Neither Docker nor Podman is installed on the system."
+  exit 1
+fi
 
 if [[ "$1" = "clear-cache" ]]; then
     rm -rf "$NODE_MODULES_CACHE_DIR"
@@ -32,6 +34,7 @@ if [[ "$1" = "clear-cache" ]]; then
 fi
 
 if [[ ! -d "$NODE_MODULES_CACHE_DIR" ]]; then
+    mkdir -p "$NODE_MODULES_CACHE_DIR"
     run_command 'npm ci'
 fi
 

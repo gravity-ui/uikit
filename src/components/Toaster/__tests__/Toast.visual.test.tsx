9c895919e3fedcba9f6ba611430bb002894b7ac7
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2024-10-09 18:32:09 +0200

    test: all themes cases

--- src/components/Toaster/__tests__/Toast.visual.test.tsx
+++ src/components/Toaster/__tests__/Toast.visual.test.tsx
@@ -10,4 +10,34 @@ test.describe('Toast', {tag: '@Toaster'}, () => {
 
         await expectScreenshot();
     });
+
+    test('render story: <ToastSimpleInfo>', async ({mount, expectScreenshot}) => {
+        await mount(<ToastStories.ToastSimpleInfo />, {width: 312});
+
+        await expectScreenshot();
+    });
+
+    test('render story: <ToastSimpleSuccess>', async ({mount, expectScreenshot}) => {
+        await mount(<ToastStories.ToastSimpleSuccess />, {width: 312});
+
+        await expectScreenshot();
+    });
+
+    test('render story: <ToastSimpleWarning>', async ({mount, expectScreenshot}) => {
+        await mount(<ToastStories.ToastSimpleWarning />, {width: 312});
+
+        await expectScreenshot();
+    });
+
+    test('render story: <ToastSimpleDanger>', async ({mount, expectScreenshot}) => {
+        await mount(<ToastStories.ToastSimpleDanger />, {width: 312});
+
+        await expectScreenshot();
+    });
+
+    test('render story: <ToastSimpleUtility>', async ({mount, expectScreenshot}) => {
+        await mount(<ToastStories.ToastSimpleUtility />, {width: 312});
+
+        await expectScreenshot();
+    });
 });

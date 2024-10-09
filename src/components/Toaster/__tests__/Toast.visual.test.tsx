bdc995d095acc3e1b57915b1afad2503886ed906
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2024-10-09 17:42:53 +0200

    test: component in page

--- src/components/Toaster/__tests__/Toast.visual.test.tsx
+++ src/components/Toaster/__tests__/Toast.visual.test.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 
-import type {Page} from '@playwright/test';
-
 import {test} from '~playwright/core';
 
 import {ToastStories} from './helpersPlaywright';
 
-function getComponent(page: Page) {
-    return page.locator('.g-toaster').filter({has: page.locator('.g-toast')});
-}
-
 test.describe('Toast', {tag: '@Toaster'}, () => {
-    test('render story: <ToastSimpleNormal>', async ({page, mount, expectScreenshot}) => {
+    test('render story: <ToastSimpleNormal>', async ({mount, expectScreenshot}) => {
         await mount(<ToastStories.ToastSimpleNormal />);
 
-        await expectScreenshot({
-            component: getComponent(page),
-        });
+        await expectScreenshot();
     });
 });

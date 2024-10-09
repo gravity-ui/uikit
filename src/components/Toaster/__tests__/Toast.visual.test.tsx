1b4cd4840d958125afa6ff9ada8df4633a49d56a
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2024-10-09 16:56:11 +0200

    test: toaster selector

--- src/components/Toaster/__tests__/Toast.visual.test.tsx
+++ src/components/Toaster/__tests__/Toast.visual.test.tsx
@@ -5,9 +5,9 @@ import {test} from '~playwright/core';
 import {ToastStories} from './helpersPlaywright';
 
 test.describe('Toast', {tag: '@Toaster'}, () => {
-    test('render story: <ToastSimpleNormal>', async ({mount, expectScreenshot}) => {
+    test('render story: <ToastSimpleNormal>', async ({page, mount, expectScreenshot}) => {
         await mount(<ToastStories.ToastSimpleNormal />);
 
-        await expectScreenshot();
+        await expectScreenshot({component: page.locator('.g-toaster')});
     });
 });

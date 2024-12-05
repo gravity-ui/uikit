fcd9444f0c2035d421a94f812a9639fcaf38adf5
Andrey Morozov <amje@yandex-team.ru>
2024-12-05 18:31:40 +0300

    feat(RadioButton)!: rename component to SegmentedRadioGroup (#1975)

--- src/components/RadioButton/__tests__/cases.tsx
+++
@@ -1,5 +0,0 @@
-import type {Cases} from '../../../stories/tests-factory/models';
-import type {RadioButtonProps} from '../RadioButton';
-
-export const sizeCases: Cases<RadioButtonProps['size']> = ['s', 'm', 'l', 'xl'];
-export const widthCases: Cases<RadioButtonProps['width']> = ['auto', 'max'];

ab051f90e179303760dbd55201b869b46f1e880f
Mikhail Golbakh <golbah@yandex.ru>
2024-12-06 19:33:12 +0300

    feat(User): add html titles for elements

--- src/components/User/constants.ts
+++ src/components/User/constants.ts
@@ -1,4 +1,9 @@
 import type {UserSize} from './types';
 
-export const COMPACT_SIZES: UserSize[] = ['xs', '2xs'];
+export const COMPACT_SIZES: Set<UserSize> = new Set(['xs', '2xs']);
 export const DEFAULT_SIZE: UserSize = 'm';
+
+export const UserQa = {
+    NAME: 'user-name',
+    DESCRIPTION: 'user-description',
+};

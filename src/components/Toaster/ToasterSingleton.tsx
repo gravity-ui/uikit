e788ad66d6b2b651efea0012350c78b0575c7480
icekimi23 <icekimi23@gmail.com>
2024-12-11 12:59:35 +0100

    refactor(Toaster): fix incompatibility of different Toaster APIs

--- src/components/Toaster/ToasterSingleton.tsx
+++ src/components/Toaster/ToasterSingleton.tsx
@@ -1,19 +1,11 @@
 'use client';
 
-import React from 'react';
-
-import get from 'lodash/get';
-import ReactDOM from 'react-dom';
-
-import {block} from '../utils/cn';
-
-import {ToasterProvider} from './Provider/ToasterProvider';
-import {ToasterComponent} from './ToasterComponent/ToasterComponent';
-import type {ToastProps, ToasterArgs, ToasterPublicMethods} from './types';
+import type {InternalToastProps, ToastProps} from './types';
+import {getToastIndex} from './utilities/getToastIndex';
+import {hasToast} from './utilities/hasToast';
+import {removeToast} from './utilities/removeToast';
 
 const TOASTER_KEY: unique symbol = Symbol('Toaster instance key');
-const bToaster = block('toaster');
-let ReactDOMClient: any;
 
 declare global {
     interface Window {
@@ -22,95 +14,86 @@ declare global {
 }
 
 export class ToasterSingleton {
-    static injectReactDOMClient(client: any) {
-        ReactDOMClient = client;
-    }
+    private toasts: InternalToastProps[] = [];
+    private listeners: ((toasts: InternalToastProps[]) => void)[] = [];
 
-    private rootNode!: HTMLDivElement;
-    private reactRoot!: any;
-    private className = '';
-    private mobile = false;
-    private componentAPI: null | ToasterPublicMethods = null;
-
-    constructor(args?: ToasterArgs) {
-        const className = get(args, ['className'], '');
-        const mobile = get(args, ['mobile'], false);
-
+    constructor() {
         if (window[TOASTER_KEY] instanceof ToasterSingleton) {
-            const me = window[TOASTER_KEY];
-            me.className = className;
-            me.mobile = mobile;
-            me.setRootNodeClassName();
-            return me;
+            return window[TOASTER_KEY];
         }
-
-        this.className = className;
-        this.mobile = mobile;
-        this.createRootNode();
-        this.createReactRoot();
-        this.render();
 
         window[TOASTER_KEY] = this;
     }
 
-    destroy() {
-        // eslint-disable-next-line react/no-deprecated
-        ReactDOM.unmountComponentAtNode(this.rootNode);
-        document.body.removeChild(this.rootNode);
-    }
+    add(toast: ToastProps) {
+        let nextToasts = this.toasts;
 
-    add = (options: ToastProps) => {
-        this.componentAPI?.add(options);
-    };
-
-    remove = (name: string) => {
-        this.componentAPI?.remove(name);
-    };
-
-    removeAll = () => {
-        this.componentAPI?.removeAll();
-    };
-
-    update = (name: string, overrideOptions: Partial<ToastProps>) => {
-        this.componentAPI?.update(name, overrideOptions);
-    };
-
-    has = (name: string) => {
-        return this.componentAPI?.has(name) ?? false;
-    };
-
-    private createRootNode() {
-        this.rootNode = document.createElement('div');
-        this.setRootNodeClassName();
-        document.body.appendChild(this.rootNode);
-    }
-
-    private createReactRoot() {
-        if (ReactDOMClient) {
-            this.reactRoot = ReactDOMClient.createRoot(this.rootNode);
+        if (hasToast(nextToasts, toast.name)) {
+            nextToasts = removeToast(nextToasts, toast.name);
         }
+
+        this.toasts = [
+            ...nextToasts,
+            {
+                ...toast,
+                addedAt: Date.now(),
+                ref: {current: null},
+            },
+        ];
+
+        this.notify();
     }
 
-    private render() {
-        const container = (
-            <ToasterProvider
-                ref={(api) => {
-                    this.componentAPI = api;
-                }}
-            >
-                <ToasterComponent hasPortal={false} mobile={this.mobile} />
-            </ToasterProvider>
-        );
+    remove(name: string) {
+        this.toasts = removeToast(this.toasts, name);
 
-        if (this.reactRoot) {
-            this.reactRoot.render(container);
-        } else {
-            // eslint-disable-next-line react/no-deprecated
-            ReactDOM.render(container, this.rootNode, () => Promise.resolve());
+        this.notify();
+    }
+
+    removeAll() {
+        this.toasts = [];
+
+        this.notify();
+    }
+
+    update(name: string, overrideOptions: Partial<ToastProps>) {
+        if (!hasToast(this.toasts, name)) {
+            return;
         }
+
+        const index = getToastIndex(this.toasts, name);
+
+        this.toasts = [
+            ...this.toasts.slice(0, index),
+            {
+                ...this.toasts[index],
+                ...overrideOptions,
+            },
+            ...this.toasts.slice(index + 1),
+        ];
+
+        this.notify();
     }
 
-    private setRootNodeClassName() {
-        this.rootNode.className = bToaster({mobile: this.mobile}, this.className);
+    has(name: string) {
+        return hasToast(this.toasts, name);
+    }
+
+    subscribe(listener: (toasts: InternalToastProps[]) => void) {
+        if (typeof listener === 'function') {
+            this.listeners.push(listener);
+        }
+
+        return () => {
+            this.listeners = this.listeners.filter(
+                (currentListener) => listener !== currentListener,
+            );
+        };
+    }
+
+    private notify() {
+        for (const listener of this.listeners) {
+            listener(this.toasts);
+        }
     }
 }

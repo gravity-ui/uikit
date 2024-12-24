a2fdcb3411f18dfde23f8429c32e80571f6cbf58
itwillwork <nurullin_edgar@mail.ru>
2024-12-24 14:40:51 +0300

    test(Row): more snapshot cases

--- src/components/layout/Row/__tests__/Row.visual.test.tsx
+++ src/components/layout/Row/__tests__/Row.visual.test.tsx
@@ -1,19 +1,49 @@
 import React from 'react';
 
-import {test} from '~playwright/core';
+import {smokeTest, test} from '~playwright/core';
+
+import {DEFAULT_LAYOUT_THEME} from '../../constants';
 
 import {RowStories} from './stories';
 
-test.describe('Flex', {tag: '@Flex'}, () => {
-    test('render story <Default>', async ({mount, expectScreenshot}) => {
-        await mount(<RowStories.Default />);
+test.describe('Row', {tag: '@Row'}, () => {
+    const RESERVE_SPACING_PX = 5;
 
-        await expectScreenshot();
-    });
+    Object.entries(DEFAULT_LAYOUT_THEME.breakpoints).forEach(
+        ([breakpointName, breakpointWidthPx]) => {
+            smokeTest(
+                `render story <Default> - ${breakpointName}`,
+                async ({page, mount, expectScreenshot}) => {
+                    const viewport = page.viewportSize();
+                    if (viewport) {
+                        await page.setViewportSize({
+                            width: breakpointWidthPx + RESERVE_SPACING_PX,
+                            height: viewport.height,
+                        });
+                    }
 
-    test('render story <ZeroSpacings>', async ({mount, expectScreenshot}) => {
-        await mount(<RowStories.ZeroSpacings />);
+                    await mount(<RowStories.Default />);
 
-        await expectScreenshot();
-    });
+                    await expectScreenshot();
+                },
+            );
+
+            smokeTest(
+                `render story <ZeroSpacings> - ${breakpointName}`,
+                async ({page, mount, expectScreenshot}) => {
+                    const viewport = page.viewportSize();
+                    if (viewport) {
+                        await page.setViewportSize({
+                            width: breakpointWidthPx + RESERVE_SPACING_PX,
+                            height: viewport.height,
+                        });
+                    }
+
+                    await mount(<RowStories.ZeroSpacings />);
+
+                    await expectScreenshot();
+                },
+            );
+        },
+    );
 });

781777257d3894349b5e40d845ece0737a9468f9
itwillwork <nurullin_edgar@mail.ru>
2024-12-25 13:50:20 +0300

    test(Progress): update snapshots

--- src/components/Progress/__tests__/Progress.visual.test.tsx
+++ src/components/Progress/__tests__/Progress.visual.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {test} from '~playwright/core';
+import {smokeTest, test} from '~playwright/core';
 
 import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
 import {Progress} from '../Progress';
@@ -8,83 +8,110 @@ import {Progress} from '../Progress';
 import {loadingCases, sizeCases, themeCases} from './cases';
 
 test.describe('Progress', {tag: '@Progress'}, () => {
-    createSmokeScenarios(
-        {},
-        {
-            size: sizeCases,
-            theme: themeCases,
-            loading: loadingCases,
-        },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, expectScreenshot}) => {
-            await mount(
-                <div style={{width: '200px', height: '50px'}}>
-                    <Progress value={50} text="Text" {...props} />
-                </div>,
-            );
+    smokeTest('smoke', async ({mount, expectScreenshot}) => {
+        const smokeScenarios = createSmokeScenarios(
+            {},
+            {
+                size: sizeCases,
+                theme: themeCases,
+                loading: loadingCases,
+            },
+        );
 
-            await expectScreenshot();
+        await mount(
+            <div style={{width: 400}}>
+                {smokeScenarios.map(([title, props]) => (
+                    <div key={title}>
+                        <h4>{title}</h4>
+                        <div style={{width: '200px', height: '50px'}}>
+                            <Progress value={50} text="Text" {...props} />
+                        </div>
+                    </div>
+                ))}
+            </div>,
+        );
+
+        await expectScreenshot({
+            themes: ['light'],
         });
     });
 
-    createSmokeScenarios(
-        {},
-        {
-            size: sizeCases,
-            loading: loadingCases,
-        },
-        {
-            scenarioName: 'with color stops',
-        },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, expectScreenshot}) => {
-            await mount(
-                <div style={{width: '200px', height: '50px'}}>
-                    <Progress
-                        value={50}
-                        text="Text"
-                        colorStops={[
-                            {theme: 'danger', stop: 20},
-                            {theme: 'warning', stop: 50},
-                            {theme: 'success', stop: 100},
-                        ]}
-                        {...props}
-                    />
-                </div>,
-            );
+    smokeTest('smoke, with color stops', async ({mount, expectScreenshot}) => {
+        const smokeScenarios = createSmokeScenarios(
+            {},
+            {
+                size: sizeCases,
+                loading: loadingCases,
+            },
+            {
+                scenarioName: 'with color stops',
+            },
+        );
 
-            await expectScreenshot();
+        await mount(
+            <div style={{width: 400}}>
+                {smokeScenarios.map(([title, props]) => (
+                    <div key={title}>
+                        <h4>{title}</h4>
+                        <div style={{width: '200px', height: '50px'}}>
+                            <Progress
+                                value={50}
+                                text="Text"
+                                colorStops={[
+                                    {theme: 'danger', stop: 20},
+                                    {theme: 'warning', stop: 50},
+                                    {theme: 'success', stop: 100},
+                                ]}
+                                {...props}
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>,
+        );
+
+        await expectScreenshot({
+            themes: ['light'],
         });
     });
 
-    createSmokeScenarios(
-        {},
-        {
-            size: sizeCases,
-            loading: loadingCases,
-        },
-        {
-            scenarioName: 'with stack',
-        },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, expectScreenshot}) => {
-            await mount(
-                <div style={{width: '200px', height: '50px'}}>
-                    <Progress
-                        value={50}
-                        text="Text"
-                        stack={[
-                            {theme: 'default', content: 'First', value: 25},
-                            {theme: 'success', content: 'Second', value: 25},
-                            {theme: 'warning', content: 'Third', value: 25},
-                            {theme: 'danger', content: 'Fourth', value: 25},
-                        ]}
-                        {...props}
-                    />
-                </div>,
-            );
+    smokeTest('smoke, with stack', async ({mount, expectScreenshot}) => {
+        const smokeScenarios = createSmokeScenarios(
+            {},
+            {
+                size: sizeCases,
+                loading: loadingCases,
+            },
+            {
+                scenarioName: 'with stack',
+            },
+        );
 
-            await expectScreenshot();
+        await mount(
+            <div style={{width: 400}}>
+                {smokeScenarios.map(([title, props]) => (
+                    <div key={title}>
+                        <h4>{title}</h4>
+                        <div style={{width: '200px', height: '50px'}}>
+                            <Progress
+                                value={50}
+                                text="Text"
+                                stack={[
+                                    {theme: 'default', content: 'First', value: 25},
+                                    {theme: 'success', content: 'Second', value: 25},
+                                    {theme: 'warning', content: 'Third', value: 25},
+                                    {theme: 'danger', content: 'Fourth', value: 25},
+                                ]}
+                                {...props}
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>,
+        );
+
+        await expectScreenshot({
+            themes: ['light'],
         });
     });
 });

82c7e609474b594b4c3cce4127b2ccd482495e92
itwillwork <nurullin_edgar@mail.ru>
2024-12-25 14:52:10 +0300

    test(Hotkey): update cases

--- src/components/Hotkey/__tests__/Hotkey.visual.test.tsx
+++ src/components/Hotkey/__tests__/Hotkey.visual.test.tsx
@@ -6,16 +6,17 @@ import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-
 import type {HotkeyProps} from '../Hotkey';
 import {Hotkey} from '../Hotkey';
 
-import {platformCases} from './cases';
+import {platformCases, valueCases} from './cases';
 
 test.describe('Hotkey', {tag: '@Hotkey'}, () => {
     smokeTest('smoke, light view', async ({mount, expectScreenshot}) => {
         const defaultProps: HotkeyProps = {
-            value: 'mod+a mod+c mod+v',
+            value: 'mod+a',
             view: 'light',
         };
 
         const smokeScenarios = createSmokeScenarios(defaultProps, {
+            value: valueCases,
             platform: platformCases,
         });
 
@@ -39,11 +40,12 @@ test.describe('Hotkey', {tag: '@Hotkey'}, () => {
 
     smokeTest('smoke, dark view', async ({mount, expectScreenshot}) => {
         const defaultProps: HotkeyProps = {
-            value: 'mod+a mod+c mod+v',
+            value: 'mod+a',
             view: 'dark',
         };
 
         const smokeScenarios = createSmokeScenarios(defaultProps, {
+            value: valueCases,
             platform: platformCases,
         });
 

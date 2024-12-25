911982be104d4d3108601557292cf6505ae08a17
itwillwork <nurullin_edgar@mail.ru>
2024-12-25 12:13:34 +0300

    test(Palette): update snapshots

--- src/components/Palette/__tests__/Palette.visual.test.tsx
+++ src/components/Palette/__tests__/Palette.visual.test.tsx
@@ -1,49 +1,56 @@
 import React from 'react';
 
-import {test} from '~playwright/core';
+import {smokeTest, test} from '~playwright/core';
 
 import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
 import type {PaletteProps} from '../Palette';
 
-import {columnsCases, sizeCases} from './cases';
+import {columnsCases, disabledCases, sizeCases} from './cases';
 import {TestPalette} from './helpersPlaywright';
 
 test.describe('Palette', {tag: '@Palette'}, () => {
     const defaultProps: PaletteProps = {};
 
-    createSmokeScenarios(defaultProps, {
-        size: sizeCases,
-        columns: columnsCases,
-    }).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, expectScreenshot}) => {
-            const root = await mount(<TestPalette {...props} />);
+    smokeTest('hover', async ({mount, expectScreenshot}) => {
+        const root = await mount(<TestPalette {...defaultProps} />);
 
-            await expectScreenshot();
+        await expectScreenshot();
 
-            await root.locator('button').locator('nth=0').focus();
+        await root.locator('button').locator('nth=0').focus();
 
-            await expectScreenshot({nameSuffix: 'after focus'});
+        await expectScreenshot({nameSuffix: 'after focus'});
 
-            await root.locator('button').locator('nth=0').click();
+        await root.locator('button').locator('nth=0').click();
 
-            await expectScreenshot({nameSuffix: 'after click'});
-        });
+        await expectScreenshot({nameSuffix: 'after click'});
     });
 
-    createSmokeScenarios(
-        {
-            ...defaultProps,
-            disabled: true,
-        },
-        {},
-        {
-            scenarioName: 'disabled',
-        },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, expectScreenshot}) => {
-            await mount(<TestPalette {...props} />);
+    smokeTest('smoke', async ({mount, expectScreenshot}) => {
+        const smokeScenarios = createSmokeScenarios(
+            defaultProps,
+            {
+                size: sizeCases,
+                columns: columnsCases,
+                disabled: disabledCases,
+            },
+            {},
+        );
 
-            await expectScreenshot();
+        await mount(
+            <div>
+                {smokeScenarios.map(([title, props]) => (
+                    <div key={title}>
+                        <h4>{title}</h4>
+                        <div>
+                            <TestPalette {...props} />
+                        </div>
+                    </div>
+                ))}
+            </div>,
+        );
+
+        await expectScreenshot({
+            themes: ['light'],
         });
     });
 });
--- src/components/Palette/__tests__/cases.tsx
+++ src/components/Palette/__tests__/cases.tsx
@@ -2,4 +2,5 @@ import type {Cases} from '../../../stories/tests-factory/models';
 import type {PaletteProps} from '../Palette';
 
 export const sizeCases: Cases<PaletteProps['size']> = ['xs', 's', 'm', 'l', 'xl'];
-export const columnsCases: Cases<PaletteProps['columns']> = [2];
+export const columnsCases: Cases<PaletteProps['columns']> = [2, 4, 10];
+export const disabledCases: Cases<PaletteProps['disabled']> = [true];

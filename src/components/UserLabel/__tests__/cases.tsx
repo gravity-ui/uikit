cc6b6dd0d19817ea1adb9556a0ed4326b8e16ca5
itwillwork <15855766+itwillwork@users.noreply.github.com>
2024-12-26 13:25:17 +0300

    test(UserLabel): add smoke visual tests (#1792)

---
+++ src/components/UserLabel/__tests__/cases.tsx
@@ -0,0 +1,21 @@
+import type {Cases, CasesWithName} from '../../../stories/tests-factory/models';
+import type {UserLabelProps} from '../types';
+
+export const sizeCases: Cases<UserLabelProps['size']> = ['xs', 's', 'm', 'l', 'xl'];
+
+export const viewCases: Cases<UserLabelProps['view']> = ['outlined', 'clear'];
+
+export const typeCases: Cases<UserLabelProps['type']> = ['person', 'email', 'empty'];
+
+export const childrenCases: CasesWithName<UserLabelProps['children']> = [
+    [
+        'long label',
+        'Charles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles DarwinCharles Darwin',
+    ],
+];
+
+export const onCloseClickCases: CasesWithName<UserLabelProps['onCloseClick']> = [
+    ['closable', () => {}],
+];
+
+export const onClickCases: CasesWithName<UserLabelProps['onClick']> = [['clickable', () => {}]];

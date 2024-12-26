467855cfcaa5a5984814274ce33e40a6e09f0088
itwillwork <nurullin_edgar@mail.ru>
2024-12-26 14:44:08 +0300

    test(RadioButton): delete comment

--- src/components/RadioButton/index.ts
+++ src/components/RadioButton/index.ts
@@ -1,5 +1,3 @@
 export * from './RadioButton';
 // FIXME rename types RadioButtonOption to RadioButtonOptionProps
 export {RadioButtonOption as RadioButtonItem} from './RadioButtonOption';
-
-// RadioButton

4a765f5a32a8e221288ed475320ee79c0f3756ac
itwillwork <nurullin_edgar@mail.ru>
2024-12-26 17:44:30 +0300

    test(ClipboardButton): update snapshots

--- src/components/ClipboardButton/__tests__/ClipboardButton.visual.test.tsx
+++ src/components/ClipboardButton/__tests__/ClipboardButton.visual.test.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
+import {smokeTest, test} from '~playwright/core';
 
-import {test} from '~playwright/core';
-
-import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
 import type {ClipboardButtonProps} from '../ClipboardButton';
 import {ClipboardButton} from '../ClipboardButton';
-
-import {sizeCases, viewCases} from './cases';
 
 test.describe('ClipboardButton', {tag: '@ClipboardButton'}, () => {
     const defaultProps: ClipboardButtonProps = {
@@ -14,62 +9,32 @@ test.describe('ClipboardButton', {tag: '@ClipboardButton'}, () => {
         onCopy: () => {},
     };
 
-    createSmokeScenarios(defaultProps, {
-        size: sizeCases,
-        view: viewCases,
-    }).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, page, expectScreenshot}) => {
-            const root = await mount(
-                <div style={{padding: '40px'}}>
-                    <ClipboardButton {...props} />
-                </div>,
-            );
+    smokeTest('', async ({mount, page, expectScreenshot}) => {
+        const root = await mount(
+            <div style={{padding: '100px'}}>
+                <ClipboardButton {...defaultProps} hasTooltip />
+            </div>,
+        );
 
-            await root.locator("button[type='button']").hover();
-
-            // wait for render tooltip
-            await page.waitForTimeout(1000);
-
-            await expectScreenshot({nameSuffix: 'after hover'});
-
-            await root.locator("button[type='button']").click();
-
-            await expectScreenshot({nameSuffix: 'after copy'});
+        await expectScreenshot({
+            themes: ['light'],
         });
-    });
 
-    createSmokeScenarios(
-        {
-            ...defaultProps,
-            hasTooltip: true,
-            tooltipInitialText: 'Tooltip initial text',
-            tooltipSuccessText: 'Tooltip success text',
-        } as const,
-        {
-            size: sizeCases,
-            view: viewCases,
-        },
-        {
-            scenarioName: 'custom tooltip',
-        },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, page, expectScreenshot}) => {
-            const root = await mount(
-                <div style={{padding: '100px'}}>
-                    <ClipboardButton {...props} />
-                </div>,
-            );
+        await root.locator("button[type='button']").hover();
 
-            await root.locator("button[type='button']").hover();
+        // wait for render tooltip
+        await page.waitForTimeout(1000);
 
-            // wait for render tooltip
-            await page.waitForTimeout(1000);
+        await expectScreenshot({
+            themes: ['light'],
+            nameSuffix: 'after hover',
+        });
 
-            await expectScreenshot({nameSuffix: 'after hover'});
+        await root.locator("button[type='button']").click();
 
-            await root.locator("button[type='button']").click();
-
-            await expectScreenshot({nameSuffix: 'after copy'});
+        await expectScreenshot({
+            themes: ['light'],
+            nameSuffix: 'after copy',
         });
     });
 });
--- src/components/ClipboardButton/__tests__/cases.tsx
+++
@@ -1,9 +0,0 @@
-import type {CasesWithName} from '../../../stories/tests-factory/models';
-import {
-    sizeCases as buttonSizeCases,
-    viewsCases as buttonViewsCases,
-} from '../../Button/__tests__/cases';
-import type {ClipboardButtonProps} from '../ClipboardButton';
-
-export const sizeCases: CasesWithName<ClipboardButtonProps['size']> = buttonSizeCases;
-export const viewCases: CasesWithName<ClipboardButtonProps['view']> = buttonViewsCases;

1fd5e81808bf3edcda5af20be2af6f7169ee9191
itwillwork <nurullin_edgar@mail.ru>
2024-12-27 20:08:08 +0300

    test(Breadcrumbs): update snapshots

--- src/components/lab/Breadcrumbs/__tests__/Breadcrumbs.visual.test.tsx
+++ src/components/lab/Breadcrumbs/__tests__/Breadcrumbs.visual.test.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import {expect, test} from '~playwright/core';
+import {expect, smokeTest, test} from '~playwright/core';
 
 import {createSmokeScenarios} from '../../../../stories/tests-factory/create-smoke-scenarios';
 import type {BreadcrumbsProps} from '../Breadcrumbs';
@@ -20,43 +18,24 @@ test.describe('Breadcrumbs', {tag: '@Breadcrumbs'}, () => {
         defaultProps,
         {
             popupPlacement: popupPlacementCases,
-            disabled: disabledCases,
         },
         {
             scenarioName: 'with text items',
         },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, page, expectScreenshot}) => {
+    ).forEach(([title, props]) => {
+        smokeTest(title, async ({mount, page, expectScreenshot}) => {
             const root = await mount(
                 <div style={{width: '200px', padding: '100px'}}>
                     <TestBreadcrumbsWithTextItems {...props} />
                 </div>,
             );
 
-            await expectScreenshot();
-
-            if (props.disabled) {
-                return;
-            }
-
-            await root.locator('ol li').locator('nth=2').hover();
-
-            await expectScreenshot({
-                nameSuffix: 'hovered',
-            });
-
-            await root.locator('button').hover();
-
-            await expectScreenshot({
-                nameSuffix: 'hovered more button',
-            });
-
             await root.locator('button').click();
 
             await expect(page.locator(`ul[role="menu"]`)).toBeVisible();
 
             await expectScreenshot({
-                nameSuffix: 'after click on more button',
+                themes: ['light'],
             });
         });
     });
@@ -69,15 +48,17 @@ test.describe('Breadcrumbs', {tag: '@Breadcrumbs'}, () => {
         {
             scenarioName: 'with link items',
         },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, expectScreenshot}) => {
+    ).forEach(([title, props]) => {
+        smokeTest(title, async ({mount, expectScreenshot}) => {
             await mount(
                 <div style={{width: '200px'}}>
                     <TestBreadcrumbsWithLinkItems {...props} />
                 </div>,
             );
 
-            await expectScreenshot();
+            await expectScreenshot({
+                themes: ['light'],
+            });
         });
     });
 
@@ -89,15 +70,17 @@ test.describe('Breadcrumbs', {tag: '@Breadcrumbs'}, () => {
         {
             scenarioName: 'with custom icons',
         },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, expectScreenshot}) => {
+    ).forEach(([title, props]) => {
+        smokeTest(title, async ({mount, expectScreenshot}) => {
             await mount(
                 <div style={{width: '400px'}}>
                     <TestBreadcrumbsWithCustomIcons {...props} />
                 </div>,
             );
 
-            await expectScreenshot();
+            await expectScreenshot({
+                themes: ['light'],
+            });
         });
     });
 
@@ -109,15 +92,17 @@ test.describe('Breadcrumbs', {tag: '@Breadcrumbs'}, () => {
         {
             scenarioName: 'with custom separator',
         },
-    ).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, expectScreenshot}) => {
+    ).forEach(([title, props]) => {
+        smokeTest(title, async ({mount, expectScreenshot}) => {
             await mount(
                 <div style={{width: '400px'}}>
                     <TestBreadcrumbsWithCustomSeparator {...props} />
                 </div>,
             );
 
-            await expectScreenshot();
+            await expectScreenshot({
+                themes: ['light'],
+            });
         });
     });
 });
--- src/components/lab/Breadcrumbs/__tests__/helpersPlaywright.tsx
+++ src/components/lab/Breadcrumbs/__tests__/helpersPlaywright.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import {ChevronRight, Flame, House, Rocket} from '@gravity-ui/icons';
 
 import {Box} from '../../../layout/Box/Box';

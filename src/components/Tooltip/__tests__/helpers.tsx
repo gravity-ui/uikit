c21f27e6114a59e66adc5541a871d5631e1227a2
itwillwork <15855766+itwillwork@users.noreply.github.com>
2024-12-27 16:30:51 +0300

    test(Tooltip): add smoke visual tests (#1790)

---
+++ src/components/Tooltip/__tests__/helpers.tsx
@@ -0,0 +1,28 @@
+import type {TooltipProps} from '../Tooltip';
+import {Tooltip} from '../Tooltip';
+
+import {TooltipQA} from './constants';
+
+export const TestTooltip = (props: Omit<TooltipProps, 'content' | 'disablePortal'>) => {
+    return (
+        <Tooltip
+            {...props}
+            disablePortal
+            content={<div data-qa={TooltipQA.tooltipContent}>tooltip content</div>}
+        >
+            <div
+                style={{
+                    width: '200px',
+                    height: '100px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    border: '1px dotted tomato',
+                }}
+                data-qa={TooltipQA.trigger}
+            >
+                <div>tooltip trigger</div>
+            </div>
+        </Tooltip>
+    );
+};

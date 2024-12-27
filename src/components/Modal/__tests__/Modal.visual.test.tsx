4e1aeaf1586caa4cf9db0ade60539f2b703eacd4
itwillwork <nurullin_edgar@mail.ru>
2024-12-27 13:08:15 +0300

    test(Modal): update snapshots

--- src/components/Modal/__tests__/Modal.visual.test.tsx
+++ src/components/Modal/__tests__/Modal.visual.test.tsx
@@ -1,31 +1,28 @@
-import React from 'react';
-
 import {expect} from '@playwright/experimental-ct-react';
 
-import {test} from '~playwright/core';
+import {smokeTest, test} from '~playwright/core';
 
-import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
-import type {ModalProps} from '../Modal';
+import {Modal} from '../Modal';
 
 import {ModalQa} from './constants';
-import {TestModal} from './helpersPlaywright';
 
 test.describe('Modal', {tag: '@Modal'}, () => {
-    const defaultProps: ModalProps = {};
+    smokeTest('', async ({mount, page, expectScreenshot}) => {
+        await page.setViewportSize({width: 500, height: 500});
 
-    createSmokeScenarios(defaultProps, {}).forEach(([title, details, props]) => {
-        test(title, details, async ({mount, page, expectScreenshot}) => {
-            await page.setViewportSize({width: 500, height: 500});
+        await mount(
+            <div>
+                <div>page content</div>
+                <Modal open qa={ModalQa.content}>
+                    <div style={{padding: 10}}>Modal content</div>
+                </Modal>
+            </div>,
+        );
 
-            await mount(<TestModal {...props} />);
+        await expect(page.getByTestId(ModalQa.content)).toBeVisible();
 
-            await page.getByTestId(ModalQa.trigger).click();
-
-            await expect(page.getByTestId(ModalQa.content)).toBeVisible();
-
-            await expectScreenshot({
-                component: page,
-            });
+        await expectScreenshot({
+            component: page,
         });
     });
 });

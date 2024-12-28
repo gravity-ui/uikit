2b42c23dfd49bb6a10a943b721d10fab1489c868
itwillwork <nurullin_edgar@mail.ru>
2024-12-28 12:11:13 +0300

    test(HelpMark): add smoke cases

--- src/components/HelpMark/__tests__/HelpMark.visual.test.tsx
+++ src/components/HelpMark/__tests__/HelpMark.visual.test.tsx
@@ -2,29 +2,48 @@ import {expect} from '@playwright/experimental-ct-react';
 
 import {smokeTest, test} from '~playwright/core';
 
+import {createSmokeScenarios} from '../../../stories/tests-factory/create-smoke-scenarios';
+import type {HelpMarkProps} from '../HelpMark';
 import {HelpMark} from '../HelpMark';
 
+import {placementCases} from './cases';
+import {HelpMarkStories} from './stories';
+
 test.describe('HelpMark', {tag: '@HelpMark'}, () => {
-    smokeTest('', async ({mount, page, expectScreenshot}) => {
-        const root = await mount(
-            <div style={{minHeight: 500, minWidth: 500}}>
-                <HelpMark
-                    qa="popover"
-                    buttonProps={{
-                        // @ts-expect-error Object literal may only specify known properties, and ''data-qa'' does not exist in type 'ButtonHTMLAttributes<HTMLButtonElement>'
-                        'data-qa': 'trigger',
-                    }}
-                >
-                    Test content
-                </HelpMark>
-            </div>,
-        );
+    test('render story: <Default>', async ({mount, expectScreenshot}) => {
+        await mount(<HelpMarkStories.Default />);
 
-        await root.getByTestId('trigger').hover();
-        await expect(page.getByTestId('popover-tooltip')).toBeVisible();
+        await expectScreenshot();
+    });
 
-        await expectScreenshot({
-            themes: ['light'],
+    createSmokeScenarios<HelpMarkProps>(
+        {},
+        {
+            placement: placementCases,
+        },
+    ).forEach(([title, props]) => {
+        smokeTest(title, async ({mount, page, expectScreenshot}) => {
+            const root = await mount(
+                <div style={{padding: 100}}>
+                    <HelpMark
+                        qa="popover"
+                        buttonProps={{
+                            // @ts-expect-error Object literal may only specify known properties, and ''data-qa'' does not exist in type 'ButtonHTMLAttributes<HTMLButtonElement>'
+                            'data-qa': 'trigger',
+                        }}
+                        {...props}
+                    >
+                        Test content
+                    </HelpMark>
+                </div>,
+            );
+
+            await root.getByTestId('trigger').hover();
+            await expect(page.getByTestId('popover-tooltip')).toBeVisible();
+
+            await expectScreenshot({
+                themes: ['light'],
+            });
         });
     });
 });

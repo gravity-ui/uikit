82a5d3d326e62d21561b4532bfa8171336ec7591
Andrey Morozov <amje@yandex-team.ru>
2025-01-15 14:28:21 +0300

    feat: rewrite usePrevious hook implementation

--- src/hooks/private/usePrevious/usePrevious.ts
+++ src/hooks/private/usePrevious/usePrevious.ts
@@ -1,11 +1,11 @@
 import * as React from 'react';
 
 export function usePrevious<T>(value: T): T | undefined {
-    const currentRef = React.useRef<T>(value);
-    const previousRef = React.useRef<T>();
-    if (currentRef.current !== value) {
-        previousRef.current = currentRef.current;
-        currentRef.current = value;
-    }
-    return previousRef.current;
+    const ref = React.useRef<T>();
+
+    React.useEffect(() => {
+        ref.current = value;
+    });
+
+    return ref.current;
 }

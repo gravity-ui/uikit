95ca9e0447946fd190a1c511df9ddbc3d667fc37
Taya Leutina <leutinatasya@yandex-team.ru>
2025-01-28 17:49:43 +0300

    feat(FilePreview)!: change size of default image preview (#2074)

--- src/components/FilePreview/__stories__/FilePreview.stories.tsx
+++ src/components/FilePreview/__stories__/FilePreview.stories.tsx
@@ -2,6 +2,7 @@ import {CircleExclamation, Link, Xmark} from '@gravity-ui/icons';
 import {action} from '@storybook/addon-actions';
 import type {Meta, StoryFn} from '@storybook/react';
 
+import {Icon} from '../../Icon';
 import {Flex} from '../../layout';
 import type {FilePreviewProps} from '../FilePreview';
 import {FilePreview} from '../FilePreview';
@@ -48,6 +49,47 @@ Default.args = {
 };
 
 const CollageTemplate: StoryFn<FilePreviewProps> = () => {
+    const items = FILE_TYPES.map((fileType) => (
+        <FilePreview
+            key={fileType}
+            file={{name: fileType, type: fileType} as File}
+            onClick={() => action('onClick')}
+            actions={[
+                {
+                    icon: <Link width={14} height={14} />,
+                    title: 'open on drive',
+                    onClick: () => action('onLink'),
+                },
+                {
+                    icon: <Icon data={Xmark} width={14} height={14} />,
+                    title: 'delete a file',
+                    onClick: () => action('onClose'),
+                },
+            ]}
+        />
+    ));
+
+    items.splice(
+        4,
+        0,
+        <FilePreview
+            file={{name: 'my-image-from-thailand.jpg', type: 'image/png'} as File}
+            imageSrc="https://storage.yandexcloud.net/uikit-storybook-assets/changelog-dialog-picture-2.png"
+            actions={[
+                {
+                    icon: <CircleExclamation width={14} height={14} />,
+                    title: 'some hint',
+                    onClick: () => action('onHint'),
+                },
+                {
+                    icon: <Xmark width={14} height={14} />,
+                    onClick: () => action('onClose'),
+                    title: 'Close',
+                },
+            ]}
+        />,
+    );
+
     return (
         <div
             style={{
@@ -55,45 +97,11 @@ const CollageTemplate: StoryFn<FilePreviewProps> = () => {
                 justifyItems: 'center',
                 alignItems: 'center',
                 gridTemplateColumns: 'repeat(3, 1fr)',
-                gridGap: 10,
-                width: 500,
+                gridGap: 8,
+                width: 400,
             }}
         >
-            {FILE_TYPES.map((fileType) => (
-                <FilePreview
-                    key={fileType}
-                    file={{name: fileType, type: fileType} as File}
-                    onClick={() => action('onClick')}
-                    actions={[
-                        {
-                            icon: <Link width={14} height={14} />,
-                            title: 'open on drive',
-                            onClick: () => action('onLink'),
-                        },
-                        {
-                            icon: <Xmark width={14} height={14} />,
-                            title: 'delete a file',
-                            onClick: () => action('onClose'),
-                        },
-                    ]}
-                />
-            ))}
-            <FilePreview
-                file={{name: 'my-image-from-thailand.jpg', type: 'image/png'} as File}
-                imageSrc="https://storage.yandexcloud.net/uikit-storybook-assets/changelog-dialog-picture-2.png"
-                actions={[
-                    {
-                        icon: <CircleExclamation width={14} height={14} />,
-                        title: 'some hint',
-                        onClick: () => action('onHint'),
-                    },
-                    {
-                        icon: <Xmark width={14} height={14} />,
-                        onClick: () => action('onClose'),
-                        title: 'Close',
-                    },
-                ]}
-            />
+            {items}
         </div>
     );
 };

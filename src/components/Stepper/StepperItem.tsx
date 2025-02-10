66191bf98a073f4307fdf635adb35543ad656916
aobityutskiy <aobityutskiy@yandex-team.ru>
2025-02-10 17:40:55 +0300

    feat: add properties to readme

--- src/components/Stepper/StepperItem.tsx
+++ src/components/Stepper/StepperItem.tsx
@@ -1,27 +1,26 @@
 import {Text} from '../Text';
 import type {TextProps} from '../Text';
 
+import type {StepperProps} from './Stepper';
 import {StepperIcon} from './StepperIcon';
-import type {StepperItemView, StepperSize} from './types';
+import type {StepperItemView} from './types';
 import {b} from './utils';
 
 import './Stepper.scss';
 
 export interface StepperItemProps {
     id?: string | number;
-    children: string | React.ReactNode;
+    children: React.ReactNode;
     view?: StepperItemView;
-    size?: StepperSize;
     disabled?: boolean;
     selected?: string | number;
     variant?: TextProps['variant'];
     icon?: React.ReactNode;
     onClick?: (event: React.MouseEvent) => void;
-    onUpdate?: (id?: string | number) => void;
     className?: string;
 }
 
-export const StepperItem = (props: StepperItemProps) => {
+export const StepperItem = (props: StepperItemProps & Pick<StepperProps, 'size' | 'onUpdate'>) => {
     const {
         id,
         size,

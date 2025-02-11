d664cae570da72657caec9ce8d89f2bf97582e76
Andrey Morozov <amje@yandex-team.ru>
2025-02-11 03:26:13 +0300

    feat: first draft of new Menu

---
+++ src/components/lab/Menu/MenuTrigger.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import {Ellipsis, EllipsisVertical} from '@gravity-ui/icons';
+
+import {Button} from '../../Button';
+import type {ButtonButtonProps} from '../../Button';
+import {BUTTON_ICON_SIZE_MAP} from '../../Button/constants';
+import {Icon} from '../../Icon';
+
+export interface MenuTriggerProps extends ButtonButtonProps {
+    icon?: 'horizontal' | 'vertical';
+}
+
+export const MenuTrigger = React.forwardRef<HTMLButtonElement, MenuTriggerProps>(
+    function MenuTrigger({size = 'm', children, icon = 'horizontal', ...restProps}, ref) {
+        return (
+            <Button ref={ref} size={size} {...restProps}>
+                {children ? (
+                    children
+                ) : (
+                    <Icon
+                        data={icon === 'vertical' ? EllipsisVertical : Ellipsis}
+                        size={BUTTON_ICON_SIZE_MAP[size]}
+                    />
+                )}
+            </Button>
+        );
+    },
+);

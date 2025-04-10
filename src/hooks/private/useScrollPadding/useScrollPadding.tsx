2bcc4a86f0a7814d33b67837782867756619afb8
imaxouning <imaxouning@yandex-team.ru>
2025-04-10 13:35:31 +0300

    feat: update root element

--- src/hooks/private/useScrollPadding/useScrollPadding.tsx
+++ src/hooks/private/useScrollPadding/useScrollPadding.tsx
@@ -2,17 +2,21 @@ import * as React from 'react';
 
 export function useScrollPadding(isOpen: boolean) {
     React.useEffect(() => {
-        const hasScrollVertical = window.innerHeight !== document.body.scrollHeight;
-        const hasScrollHorizontal = window.innerWidth !== document.body.scrollWidth;
+        const htmlElement = document?.querySelector('html');
+        if (htmlElement) {
+            const hasScrollVertical = window.innerHeight !== htmlElement.scrollHeight;
+            const hasScrollHorizontal = window.innerWidth !== htmlElement.scrollWidth;
 
-        if (isOpen) {
-            if (hasScrollVertical) {
-                const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-                document.body.style.paddingRight ??= `${scrollbarWidth}px`;
-            }
-            if (hasScrollHorizontal) {
-                const scrollbarHeight = window.innerHeight - document.documentElement.clientHeight;
-                document.body.style.paddingBottom ??= `${scrollbarHeight}px`;
+            if (isOpen) {
+                if (hasScrollVertical) {
+                    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+                    htmlElement.style.paddingRight ??= `${scrollbarWidth}px`;
+                }
+                if (hasScrollHorizontal) {
+                    const scrollbarHeight =
+                        window.innerHeight - document.documentElement.clientHeight;
+                    htmlElement.style.paddingBottom ??= `${scrollbarHeight}px`;
+                }
             }
         }
     }, [isOpen]);

f6c18c070fa8a8c6ad63ab630fa6492b443e100b
Artem Tryapichnikov <golysheeet@gmail.com>
2025-04-27 00:33:25 +0300

    test(DropdownMenu): subitems size inheritance

---
+++ src/components/DropdownMenu/__tests__/cases.ts
@@ -0,0 +1,4 @@
+import type {Cases} from '../../../stories/tests-factory/models';
+import type {DropdownMenuItemProps} from '../DropdownMenuItem';
+
+export const sizeCases: Cases<DropdownMenuItemProps<unknown>['size']> = ['s', 'm', 'l', 'xl'];

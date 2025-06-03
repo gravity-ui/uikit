0d119d6c5858d8f9c22fabfda803fb58c7c1d048
vanilla-wave <vizetmail@gmail.com>
2025-06-03 12:19:00 +0200

    feat: oklch generator prototype

--- src/hooks/useColorGenerator/constants.ts
+++ src/hooks/useColorGenerator/constants.ts
@@ -1,34 +1,41 @@
-import type {ColorOptions, ThemeColorSettings} from './types';
+import type {ThemeColorSettings} from './types';
 
-const heavy: ColorOptions = {
-    lightness: [45, 55],
-    situration: [45, 55],
-};
+const optionsFromLS: Record<string, ThemeColorSettings> = JSON.parse(
+    localStorage.getItem('colorOptions') ?? '{}',
+);
 
-export const colorOptions: Record<string, ThemeColorSettings> = {
-    dark: {
-        light: {
-            lightness: [25, 35],
-            situration: [45, 55],
-        },
-        medium: {
-            lightness: [40, 80],
-            situration: [15, 55],
-        },
-        heavy,
-    },
-    light: {
-        light: {
-            lightness: [80, 90],
-            situration: [45, 55],
-        },
-        medium: {
-            lightness: [40, 80],
-            situration: [15, 55],
-        },
-        heavy,
-    },
-};
+export const colorOptions: Record<string, ThemeColorSettings> = localStorage.getItem('colorOptions')
+    ? optionsFromLS
+    : {
+          dark: {
+              light: {
+                  lightness: [25, 35],
+                  situration: [45, 55],
+              },
+              medium: {
+                  lightness: [40, 80],
+                  situration: [15, 55],
+              },
+              heavy: {
+                  lightness: [45, 55],
+                  situration: [45, 55],
+              },
+          },
+          light: {
+              light: {
+                  lightness: [90, 95],
+                  situration: [5, 10],
+              },
+              medium: {
+                  lightness: [75, 85],
+                  situration: [15, 20],
+              },
+              heavy: {
+                  lightness: [45, 55],
+                  situration: [30, 40],
+              },
+          },
+      };
 
 export const WHITE_COLOR = '#ffffff';
 export const BLACK_COLOR = '#000000';

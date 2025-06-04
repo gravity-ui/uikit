ee36baaa9334210432346b709cb96e90ea102ad0
vanilla-wave <vizetmail@gmail.com>
2025-06-04 15:06:38 +0200

    feat: use oklch color space for color generator

--- src/hooks/useColorGenerator/constants.ts
+++ src/hooks/useColorGenerator/constants.ts
@@ -1,41 +1,32 @@
 import type {ThemeColorSettings} from './types';
 
-const optionsFromLS: Record<string, ThemeColorSettings> = JSON.parse(
-    localStorage.getItem('colorOptions') ?? '{}',
-);
-
-export const colorOptions: Record<string, ThemeColorSettings> = localStorage.getItem('colorOptions')
-    ? optionsFromLS
-    : {
-          dark: {
-              light: {
-                  lightness: [25, 35],
-                  situration: [45, 55],
-              },
-              medium: {
-                  lightness: [40, 80],
-                  situration: [15, 55],
-              },
-              heavy: {
-                  lightness: [45, 55],
-                  situration: [45, 55],
-              },
-          },
-          light: {
-              light: {
-                  lightness: [90, 95],
-                  situration: [5, 10],
-              },
-              medium: {
-                  lightness: [75, 85],
-                  situration: [15, 20],
-              },
-              heavy: {
-                  lightness: [45, 55],
-                  situration: [30, 40],
-              },
-          },
-      };
-
-export const WHITE_COLOR = '#ffffff';
-export const BLACK_COLOR = '#000000';
+export const colorOptions: Record<string, ThemeColorSettings> = {
+    dark: {
+        light: {
+            lightness: [35, 40],
+            saturation: [5, 10],
+        },
+        medium: {
+            lightness: [50, 55],
+            saturation: [12, 17],
+        },
+        heavy: {
+            lightness: [75, 80],
+            saturation: [20, 25],
+        },
+    },
+    light: {
+        light: {
+            lightness: [90, 95],
+            saturation: [5, 10],
+        },
+        medium: {
+            lightness: [75, 80],
+            saturation: [12, 17],
+        },
+        heavy: {
+            lightness: [55, 60],
+            saturation: [20, 25],
+        },
+    },
+};

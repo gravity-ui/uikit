888ba80a840c571d730d89ef7170edbcb35c27c3
vanilla-wave <vizetmail@gmail.com>
2025-06-04 15:23:03 +0200

    fix: randomString util string length fix

--- src/components/utils/common.ts
+++ src/components/utils/common.ts
@@ -38,7 +38,7 @@ const MASK = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 export const randomString = (length: number) => {
     let result = '';
 
-    for (let index = length; index >= 0; index--) {
+    for (let index = length; index > 0; index--) {
         result += MASK[Math.round(Math.random() * (MASK.length - 1))];
     }
 

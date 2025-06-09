40b997305a4d7f9c39ad119231a2a6e2f1eab76b
Roman Fedorenkov <1071418+SeqviriouM@users.noreply.github.com>
2025-06-09 14:01:00 +0300

    feat: add prop 'maxWidth' to PlaceholderContainer component (#2296)

--- src/components/PlaceholderContainer/__stories__/PlaceholderContainer.stories.tsx
+++ src/components/PlaceholderContainer/__stories__/PlaceholderContainer.stories.tsx
@@ -187,3 +187,21 @@ export const Actions: Story = {
         ...Default.args,
     },
 };
+
+export const CustomMaxWidth: Story = {
+    render: (args) => (
+        <React.Fragment>
+            <Showcase>
+                <PlaceholderContainer {...args} title="Default max-width" />
+            </Showcase>
+            <Showcase>
+                <PlaceholderContainer {...args} title="Custom max-width: 800" maxWidth={800} />
+            </Showcase>
+        </React.Fragment>
+    ),
+    args: {
+        ...Default.args,
+        description:
+            'This example demonstrates how to override the default max-width of the content using the maxWidth prop.',
+    },
+};

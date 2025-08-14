be875799a3ce1d6ae04d4d001c448d448f2c1e4b
Anastasiia Beriia <estasieberia@yandex-team.ru>
2025-08-14 22:34:20 +0400

    feat(colorpicker): adds input at color display

--- src/components/ColorPicker/components/ColorDisplay/ColorDisplay.tsx
+++ src/components/ColorPicker/components/ColorDisplay/ColorDisplay.tsx
@@ -5,31 +5,64 @@ import type {HsvaColor} from '@uiw/react-color';
 
 import {Button} from '../../../Button';
 import {Card} from '../../../Card';
+import {TextInput} from '../../../controls';
 import {Flex} from '../../../layout';
 import {b} from '../../constants';
 import {Modes} from '../../types';
-import {getTextValueByMode} from '../../utils';
+import {convertSelectedModeColorToHsva, getTextValueByMode} from '../../utils';
 
 type ColorDisplayProps = {
     hsva: HsvaColor;
     onClick: () => void;
+    onColorChange?: (color: HsvaColor) => void;
 };
 export const ColorDisplay = React.forwardRef<HTMLDivElement, ColorDisplayProps>(
-    ({hsva, onClick}, ref) => (
-        <Card view={'outlined'} className={b('picker-wrapper')} ref={ref}>
-            <Flex alignItems={'center'} gap={2}>
-                <Button size={'s'} className={b('underlay')} onClick={onClick}>
-                    <div
-                        className={b('overlay')}
-                        style={{
-                            backgroundColor: hsvaToRgbaString(hsva),
-                        }}
+    ({hsva, onClick, onColorChange}, ref) => {
+        const [inputValue, setInputValue] = React.useState(() =>
+            getTextValueByMode(hsva, Modes.Hex, false),
+        );
+
+        React.useEffect(() => {
+            setInputValue(getTextValueByMode(hsva, Modes.Hex, false));
+        }, [hsva]);
+
+        const handleInputChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+            setInputValue(e.target.value);
+        }, []);
+
+        const handleInputBlur = React.useCallback(() => {
+            try {
+                const newHsva = convertSelectedModeColorToHsva(inputValue, Modes.Hex, false);
+                const updatedHsva = {...newHsva, a: hsva.a};
+
+                onColorChange?.(updatedHsva);
+            } catch {
+                setInputValue(getTextValueByMode(hsva, Modes.Hex, false));
+            }
+        }, [inputValue, hsva.a, onColorChange, hsva]);
+
+        return (
+            <Card view={'outlined'} className={b('picker-wrapper')} ref={ref}>
+                <Flex alignItems={'center'} gap={2}>
+                    <Button size={'s'} className={b('underlay')} onClick={onClick}>
+                        <div
+                            className={b('overlay')}
+                            style={{
+                                backgroundColor: hsvaToRgbaString(hsva),
+                            }}
+                        />
+                    </Button>
+                    <TextInput
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        onBlur={handleInputBlur}
+                        size="s"
+                        view={'clear'}
                     />
-                </Button>
-                <div>{getTextValueByMode(hsva, Modes.Hex, false)}</div>
-            </Flex>
-        </Card>
-    ),
+                </Flex>
+            </Card>
+        );
+    },
 );
 
 ColorDisplay.displayName = 'ColorDisplay';

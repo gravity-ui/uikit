537706f830715359a9c2cd14bf6b943398098239
Alexey <sudilovskiy@yandex-team.ru>
2025-10-28 16:48:28 +0000

    feat: export `EventEmitter` class (#2467)

--- src/utils/EventEmitter/EventEmitter.ts
+++ src/utils/EventEmitter/EventEmitter.ts
@@ -1,35 +1,52 @@
-type EventListener<T> = (data: T) => void;
+import type {EventListeners} from './types';
 
-export class EventEmitter<T> {
-    private listeners: EventListener<T>[];
+/**
+ * Class for emitting events
+ * @example
+ * import {EventEmitter} from '@gravity-ui/uikit';
+ *
+ * const emitter = new EventEmitter<{
+ *   foobar: [string, number]
+ * }>();
+ *
+ * emitter.notify('foobar', ['foo', 0]); // returns "false", because there is no attached listeners yet
+ *
+ * const unsubscribe = emitter.subscribe('foobar', (a, b) => {
+ *   console.log(a, b);
+ * });
+ *
+ * emitter.notify('foobar', ['foo', 1]); // returns "true", because listener is appeared
+ */
+export class EventEmitter<T extends Record<string, unknown[]>> {
+    private listeners: EventListeners<T>;
 
     constructor() {
-        this.listeners = [];
+        this.listeners = {};
     }
 
     destroy() {
-        this.listeners = [];
+        this.listeners = {};
     }
 
-    subscribe(listener: EventListener<T>) {
+    subscribe<Event extends keyof T>(event: Event, listener: (...args: T[Event]) => void) {
         if (typeof listener === 'function') {
-            this.listeners.push(listener);
+            this.listeners[event] = (this.listeners[event] || []).concat(listener);
         }
 
         return () => {
-            this.listeners = this.listeners.filter(
+            this.listeners[event] = this.listeners[event]?.filter(
                 (currentListener) => listener !== currentListener,
             );
         };
     }
 
-    notify(data: T) {
-        if (this.listeners.length === 0) {
+    notify<Event extends keyof T>(event: Event, data: T[Event]) {
+        if (!this.listeners[event]?.length) {
             return false;
         }
 
-        for (const listener of this.listeners) {
-            listener(data);
+        for (const listener of this.listeners[event]) {
+            listener(...data);
         }
 
         return true;

30f8ece18da7c3689d19cde44c84b2987fc6442d
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2025-10-28 16:54:50 +0100

    feat: reexport `EventEmitter`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export {Lang, configure, getConfig} from './utils/configure';
 export type {StringWithSuggest} from './utils/types';
+export {EventEmitter} from './utils/EventEmitter';
+export type {EventListeners} from './utils/EventEmitter';
 export * from './components';
 export * from './hooks';
 export * from './utils/dom';

f969d80e904f445aae0e88c19e2474d4d417847f
Igor Dyrov <dyrovigor@yandex-team.ru>
2025-11-05 09:34:14 +0300

    chore: default story

--- src/components/Drawer/__stories__/Drawer.stories.tsx
+++ src/components/Drawer/__stories__/Drawer.stories.tsx
@@ -1,13 +1,62 @@
-import type {Meta, StoryFn} from '@storybook/react-webpack5';
+import type {Meta, StoryFn, StoryObj} from '@storybook/react-webpack5';
+import {action} from 'storybook/actions';
+
+import {Drawer} from '../components/Drawer';
 
 import {DisablePortalShowcase} from './DisablePortal';
 import {HideVeilShowcase} from './HideVeil';
 import {ResizableItemShowcase} from './ResizableItem';
+import {PlaceholderText} from './mock';
 
 export default {
     title: 'Components/Overlays/Drawer',
-    component: ResizableItemShowcase,
+    component: Drawer,
+    parameters: {
+        a11y: {
+            context: '#storybook-root',
+            config: {
+                rules: [
+                    {
+                        id: 'color-contrast',
+                        enabled: false,
+                    },
+                    {
+                        id: 'label',
+                        enabled: false,
+                    },
+                ],
+            },
+        },
+    },
 } as Meta;
+
+type Story = StoryObj<typeof Drawer>;
+
+export const Default: Story = {
+    args: {
+        children: (
+            <div
+                style={{
+                    boxSizing: 'border-box',
+                    padding: '8px 16px',
+                    height: '100%',
+                    overflowY: 'auto',
+                }}
+            >
+                <PlaceholderText />
+            </div>
+        ),
+        open: true,
+        onOpenChange: action('onOpenChange'),
+        onResizeStart: action('onResizeStart'),
+        onResizeEnd: action('onResizeEnd'),
+        onResize: action('onResize'),
+        onTransitionIn: action('onTransitionIn'),
+        onTransitionInComplete: action('onTransitionInComplete'),
+        onTransitionOut: action('onTransitionOut'),
+        onTransitionOutComplete: action('onTransitionOutComplete'),
+    },
+};
 
 const ResizableItemTemplate: StoryFn = () => <ResizableItemShowcase />;
 export const ResizableItem = ResizableItemTemplate.bind({});

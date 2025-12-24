49a72064950fbecfff5287c51bf2c191fd17c495
Arsen Afaunov <arsen-afaunov@yandex-team.ru>
2025-12-24 15:55:22 +0300

    fix: fix nesting counter consistency issues

--- src/components/FileDropZone/FileDropZone.tsx
+++ src/components/FileDropZone/FileDropZone.tsx
@@ -4,7 +4,7 @@ import type {IconData} from '../..';
 import type {UseDropZoneAccept} from '../../hooks/useDropZone';
 import type {BaseInputControlProps} from '../controls/types';
 
-import {FileDropZoneProvider} from './FileDropZone.Provider';
+import {FileDropZoneProvider, useFileZoneContext} from './FileDropZone.Provider';
 import {cnFileDropZone} from './FileDropZone.classname';
 import {FileDropZoneButton} from './parts/FileDropZone.Button';
 import {FileDropZoneDescription} from './parts/FileDropZone.Description';
@@ -28,31 +28,58 @@ export interface FileDropZoneProps extends Pick<BaseInputControlProps, 'validati
     children?: React.ReactNode;
 }
 
-const BaseFileDropZone = React.memo<FileDropZoneProps>(
-    ({children, ...restProps}: FileDropZoneProps) => {
-        const content = React.useMemo(() => {
-            if (typeof children !== 'undefined') {
-                return children;
-            }
+interface FileDropZoneContainerProps {
+    className?: string;
+    children: React.ReactNode;
+}
 
-            return (
-                <React.Fragment>
-                    <FileDropZoneIcon />
-                    <FileDropZoneTitle />
-                    <FileDropZoneDescription />
-                    <FileDropZoneButton />
-                </React.Fragment>
-            );
-        }, [children]);
+const FileDropZoneContent = ({className, children}: FileDropZoneContainerProps) => {
+    const {isDraggingOver, disabled, errorMessage, validationState, getDroppableProps, onKeyDown} =
+        useFileZoneContext();
+
+    const hasError = Boolean(errorMessage);
+
+    const content = React.useMemo(() => {
+        if (typeof children !== 'undefined') {
+            return children;
+        }
 
         return (
-            <FileDropZoneProvider
-                {...restProps}
-                className={cnFileDropZone({
+            <React.Fragment>
+                <FileDropZoneIcon />
+                <FileDropZoneTitle />
+                <FileDropZoneDescription />
+                <FileDropZoneButton />
+            </React.Fragment>
+        );
+    }, [children]);
+
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
+    return (
+        <div
+            {...getDroppableProps()}
+            onKeyDown={onKeyDown}
+            className={cnFileDropZone(
+                {
+                    'drag-hover': isDraggingOver,
+                    disabled: disabled,
+                    error: hasError || validationState === 'invalid',
                     'default-layout': typeof children === 'undefined',
-                })}
-            >
-                {content}
+                },
+                className,
+            )}
+        >
+            {content}
+        </div>
+    );
+    /* eslint-enable jsx-a11y/no-static-element-interactions */
+};
+
+const BaseFileDropZone = React.memo<FileDropZoneProps>(
+    ({children, className, ...restProps}: FileDropZoneProps) => {
+        return (
+            <FileDropZoneProvider {...restProps}>
+                <FileDropZoneContent className={className}>{children}</FileDropZoneContent>
             </FileDropZoneProvider>
         );
     },

5ca12c7a07b674f5dd5f9e47f810e639790b7886
Alexey <sudilovskiy@yandex-team.ru>
2026-01-13 19:13:45 +0300

    refactor(Toaster): rename ToasterPublicClass -> ToasterImplementation (#2546)

--- src/components/Toaster/Provider/ToasterProvider.tsx
+++ src/components/Toaster/Provider/ToasterProvider.tsx
@@ -2,13 +2,13 @@
 
 import * as React from 'react';
 
-import type {InternalToastProps, ToasterPublicClass} from '../types';
+import type {InternalToastProps, ToasterImplementation} from '../types';
 
 import {ToasterContext} from './ToasterContext';
 import {ToastsContext} from './ToastsContext';
 
 type Props = React.PropsWithChildren<{
-    toaster: ToasterPublicClass;
+    toaster: ToasterImplementation;
 }>;
 
 export const ToasterProvider = ({toaster, children}: Props) => {

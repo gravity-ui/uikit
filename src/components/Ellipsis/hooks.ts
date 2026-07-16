9d792bbc67c489740ae9794fe70f6ae1e73777e3
PahaN47 <pahan47@yandex-team.ru>
2026-07-16 17:18:03 +0300

    fix: per-instance observer

--- src/components/Ellipsis/hooks.ts
+++ src/components/Ellipsis/hooks.ts
@@ -1,41 +1,28 @@
 import * as React from 'react';
 
+import {useResizeObserver} from '../../hooks';
+
 interface ResizeParams {
     text: string;
     startOffset: string;
     endOffset: string;
     setVisibleText: (text: string) => void;
-    measure: HTMLSpanElement | null;
+    containerRef: React.RefObject<HTMLSpanElement | null>;
+    measureRef: React.RefObject<HTMLSpanElement | null>;
 }
 
 const ELLIPSIS_CHAR = '\u2026';
 
-let observer: ResizeObserver | null = null;
-const resizeParamsMap: Map<HTMLSpanElement, ResizeParams> = new Map();
-const measureToContainerMap: Map<Element, HTMLSpanElement> = new Map();
-const containerToMeasureMap: Map<HTMLSpanElement, Element> = new Map();
+const getElementWidth = (element: HTMLElement) => element.getBoundingClientRect().width;
 
-const handleResize = (container: HTMLSpanElement | null) => {
-    if (!container) {
-        return;
-    }
+const calculateCollapsedText = (
+    availableSpace: number,
+    {text, startOffset, endOffset, measureRef}: ResizeParams,
+): string | null => {
+    const measure = measureRef.current;
 
-    const resizeParams = resizeParamsMap.get(container);
-    if (!resizeParams?.measure) {
-        return;
-    }
-
-    const {text, startOffset, endOffset, setVisibleText, measure} = resizeParams;
-
-    const available = container.getBoundingClientRect().width;
-    if (available <= 0) {
-        return;
-    }
-
-    measure.textContent = text;
-    if (measure.getBoundingClientRect().width <= available) {
-        setVisibleText(text);
-        return;
+    if (!measure) {
+        return null;
     }
 
     const collapsibleStartIndex = startOffset.length;
@@ -60,7 +47,7 @@ const handleResize = (container: HTMLSpanElement | null) => {
         currentResult = startOffset + start + ELLIPSIS_CHAR + end + endOffset;
 
         measure.textContent = currentResult;
-        if (measure.getBoundingClientRect().width <= available) {
+        if (getElementWidth(measure) <= availableSpace) {
             result = currentResult;
             minCharacters = currentLength + 1;
         } else {
@@ -70,80 +57,41 @@ const handleResize = (container: HTMLSpanElement | null) => {
 
     // Measure is observed only for font changes, so we keep the text the same
     measure.textContent = text;
-    setVisibleText(result);
+
+    return result;
 };
 
-const observerCallback = async (entries: ResizeObserverEntry[]) => {
-    const handledContainers = new Set<HTMLSpanElement>();
+const handleResize = (params: ResizeParams) => {
+    const {text, setVisibleText, containerRef, measureRef} = params;
+    const container = containerRef.current;
+    const measure = measureRef.current;
 
-    for (const {target} of entries) {
-        const container = measureToContainerMap.get(target) ?? (target as HTMLSpanElement);
-        if (handledContainers.has(container)) {
-            continue;
-        }
-
-        handledContainers.add(container);
-
-        handleResize(container);
-    }
-};
-
-const setResizeParams = (container: HTMLSpanElement | null, resizeParams: ResizeParams) => {
-    if (!container) {
+    if (!container || !measure) {
         return;
     }
 
-    resizeParamsMap.set(container, resizeParams);
-};
-
-const deleteResizeParams = (container: HTMLSpanElement | null) => {
-    if (!container) {
+    const availableSpace = getElementWidth(container);
+    if (availableSpace <= 0) {
         return;
     }
 
-    resizeParamsMap.delete(container);
-};
-
-const subscribeResize = (container: HTMLSpanElement | null) => {
-    if (!container) {
+    measure.textContent ||= text;
+    if (getElementWidth(measure) <= availableSpace) {
+        setVisibleText(text);
         return;
     }
 
-    if (!observer) {
-        observer = new ResizeObserver(observerCallback);
-    }
+    const collapsedText = calculateCollapsedText(availableSpace, params);
 
-    // Observe container to detect size and content changes
-    observer.observe(container);
-
-    // Observe measure as well to detect font changes
-    const measure = resizeParamsMap.get(container)?.measure;
-    if (measure) {
-        observer.observe(measure);
-        measureToContainerMap.set(measure, container);
-        containerToMeasureMap.set(container, measure);
-    }
-};
-
-const unsubscribeResize = (container: HTMLSpanElement | null) => {
-    if (!container) {
+    if (collapsedText === null) {
         return;
     }
 
-    const measure = containerToMeasureMap.get(container);
+    setVisibleText(collapsedText);
+};
 
-    if (measure) {
-        observer?.unobserve(measure);
-        measureToContainerMap.delete(measure);
-    }
-
-    containerToMeasureMap.delete(container);
-    observer?.unobserve(container);
-
-    if (resizeParamsMap.size === 0) {
-        observer?.disconnect();
-        observer = null;
-    }
+const makeOnResize = (params: ResizeParams) => () => {
+    handleResize(params);
 };
 
 export interface CenterEllipsisProps {
@@ -159,62 +107,40 @@ export const useCenterEllipsis = ({
 }: CenterEllipsisProps) => {
     const containerRef = React.useRef<HTMLSpanElement | null>(null);
     const measureRef = React.useRef<HTMLSpanElement>(null);
+
     const [visibleText, setVisibleText] = React.useState(text);
 
-    const handleTextChange = React.useCallback(
-        (container: HTMLSpanElement | null) => {
-            setResizeParams(container, {
+    const onResize = React.useMemo(
+        () =>
+            makeOnResize({
                 text,
                 startOffset,
                 endOffset,
                 setVisibleText,
-                measure: measureRef.current,
-            });
-            handleResize(container);
-        },
-        [endOffset, startOffset, text],
+                measureRef,
+                containerRef,
+            }),
+        [text, startOffset, endOffset, setVisibleText],
     );
 
-    // does the same as useLayoutEffect, but does not trigger warnings
+    // Does the same as useLayoutEffect, but does not trigger warnings with SSR
     const containerRefCallback = React.useCallback<React.RefCallback<HTMLSpanElement>>(
         (node) => {
             if (!node) {
                 return;
             }
 
-            if (!containerRef.current) {
-                document.fonts.ready.then(() => handleTextChange(node));
-            }
-
             containerRef.current = node;
+            onResize();
         },
-        [handleTextChange],
+        [onResize],
     );
 
-    React.useEffect(() => {
-        const container = containerRef.current;
-        if (!container) {
-            return;
-        }
-
-        handleTextChange(container);
-    }, [handleTextChange, text]);
-
-    React.useEffect(() => {
-        const container = containerRef.current;
-
-        if (!container) {
-            return;
-        }
-
-        subscribeResize(container);
-
-        // eslint-disable-next-line consistent-return
-        return () => {
-            deleteResizeParams(container);
-            unsubscribeResize(container);
-        };
-    }, []);
+    useResizeObserver({
+        // Observing container for resize and measure for font changes
+        ref: React.useMemo(() => [containerRef, measureRef], [containerRef, measureRef]),
+        onResize,
+    });
 
     return {visibleText, containerRef: containerRefCallback, measureRef};
 };
